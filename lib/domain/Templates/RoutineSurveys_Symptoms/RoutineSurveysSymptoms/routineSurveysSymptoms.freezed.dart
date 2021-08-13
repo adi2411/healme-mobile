c3c49a7efead717e56148c65fbac4e2fbd0c058f
aditya <aditya_jinegar@yahoo.com>
2021-08-13 13:53:17 -0400

    Create routineSurveysSymptoms.freezed.dart

---
+++ lib/domain/Templates/RoutineSurveys_Symptoms/RoutineSurveysSymptoms/routineSurveysSymptoms.freezed.dart
@@ -0,0 +1,175 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides
+
+part of 'routineSurveysSymptoms.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more informations: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+class _$RoutineSurveysSymptomsTearOff {
+  const _$RoutineSurveysSymptomsTearOff();
+
+  _RoutineSurveysSyptoms call(
+      {required int routineSurveyRoutineSurveyId,
+      required int symptomsSymptomId}) {
+    return _RoutineSurveysSyptoms(
+      routineSurveyRoutineSurveyId: routineSurveyRoutineSurveyId,
+      symptomsSymptomId: symptomsSymptomId,
+    );
+  }
+}
+
+/// @nodoc
+const $RoutineSurveysSymptoms = _$RoutineSurveysSymptomsTearOff();
+
+/// @nodoc
+mixin _$RoutineSurveysSymptoms {
+  int get routineSurveyRoutineSurveyId => throw _privateConstructorUsedError;
+  int get symptomsSymptomId => throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $RoutineSurveysSymptomsCopyWith<RoutineSurveysSymptoms> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $RoutineSurveysSymptomsCopyWith<$Res> {
+  factory $RoutineSurveysSymptomsCopyWith(RoutineSurveysSymptoms value,
+          $Res Function(RoutineSurveysSymptoms) then) =
+      _$RoutineSurveysSymptomsCopyWithImpl<$Res>;
+  $Res call({int routineSurveyRoutineSurveyId, int symptomsSymptomId});
+}
+
+/// @nodoc
+class _$RoutineSurveysSymptomsCopyWithImpl<$Res>
+    implements $RoutineSurveysSymptomsCopyWith<$Res> {
+  _$RoutineSurveysSymptomsCopyWithImpl(this._value, this._then);
+
+  final RoutineSurveysSymptoms _value;
+  // ignore: unused_field
+  final $Res Function(RoutineSurveysSymptoms) _then;
+
+  @override
+  $Res call({
+    Object? routineSurveyRoutineSurveyId = freezed,
+    Object? symptomsSymptomId = freezed,
+  }) {
+    return _then(_value.copyWith(
+      routineSurveyRoutineSurveyId: routineSurveyRoutineSurveyId == freezed
+          ? _value.routineSurveyRoutineSurveyId
+          : routineSurveyRoutineSurveyId // ignore: cast_nullable_to_non_nullable
+              as int,
+      symptomsSymptomId: symptomsSymptomId == freezed
+          ? _value.symptomsSymptomId
+          : symptomsSymptomId // ignore: cast_nullable_to_non_nullable
+              as int,
+    ));
+  }
+}
+
+/// @nodoc
+abstract class _$RoutineSurveysSyptomsCopyWith<$Res>
+    implements $RoutineSurveysSymptomsCopyWith<$Res> {
+  factory _$RoutineSurveysSyptomsCopyWith(_RoutineSurveysSyptoms value,
+          $Res Function(_RoutineSurveysSyptoms) then) =
+      __$RoutineSurveysSyptomsCopyWithImpl<$Res>;
+  @override
+  $Res call({int routineSurveyRoutineSurveyId, int symptomsSymptomId});
+}
+
+/// @nodoc
+class __$RoutineSurveysSyptomsCopyWithImpl<$Res>
+    extends _$RoutineSurveysSymptomsCopyWithImpl<$Res>
+    implements _$RoutineSurveysSyptomsCopyWith<$Res> {
+  __$RoutineSurveysSyptomsCopyWithImpl(_RoutineSurveysSyptoms _value,
+      $Res Function(_RoutineSurveysSyptoms) _then)
+      : super(_value, (v) => _then(v as _RoutineSurveysSyptoms));
+
+  @override
+  _RoutineSurveysSyptoms get _value => super._value as _RoutineSurveysSyptoms;
+
+  @override
+  $Res call({
+    Object? routineSurveyRoutineSurveyId = freezed,
+    Object? symptomsSymptomId = freezed,
+  }) {
+    return _then(_RoutineSurveysSyptoms(
+      routineSurveyRoutineSurveyId: routineSurveyRoutineSurveyId == freezed
+          ? _value.routineSurveyRoutineSurveyId
+          : routineSurveyRoutineSurveyId // ignore: cast_nullable_to_non_nullable
+              as int,
+      symptomsSymptomId: symptomsSymptomId == freezed
+          ? _value.symptomsSymptomId
+          : symptomsSymptomId // ignore: cast_nullable_to_non_nullable
+              as int,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$_RoutineSurveysSyptoms extends _RoutineSurveysSyptoms {
+  _$_RoutineSurveysSyptoms(
+      {required this.routineSurveyRoutineSurveyId,
+      required this.symptomsSymptomId})
+      : super._();
+
+  @override
+  final int routineSurveyRoutineSurveyId;
+  @override
+  final int symptomsSymptomId;
+
+  @override
+  String toString() {
+    return 'RoutineSurveysSymptoms(routineSurveyRoutineSurveyId: $routineSurveyRoutineSurveyId, symptomsSymptomId: $symptomsSymptomId)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other is _RoutineSurveysSyptoms &&
+            (identical(other.routineSurveyRoutineSurveyId,
+                    routineSurveyRoutineSurveyId) ||
+                const DeepCollectionEquality().equals(
+                    other.routineSurveyRoutineSurveyId,
+                    routineSurveyRoutineSurveyId)) &&
+            (identical(other.symptomsSymptomId, symptomsSymptomId) ||
+                const DeepCollectionEquality()
+                    .equals(other.symptomsSymptomId, symptomsSymptomId)));
+  }
+
+  @override
+  int get hashCode =>
+      runtimeType.hashCode ^
+      const DeepCollectionEquality().hash(routineSurveyRoutineSurveyId) ^
+      const DeepCollectionEquality().hash(symptomsSymptomId);
+
+  @JsonKey(ignore: true)
+  @override
+  _$RoutineSurveysSyptomsCopyWith<_RoutineSurveysSyptoms> get copyWith =>
+      __$RoutineSurveysSyptomsCopyWithImpl<_RoutineSurveysSyptoms>(
+          this, _$identity);
+}
+
+abstract class _RoutineSurveysSyptoms extends RoutineSurveysSymptoms {
+  factory _RoutineSurveysSyptoms(
+      {required int routineSurveyRoutineSurveyId,
+      required int symptomsSymptomId}) = _$_RoutineSurveysSyptoms;
+  _RoutineSurveysSyptoms._() : super._();
+
+  @override
+  int get routineSurveyRoutineSurveyId => throw _privateConstructorUsedError;
+  @override
+  int get symptomsSymptomId => throw _privateConstructorUsedError;
+  @override
+  @JsonKey(ignore: true)
+  _$RoutineSurveysSyptomsCopyWith<_RoutineSurveysSyptoms> get copyWith =>
+      throw _privateConstructorUsedError;
+}

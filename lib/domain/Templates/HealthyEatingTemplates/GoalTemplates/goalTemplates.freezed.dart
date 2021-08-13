58676f3f32228c592d90ad1dfd4186a51ebd8b9b
aditya <aditya_jinegar@yahoo.com>
2021-08-13 12:08:04 -0400

    Create goalTemplates.freezed.dart

---
+++ lib/domain/Templates/HealthyEatingTemplates/GoalTemplates/goalTemplates.freezed.dart
@@ -0,0 +1,301 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides
+
+part of 'goalTemplates.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more informations: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+class _$GoalTemplatesTearOff {
+  const _$GoalTemplatesTearOff();
+
+  _GoalTemplates call(
+      {required int goalTemplateId,
+      required StringSingleLine createdBy,
+      required DateTime createdDate,
+      required StringSingleLine lastModifiedBy,
+      required DateTime lastModifiedDate,
+      required StringMultiLine goalDesc,
+      required StringSingleLine goalType}) {
+    return _GoalTemplates(
+      goalTemplateId: goalTemplateId,
+      createdBy: createdBy,
+      createdDate: createdDate,
+      lastModifiedBy: lastModifiedBy,
+      lastModifiedDate: lastModifiedDate,
+      goalDesc: goalDesc,
+      goalType: goalType,
+    );
+  }
+}
+
+/// @nodoc
+const $GoalTemplates = _$GoalTemplatesTearOff();
+
+/// @nodoc
+mixin _$GoalTemplates {
+  int get goalTemplateId => throw _privateConstructorUsedError;
+  StringSingleLine get createdBy => throw _privateConstructorUsedError;
+  DateTime get createdDate => throw _privateConstructorUsedError;
+  StringSingleLine get lastModifiedBy => throw _privateConstructorUsedError;
+  DateTime get lastModifiedDate => throw _privateConstructorUsedError;
+  StringMultiLine get goalDesc => throw _privateConstructorUsedError;
+  StringSingleLine get goalType => throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $GoalTemplatesCopyWith<GoalTemplates> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $GoalTemplatesCopyWith<$Res> {
+  factory $GoalTemplatesCopyWith(
+          GoalTemplates value, $Res Function(GoalTemplates) then) =
+      _$GoalTemplatesCopyWithImpl<$Res>;
+  $Res call(
+      {int goalTemplateId,
+      StringSingleLine createdBy,
+      DateTime createdDate,
+      StringSingleLine lastModifiedBy,
+      DateTime lastModifiedDate,
+      StringMultiLine goalDesc,
+      StringSingleLine goalType});
+}
+
+/// @nodoc
+class _$GoalTemplatesCopyWithImpl<$Res>
+    implements $GoalTemplatesCopyWith<$Res> {
+  _$GoalTemplatesCopyWithImpl(this._value, this._then);
+
+  final GoalTemplates _value;
+  // ignore: unused_field
+  final $Res Function(GoalTemplates) _then;
+
+  @override
+  $Res call({
+    Object? goalTemplateId = freezed,
+    Object? createdBy = freezed,
+    Object? createdDate = freezed,
+    Object? lastModifiedBy = freezed,
+    Object? lastModifiedDate = freezed,
+    Object? goalDesc = freezed,
+    Object? goalType = freezed,
+  }) {
+    return _then(_value.copyWith(
+      goalTemplateId: goalTemplateId == freezed
+          ? _value.goalTemplateId
+          : goalTemplateId // ignore: cast_nullable_to_non_nullable
+              as int,
+      createdBy: createdBy == freezed
+          ? _value.createdBy
+          : createdBy // ignore: cast_nullable_to_non_nullable
+              as StringSingleLine,
+      createdDate: createdDate == freezed
+          ? _value.createdDate
+          : createdDate // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      lastModifiedBy: lastModifiedBy == freezed
+          ? _value.lastModifiedBy
+          : lastModifiedBy // ignore: cast_nullable_to_non_nullable
+              as StringSingleLine,
+      lastModifiedDate: lastModifiedDate == freezed
+          ? _value.lastModifiedDate
+          : lastModifiedDate // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      goalDesc: goalDesc == freezed
+          ? _value.goalDesc
+          : goalDesc // ignore: cast_nullable_to_non_nullable
+              as StringMultiLine,
+      goalType: goalType == freezed
+          ? _value.goalType
+          : goalType // ignore: cast_nullable_to_non_nullable
+              as StringSingleLine,
+    ));
+  }
+}
+
+/// @nodoc
+abstract class _$GoalTemplatesCopyWith<$Res>
+    implements $GoalTemplatesCopyWith<$Res> {
+  factory _$GoalTemplatesCopyWith(
+          _GoalTemplates value, $Res Function(_GoalTemplates) then) =
+      __$GoalTemplatesCopyWithImpl<$Res>;
+  @override
+  $Res call(
+      {int goalTemplateId,
+      StringSingleLine createdBy,
+      DateTime createdDate,
+      StringSingleLine lastModifiedBy,
+      DateTime lastModifiedDate,
+      StringMultiLine goalDesc,
+      StringSingleLine goalType});
+}
+
+/// @nodoc
+class __$GoalTemplatesCopyWithImpl<$Res>
+    extends _$GoalTemplatesCopyWithImpl<$Res>
+    implements _$GoalTemplatesCopyWith<$Res> {
+  __$GoalTemplatesCopyWithImpl(
+      _GoalTemplates _value, $Res Function(_GoalTemplates) _then)
+      : super(_value, (v) => _then(v as _GoalTemplates));
+
+  @override
+  _GoalTemplates get _value => super._value as _GoalTemplates;
+
+  @override
+  $Res call({
+    Object? goalTemplateId = freezed,
+    Object? createdBy = freezed,
+    Object? createdDate = freezed,
+    Object? lastModifiedBy = freezed,
+    Object? lastModifiedDate = freezed,
+    Object? goalDesc = freezed,
+    Object? goalType = freezed,
+  }) {
+    return _then(_GoalTemplates(
+      goalTemplateId: goalTemplateId == freezed
+          ? _value.goalTemplateId
+          : goalTemplateId // ignore: cast_nullable_to_non_nullable
+              as int,
+      createdBy: createdBy == freezed
+          ? _value.createdBy
+          : createdBy // ignore: cast_nullable_to_non_nullable
+              as StringSingleLine,
+      createdDate: createdDate == freezed
+          ? _value.createdDate
+          : createdDate // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      lastModifiedBy: lastModifiedBy == freezed
+          ? _value.lastModifiedBy
+          : lastModifiedBy // ignore: cast_nullable_to_non_nullable
+              as StringSingleLine,
+      lastModifiedDate: lastModifiedDate == freezed
+          ? _value.lastModifiedDate
+          : lastModifiedDate // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      goalDesc: goalDesc == freezed
+          ? _value.goalDesc
+          : goalDesc // ignore: cast_nullable_to_non_nullable
+              as StringMultiLine,
+      goalType: goalType == freezed
+          ? _value.goalType
+          : goalType // ignore: cast_nullable_to_non_nullable
+              as StringSingleLine,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$_GoalTemplates extends _GoalTemplates {
+  _$_GoalTemplates(
+      {required this.goalTemplateId,
+      required this.createdBy,
+      required this.createdDate,
+      required this.lastModifiedBy,
+      required this.lastModifiedDate,
+      required this.goalDesc,
+      required this.goalType})
+      : super._();
+
+  @override
+  final int goalTemplateId;
+  @override
+  final StringSingleLine createdBy;
+  @override
+  final DateTime createdDate;
+  @override
+  final StringSingleLine lastModifiedBy;
+  @override
+  final DateTime lastModifiedDate;
+  @override
+  final StringMultiLine goalDesc;
+  @override
+  final StringSingleLine goalType;
+
+  @override
+  String toString() {
+    return 'GoalTemplates(goalTemplateId: $goalTemplateId, createdBy: $createdBy, createdDate: $createdDate, lastModifiedBy: $lastModifiedBy, lastModifiedDate: $lastModifiedDate, goalDesc: $goalDesc, goalType: $goalType)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other is _GoalTemplates &&
+            (identical(other.goalTemplateId, goalTemplateId) ||
+                const DeepCollectionEquality()
+                    .equals(other.goalTemplateId, goalTemplateId)) &&
+            (identical(other.createdBy, createdBy) ||
+                const DeepCollectionEquality()
+                    .equals(other.createdBy, createdBy)) &&
+            (identical(other.createdDate, createdDate) ||
+                const DeepCollectionEquality()
+                    .equals(other.createdDate, createdDate)) &&
+            (identical(other.lastModifiedBy, lastModifiedBy) ||
+                const DeepCollectionEquality()
+                    .equals(other.lastModifiedBy, lastModifiedBy)) &&
+            (identical(other.lastModifiedDate, lastModifiedDate) ||
+                const DeepCollectionEquality()
+                    .equals(other.lastModifiedDate, lastModifiedDate)) &&
+            (identical(other.goalDesc, goalDesc) ||
+                const DeepCollectionEquality()
+                    .equals(other.goalDesc, goalDesc)) &&
+            (identical(other.goalType, goalType) ||
+                const DeepCollectionEquality()
+                    .equals(other.goalType, goalType)));
+  }
+
+  @override
+  int get hashCode =>
+      runtimeType.hashCode ^
+      const DeepCollectionEquality().hash(goalTemplateId) ^
+      const DeepCollectionEquality().hash(createdBy) ^
+      const DeepCollectionEquality().hash(createdDate) ^
+      const DeepCollectionEquality().hash(lastModifiedBy) ^
+      const DeepCollectionEquality().hash(lastModifiedDate) ^
+      const DeepCollectionEquality().hash(goalDesc) ^
+      const DeepCollectionEquality().hash(goalType);
+
+  @JsonKey(ignore: true)
+  @override
+  _$GoalTemplatesCopyWith<_GoalTemplates> get copyWith =>
+      __$GoalTemplatesCopyWithImpl<_GoalTemplates>(this, _$identity);
+}
+
+abstract class _GoalTemplates extends GoalTemplates {
+  factory _GoalTemplates(
+      {required int goalTemplateId,
+      required StringSingleLine createdBy,
+      required DateTime createdDate,
+      required StringSingleLine lastModifiedBy,
+      required DateTime lastModifiedDate,
+      required StringMultiLine goalDesc,
+      required StringSingleLine goalType}) = _$_GoalTemplates;
+  _GoalTemplates._() : super._();
+
+  @override
+  int get goalTemplateId => throw _privateConstructorUsedError;
+  @override
+  StringSingleLine get createdBy => throw _privateConstructorUsedError;
+  @override
+  DateTime get createdDate => throw _privateConstructorUsedError;
+  @override
+  StringSingleLine get lastModifiedBy => throw _privateConstructorUsedError;
+  @override
+  DateTime get lastModifiedDate => throw _privateConstructorUsedError;
+  @override
+  StringMultiLine get goalDesc => throw _privateConstructorUsedError;
+  @override
+  StringSingleLine get goalType => throw _privateConstructorUsedError;
+  @override
+  @JsonKey(ignore: true)
+  _$GoalTemplatesCopyWith<_GoalTemplates> get copyWith =>
+      throw _privateConstructorUsedError;
+}

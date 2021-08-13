81c7f6e922c351eeb9069ded9079fbcb8254a2f5
aditya <aditya_jinegar@yahoo.com>
2021-08-13 13:52:35 -0400

    Create programTemplatesMessages.freezed.dart

---
+++ lib/domain/Templates/ProgramTemplates/ProgramTemplatesMessages/programTemplatesMessages.freezed.dart
@@ -0,0 +1,193 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides
+
+part of 'programTemplatesMessages.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more informations: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+class _$ProgramTemplatesMessagesTearOff {
+  const _$ProgramTemplatesMessagesTearOff();
+
+  _ProgramTemplatesMessages call(
+      {required int programTemplateProgramTemplateId,
+      required int messagesMotivationalMessageTemplateId}) {
+    return _ProgramTemplatesMessages(
+      programTemplateProgramTemplateId: programTemplateProgramTemplateId,
+      messagesMotivationalMessageTemplateId:
+          messagesMotivationalMessageTemplateId,
+    );
+  }
+}
+
+/// @nodoc
+const $ProgramTemplatesMessages = _$ProgramTemplatesMessagesTearOff();
+
+/// @nodoc
+mixin _$ProgramTemplatesMessages {
+  int get programTemplateProgramTemplateId =>
+      throw _privateConstructorUsedError;
+  int get messagesMotivationalMessageTemplateId =>
+      throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $ProgramTemplatesMessagesCopyWith<ProgramTemplatesMessages> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $ProgramTemplatesMessagesCopyWith<$Res> {
+  factory $ProgramTemplatesMessagesCopyWith(ProgramTemplatesMessages value,
+          $Res Function(ProgramTemplatesMessages) then) =
+      _$ProgramTemplatesMessagesCopyWithImpl<$Res>;
+  $Res call(
+      {int programTemplateProgramTemplateId,
+      int messagesMotivationalMessageTemplateId});
+}
+
+/// @nodoc
+class _$ProgramTemplatesMessagesCopyWithImpl<$Res>
+    implements $ProgramTemplatesMessagesCopyWith<$Res> {
+  _$ProgramTemplatesMessagesCopyWithImpl(this._value, this._then);
+
+  final ProgramTemplatesMessages _value;
+  // ignore: unused_field
+  final $Res Function(ProgramTemplatesMessages) _then;
+
+  @override
+  $Res call({
+    Object? programTemplateProgramTemplateId = freezed,
+    Object? messagesMotivationalMessageTemplateId = freezed,
+  }) {
+    return _then(_value.copyWith(
+      programTemplateProgramTemplateId: programTemplateProgramTemplateId ==
+              freezed
+          ? _value.programTemplateProgramTemplateId
+          : programTemplateProgramTemplateId // ignore: cast_nullable_to_non_nullable
+              as int,
+      messagesMotivationalMessageTemplateId:
+          messagesMotivationalMessageTemplateId == freezed
+              ? _value.messagesMotivationalMessageTemplateId
+              : messagesMotivationalMessageTemplateId // ignore: cast_nullable_to_non_nullable
+                  as int,
+    ));
+  }
+}
+
+/// @nodoc
+abstract class _$ProgramTemplatesMessagesCopyWith<$Res>
+    implements $ProgramTemplatesMessagesCopyWith<$Res> {
+  factory _$ProgramTemplatesMessagesCopyWith(_ProgramTemplatesMessages value,
+          $Res Function(_ProgramTemplatesMessages) then) =
+      __$ProgramTemplatesMessagesCopyWithImpl<$Res>;
+  @override
+  $Res call(
+      {int programTemplateProgramTemplateId,
+      int messagesMotivationalMessageTemplateId});
+}
+
+/// @nodoc
+class __$ProgramTemplatesMessagesCopyWithImpl<$Res>
+    extends _$ProgramTemplatesMessagesCopyWithImpl<$Res>
+    implements _$ProgramTemplatesMessagesCopyWith<$Res> {
+  __$ProgramTemplatesMessagesCopyWithImpl(_ProgramTemplatesMessages _value,
+      $Res Function(_ProgramTemplatesMessages) _then)
+      : super(_value, (v) => _then(v as _ProgramTemplatesMessages));
+
+  @override
+  _ProgramTemplatesMessages get _value =>
+      super._value as _ProgramTemplatesMessages;
+
+  @override
+  $Res call({
+    Object? programTemplateProgramTemplateId = freezed,
+    Object? messagesMotivationalMessageTemplateId = freezed,
+  }) {
+    return _then(_ProgramTemplatesMessages(
+      programTemplateProgramTemplateId: programTemplateProgramTemplateId ==
+              freezed
+          ? _value.programTemplateProgramTemplateId
+          : programTemplateProgramTemplateId // ignore: cast_nullable_to_non_nullable
+              as int,
+      messagesMotivationalMessageTemplateId:
+          messagesMotivationalMessageTemplateId == freezed
+              ? _value.messagesMotivationalMessageTemplateId
+              : messagesMotivationalMessageTemplateId // ignore: cast_nullable_to_non_nullable
+                  as int,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$_ProgramTemplatesMessages extends _ProgramTemplatesMessages {
+  _$_ProgramTemplatesMessages(
+      {required this.programTemplateProgramTemplateId,
+      required this.messagesMotivationalMessageTemplateId})
+      : super._();
+
+  @override
+  final int programTemplateProgramTemplateId;
+  @override
+  final int messagesMotivationalMessageTemplateId;
+
+  @override
+  String toString() {
+    return 'ProgramTemplatesMessages(programTemplateProgramTemplateId: $programTemplateProgramTemplateId, messagesMotivationalMessageTemplateId: $messagesMotivationalMessageTemplateId)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other is _ProgramTemplatesMessages &&
+            (identical(other.programTemplateProgramTemplateId,
+                    programTemplateProgramTemplateId) ||
+                const DeepCollectionEquality().equals(
+                    other.programTemplateProgramTemplateId,
+                    programTemplateProgramTemplateId)) &&
+            (identical(other.messagesMotivationalMessageTemplateId,
+                    messagesMotivationalMessageTemplateId) ||
+                const DeepCollectionEquality().equals(
+                    other.messagesMotivationalMessageTemplateId,
+                    messagesMotivationalMessageTemplateId)));
+  }
+
+  @override
+  int get hashCode =>
+      runtimeType.hashCode ^
+      const DeepCollectionEquality().hash(programTemplateProgramTemplateId) ^
+      const DeepCollectionEquality()
+          .hash(messagesMotivationalMessageTemplateId);
+
+  @JsonKey(ignore: true)
+  @override
+  _$ProgramTemplatesMessagesCopyWith<_ProgramTemplatesMessages> get copyWith =>
+      __$ProgramTemplatesMessagesCopyWithImpl<_ProgramTemplatesMessages>(
+          this, _$identity);
+}
+
+abstract class _ProgramTemplatesMessages extends ProgramTemplatesMessages {
+  factory _ProgramTemplatesMessages(
+          {required int programTemplateProgramTemplateId,
+          required int messagesMotivationalMessageTemplateId}) =
+      _$_ProgramTemplatesMessages;
+  _ProgramTemplatesMessages._() : super._();
+
+  @override
+  int get programTemplateProgramTemplateId =>
+      throw _privateConstructorUsedError;
+  @override
+  int get messagesMotivationalMessageTemplateId =>
+      throw _privateConstructorUsedError;
+  @override
+  @JsonKey(ignore: true)
+  _$ProgramTemplatesMessagesCopyWith<_ProgramTemplatesMessages> get copyWith =>
+      throw _privateConstructorUsedError;
+}

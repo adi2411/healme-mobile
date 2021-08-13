7195ce10bd349a6583518bceb3be73af3bd25085
aditya <aditya_jinegar@yahoo.com>
2021-08-13 12:10:09 -0400

    Create routineSurveyTemplates.freezed.dart

---
+++ lib/domain/Templates/RoutineSurveys_Symptoms/RoutineSurveyTemplates/routineSurveyTemplates.freezed.dart
@@ -0,0 +1,301 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides
+
+part of 'routineSurveyTemplates.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more informations: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+class _$RoutineSurveyTemplatesTearOff {
+  const _$RoutineSurveyTemplatesTearOff();
+
+  _RoutineSurveyTemplates call(
+      {required int routineSurveyTemplateId,
+      required StringSingleLine createdBy,
+      required DateTime createdDate,
+      required StringSingleLine lastModifiedBy,
+      required DateTime lastModifiedDate,
+      required int active,
+      required StringMultiLine comment}) {
+    return _RoutineSurveyTemplates(
+      routineSurveyTemplateId: routineSurveyTemplateId,
+      createdBy: createdBy,
+      createdDate: createdDate,
+      lastModifiedBy: lastModifiedBy,
+      lastModifiedDate: lastModifiedDate,
+      active: active,
+      comment: comment,
+    );
+  }
+}
+
+/// @nodoc
+const $RoutineSurveyTemplates = _$RoutineSurveyTemplatesTearOff();
+
+/// @nodoc
+mixin _$RoutineSurveyTemplates {
+  int get routineSurveyTemplateId => throw _privateConstructorUsedError;
+  StringSingleLine get createdBy => throw _privateConstructorUsedError;
+  DateTime get createdDate => throw _privateConstructorUsedError;
+  StringSingleLine get lastModifiedBy => throw _privateConstructorUsedError;
+  DateTime get lastModifiedDate => throw _privateConstructorUsedError;
+  int get active => throw _privateConstructorUsedError;
+  StringMultiLine get comment => throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $RoutineSurveyTemplatesCopyWith<RoutineSurveyTemplates> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $RoutineSurveyTemplatesCopyWith<$Res> {
+  factory $RoutineSurveyTemplatesCopyWith(RoutineSurveyTemplates value,
+          $Res Function(RoutineSurveyTemplates) then) =
+      _$RoutineSurveyTemplatesCopyWithImpl<$Res>;
+  $Res call(
+      {int routineSurveyTemplateId,
+      StringSingleLine createdBy,
+      DateTime createdDate,
+      StringSingleLine lastModifiedBy,
+      DateTime lastModifiedDate,
+      int active,
+      StringMultiLine comment});
+}
+
+/// @nodoc
+class _$RoutineSurveyTemplatesCopyWithImpl<$Res>
+    implements $RoutineSurveyTemplatesCopyWith<$Res> {
+  _$RoutineSurveyTemplatesCopyWithImpl(this._value, this._then);
+
+  final RoutineSurveyTemplates _value;
+  // ignore: unused_field
+  final $Res Function(RoutineSurveyTemplates) _then;
+
+  @override
+  $Res call({
+    Object? routineSurveyTemplateId = freezed,
+    Object? createdBy = freezed,
+    Object? createdDate = freezed,
+    Object? lastModifiedBy = freezed,
+    Object? lastModifiedDate = freezed,
+    Object? active = freezed,
+    Object? comment = freezed,
+  }) {
+    return _then(_value.copyWith(
+      routineSurveyTemplateId: routineSurveyTemplateId == freezed
+          ? _value.routineSurveyTemplateId
+          : routineSurveyTemplateId // ignore: cast_nullable_to_non_nullable
+              as int,
+      createdBy: createdBy == freezed
+          ? _value.createdBy
+          : createdBy // ignore: cast_nullable_to_non_nullable
+              as StringSingleLine,
+      createdDate: createdDate == freezed
+          ? _value.createdDate
+          : createdDate // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      lastModifiedBy: lastModifiedBy == freezed
+          ? _value.lastModifiedBy
+          : lastModifiedBy // ignore: cast_nullable_to_non_nullable
+              as StringSingleLine,
+      lastModifiedDate: lastModifiedDate == freezed
+          ? _value.lastModifiedDate
+          : lastModifiedDate // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      active: active == freezed
+          ? _value.active
+          : active // ignore: cast_nullable_to_non_nullable
+              as int,
+      comment: comment == freezed
+          ? _value.comment
+          : comment // ignore: cast_nullable_to_non_nullable
+              as StringMultiLine,
+    ));
+  }
+}
+
+/// @nodoc
+abstract class _$RoutineSurveyTemplatesCopyWith<$Res>
+    implements $RoutineSurveyTemplatesCopyWith<$Res> {
+  factory _$RoutineSurveyTemplatesCopyWith(_RoutineSurveyTemplates value,
+          $Res Function(_RoutineSurveyTemplates) then) =
+      __$RoutineSurveyTemplatesCopyWithImpl<$Res>;
+  @override
+  $Res call(
+      {int routineSurveyTemplateId,
+      StringSingleLine createdBy,
+      DateTime createdDate,
+      StringSingleLine lastModifiedBy,
+      DateTime lastModifiedDate,
+      int active,
+      StringMultiLine comment});
+}
+
+/// @nodoc
+class __$RoutineSurveyTemplatesCopyWithImpl<$Res>
+    extends _$RoutineSurveyTemplatesCopyWithImpl<$Res>
+    implements _$RoutineSurveyTemplatesCopyWith<$Res> {
+  __$RoutineSurveyTemplatesCopyWithImpl(_RoutineSurveyTemplates _value,
+      $Res Function(_RoutineSurveyTemplates) _then)
+      : super(_value, (v) => _then(v as _RoutineSurveyTemplates));
+
+  @override
+  _RoutineSurveyTemplates get _value => super._value as _RoutineSurveyTemplates;
+
+  @override
+  $Res call({
+    Object? routineSurveyTemplateId = freezed,
+    Object? createdBy = freezed,
+    Object? createdDate = freezed,
+    Object? lastModifiedBy = freezed,
+    Object? lastModifiedDate = freezed,
+    Object? active = freezed,
+    Object? comment = freezed,
+  }) {
+    return _then(_RoutineSurveyTemplates(
+      routineSurveyTemplateId: routineSurveyTemplateId == freezed
+          ? _value.routineSurveyTemplateId
+          : routineSurveyTemplateId // ignore: cast_nullable_to_non_nullable
+              as int,
+      createdBy: createdBy == freezed
+          ? _value.createdBy
+          : createdBy // ignore: cast_nullable_to_non_nullable
+              as StringSingleLine,
+      createdDate: createdDate == freezed
+          ? _value.createdDate
+          : createdDate // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      lastModifiedBy: lastModifiedBy == freezed
+          ? _value.lastModifiedBy
+          : lastModifiedBy // ignore: cast_nullable_to_non_nullable
+              as StringSingleLine,
+      lastModifiedDate: lastModifiedDate == freezed
+          ? _value.lastModifiedDate
+          : lastModifiedDate // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      active: active == freezed
+          ? _value.active
+          : active // ignore: cast_nullable_to_non_nullable
+              as int,
+      comment: comment == freezed
+          ? _value.comment
+          : comment // ignore: cast_nullable_to_non_nullable
+              as StringMultiLine,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$_RoutineSurveyTemplates extends _RoutineSurveyTemplates {
+  _$_RoutineSurveyTemplates(
+      {required this.routineSurveyTemplateId,
+      required this.createdBy,
+      required this.createdDate,
+      required this.lastModifiedBy,
+      required this.lastModifiedDate,
+      required this.active,
+      required this.comment})
+      : super._();
+
+  @override
+  final int routineSurveyTemplateId;
+  @override
+  final StringSingleLine createdBy;
+  @override
+  final DateTime createdDate;
+  @override
+  final StringSingleLine lastModifiedBy;
+  @override
+  final DateTime lastModifiedDate;
+  @override
+  final int active;
+  @override
+  final StringMultiLine comment;
+
+  @override
+  String toString() {
+    return 'RoutineSurveyTemplates(routineSurveyTemplateId: $routineSurveyTemplateId, createdBy: $createdBy, createdDate: $createdDate, lastModifiedBy: $lastModifiedBy, lastModifiedDate: $lastModifiedDate, active: $active, comment: $comment)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other is _RoutineSurveyTemplates &&
+            (identical(
+                    other.routineSurveyTemplateId, routineSurveyTemplateId) ||
+                const DeepCollectionEquality().equals(
+                    other.routineSurveyTemplateId, routineSurveyTemplateId)) &&
+            (identical(other.createdBy, createdBy) ||
+                const DeepCollectionEquality()
+                    .equals(other.createdBy, createdBy)) &&
+            (identical(other.createdDate, createdDate) ||
+                const DeepCollectionEquality()
+                    .equals(other.createdDate, createdDate)) &&
+            (identical(other.lastModifiedBy, lastModifiedBy) ||
+                const DeepCollectionEquality()
+                    .equals(other.lastModifiedBy, lastModifiedBy)) &&
+            (identical(other.lastModifiedDate, lastModifiedDate) ||
+                const DeepCollectionEquality()
+                    .equals(other.lastModifiedDate, lastModifiedDate)) &&
+            (identical(other.active, active) ||
+                const DeepCollectionEquality().equals(other.active, active)) &&
+            (identical(other.comment, comment) ||
+                const DeepCollectionEquality().equals(other.comment, comment)));
+  }
+
+  @override
+  int get hashCode =>
+      runtimeType.hashCode ^
+      const DeepCollectionEquality().hash(routineSurveyTemplateId) ^
+      const DeepCollectionEquality().hash(createdBy) ^
+      const DeepCollectionEquality().hash(createdDate) ^
+      const DeepCollectionEquality().hash(lastModifiedBy) ^
+      const DeepCollectionEquality().hash(lastModifiedDate) ^
+      const DeepCollectionEquality().hash(active) ^
+      const DeepCollectionEquality().hash(comment);
+
+  @JsonKey(ignore: true)
+  @override
+  _$RoutineSurveyTemplatesCopyWith<_RoutineSurveyTemplates> get copyWith =>
+      __$RoutineSurveyTemplatesCopyWithImpl<_RoutineSurveyTemplates>(
+          this, _$identity);
+}
+
+abstract class _RoutineSurveyTemplates extends RoutineSurveyTemplates {
+  factory _RoutineSurveyTemplates(
+      {required int routineSurveyTemplateId,
+      required StringSingleLine createdBy,
+      required DateTime createdDate,
+      required StringSingleLine lastModifiedBy,
+      required DateTime lastModifiedDate,
+      required int active,
+      required StringMultiLine comment}) = _$_RoutineSurveyTemplates;
+  _RoutineSurveyTemplates._() : super._();
+
+  @override
+  int get routineSurveyTemplateId => throw _privateConstructorUsedError;
+  @override
+  StringSingleLine get createdBy => throw _privateConstructorUsedError;
+  @override
+  DateTime get createdDate => throw _privateConstructorUsedError;
+  @override
+  StringSingleLine get lastModifiedBy => throw _privateConstructorUsedError;
+  @override
+  DateTime get lastModifiedDate => throw _privateConstructorUsedError;
+  @override
+  int get active => throw _privateConstructorUsedError;
+  @override
+  StringMultiLine get comment => throw _privateConstructorUsedError;
+  @override
+  @JsonKey(ignore: true)
+  _$RoutineSurveyTemplatesCopyWith<_RoutineSurveyTemplates> get copyWith =>
+      throw _privateConstructorUsedError;
+}

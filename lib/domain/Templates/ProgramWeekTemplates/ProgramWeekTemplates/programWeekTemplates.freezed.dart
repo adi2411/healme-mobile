07331cc139e2c803a332ec7a1d50a6f84dcd2cf7
aditya <aditya_jinegar@yahoo.com>
2021-08-13 12:09:26 -0400

    Create programWeekTemplates.freezed.dart

---
+++ lib/domain/Templates/ProgramWeekTemplates/ProgramWeekTemplates/programWeekTemplates.freezed.dart
@@ -0,0 +1,276 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides
+
+part of 'programWeekTemplates.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more informations: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+class _$ProgramWeekTemplatesTearOff {
+  const _$ProgramWeekTemplatesTearOff();
+
+  _ProgramWeekTemplates call(
+      {required int programWeekTemplateId,
+      required StringSingleLine createdBy,
+      required DateTime createdDate,
+      required StringSingleLine lastModifiedBy,
+      required DateTime lastModifiedDate,
+      required int active}) {
+    return _ProgramWeekTemplates(
+      programWeekTemplateId: programWeekTemplateId,
+      createdBy: createdBy,
+      createdDate: createdDate,
+      lastModifiedBy: lastModifiedBy,
+      lastModifiedDate: lastModifiedDate,
+      active: active,
+    );
+  }
+}
+
+/// @nodoc
+const $ProgramWeekTemplates = _$ProgramWeekTemplatesTearOff();
+
+/// @nodoc
+mixin _$ProgramWeekTemplates {
+  int get programWeekTemplateId => throw _privateConstructorUsedError;
+  StringSingleLine get createdBy => throw _privateConstructorUsedError;
+  DateTime get createdDate => throw _privateConstructorUsedError;
+  StringSingleLine get lastModifiedBy => throw _privateConstructorUsedError;
+  DateTime get lastModifiedDate => throw _privateConstructorUsedError;
+  int get active => throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $ProgramWeekTemplatesCopyWith<ProgramWeekTemplates> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $ProgramWeekTemplatesCopyWith<$Res> {
+  factory $ProgramWeekTemplatesCopyWith(ProgramWeekTemplates value,
+          $Res Function(ProgramWeekTemplates) then) =
+      _$ProgramWeekTemplatesCopyWithImpl<$Res>;
+  $Res call(
+      {int programWeekTemplateId,
+      StringSingleLine createdBy,
+      DateTime createdDate,
+      StringSingleLine lastModifiedBy,
+      DateTime lastModifiedDate,
+      int active});
+}
+
+/// @nodoc
+class _$ProgramWeekTemplatesCopyWithImpl<$Res>
+    implements $ProgramWeekTemplatesCopyWith<$Res> {
+  _$ProgramWeekTemplatesCopyWithImpl(this._value, this._then);
+
+  final ProgramWeekTemplates _value;
+  // ignore: unused_field
+  final $Res Function(ProgramWeekTemplates) _then;
+
+  @override
+  $Res call({
+    Object? programWeekTemplateId = freezed,
+    Object? createdBy = freezed,
+    Object? createdDate = freezed,
+    Object? lastModifiedBy = freezed,
+    Object? lastModifiedDate = freezed,
+    Object? active = freezed,
+  }) {
+    return _then(_value.copyWith(
+      programWeekTemplateId: programWeekTemplateId == freezed
+          ? _value.programWeekTemplateId
+          : programWeekTemplateId // ignore: cast_nullable_to_non_nullable
+              as int,
+      createdBy: createdBy == freezed
+          ? _value.createdBy
+          : createdBy // ignore: cast_nullable_to_non_nullable
+              as StringSingleLine,
+      createdDate: createdDate == freezed
+          ? _value.createdDate
+          : createdDate // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      lastModifiedBy: lastModifiedBy == freezed
+          ? _value.lastModifiedBy
+          : lastModifiedBy // ignore: cast_nullable_to_non_nullable
+              as StringSingleLine,
+      lastModifiedDate: lastModifiedDate == freezed
+          ? _value.lastModifiedDate
+          : lastModifiedDate // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      active: active == freezed
+          ? _value.active
+          : active // ignore: cast_nullable_to_non_nullable
+              as int,
+    ));
+  }
+}
+
+/// @nodoc
+abstract class _$ProgramWeekTemplatesCopyWith<$Res>
+    implements $ProgramWeekTemplatesCopyWith<$Res> {
+  factory _$ProgramWeekTemplatesCopyWith(_ProgramWeekTemplates value,
+          $Res Function(_ProgramWeekTemplates) then) =
+      __$ProgramWeekTemplatesCopyWithImpl<$Res>;
+  @override
+  $Res call(
+      {int programWeekTemplateId,
+      StringSingleLine createdBy,
+      DateTime createdDate,
+      StringSingleLine lastModifiedBy,
+      DateTime lastModifiedDate,
+      int active});
+}
+
+/// @nodoc
+class __$ProgramWeekTemplatesCopyWithImpl<$Res>
+    extends _$ProgramWeekTemplatesCopyWithImpl<$Res>
+    implements _$ProgramWeekTemplatesCopyWith<$Res> {
+  __$ProgramWeekTemplatesCopyWithImpl(
+      _ProgramWeekTemplates _value, $Res Function(_ProgramWeekTemplates) _then)
+      : super(_value, (v) => _then(v as _ProgramWeekTemplates));
+
+  @override
+  _ProgramWeekTemplates get _value => super._value as _ProgramWeekTemplates;
+
+  @override
+  $Res call({
+    Object? programWeekTemplateId = freezed,
+    Object? createdBy = freezed,
+    Object? createdDate = freezed,
+    Object? lastModifiedBy = freezed,
+    Object? lastModifiedDate = freezed,
+    Object? active = freezed,
+  }) {
+    return _then(_ProgramWeekTemplates(
+      programWeekTemplateId: programWeekTemplateId == freezed
+          ? _value.programWeekTemplateId
+          : programWeekTemplateId // ignore: cast_nullable_to_non_nullable
+              as int,
+      createdBy: createdBy == freezed
+          ? _value.createdBy
+          : createdBy // ignore: cast_nullable_to_non_nullable
+              as StringSingleLine,
+      createdDate: createdDate == freezed
+          ? _value.createdDate
+          : createdDate // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      lastModifiedBy: lastModifiedBy == freezed
+          ? _value.lastModifiedBy
+          : lastModifiedBy // ignore: cast_nullable_to_non_nullable
+              as StringSingleLine,
+      lastModifiedDate: lastModifiedDate == freezed
+          ? _value.lastModifiedDate
+          : lastModifiedDate // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      active: active == freezed
+          ? _value.active
+          : active // ignore: cast_nullable_to_non_nullable
+              as int,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$_ProgramWeekTemplates extends _ProgramWeekTemplates {
+  _$_ProgramWeekTemplates(
+      {required this.programWeekTemplateId,
+      required this.createdBy,
+      required this.createdDate,
+      required this.lastModifiedBy,
+      required this.lastModifiedDate,
+      required this.active})
+      : super._();
+
+  @override
+  final int programWeekTemplateId;
+  @override
+  final StringSingleLine createdBy;
+  @override
+  final DateTime createdDate;
+  @override
+  final StringSingleLine lastModifiedBy;
+  @override
+  final DateTime lastModifiedDate;
+  @override
+  final int active;
+
+  @override
+  String toString() {
+    return 'ProgramWeekTemplates(programWeekTemplateId: $programWeekTemplateId, createdBy: $createdBy, createdDate: $createdDate, lastModifiedBy: $lastModifiedBy, lastModifiedDate: $lastModifiedDate, active: $active)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other is _ProgramWeekTemplates &&
+            (identical(other.programWeekTemplateId, programWeekTemplateId) ||
+                const DeepCollectionEquality().equals(
+                    other.programWeekTemplateId, programWeekTemplateId)) &&
+            (identical(other.createdBy, createdBy) ||
+                const DeepCollectionEquality()
+                    .equals(other.createdBy, createdBy)) &&
+            (identical(other.createdDate, createdDate) ||
+                const DeepCollectionEquality()
+                    .equals(other.createdDate, createdDate)) &&
+            (identical(other.lastModifiedBy, lastModifiedBy) ||
+                const DeepCollectionEquality()
+                    .equals(other.lastModifiedBy, lastModifiedBy)) &&
+            (identical(other.lastModifiedDate, lastModifiedDate) ||
+                const DeepCollectionEquality()
+                    .equals(other.lastModifiedDate, lastModifiedDate)) &&
+            (identical(other.active, active) ||
+                const DeepCollectionEquality().equals(other.active, active)));
+  }
+
+  @override
+  int get hashCode =>
+      runtimeType.hashCode ^
+      const DeepCollectionEquality().hash(programWeekTemplateId) ^
+      const DeepCollectionEquality().hash(createdBy) ^
+      const DeepCollectionEquality().hash(createdDate) ^
+      const DeepCollectionEquality().hash(lastModifiedBy) ^
+      const DeepCollectionEquality().hash(lastModifiedDate) ^
+      const DeepCollectionEquality().hash(active);
+
+  @JsonKey(ignore: true)
+  @override
+  _$ProgramWeekTemplatesCopyWith<_ProgramWeekTemplates> get copyWith =>
+      __$ProgramWeekTemplatesCopyWithImpl<_ProgramWeekTemplates>(
+          this, _$identity);
+}
+
+abstract class _ProgramWeekTemplates extends ProgramWeekTemplates {
+  factory _ProgramWeekTemplates(
+      {required int programWeekTemplateId,
+      required StringSingleLine createdBy,
+      required DateTime createdDate,
+      required StringSingleLine lastModifiedBy,
+      required DateTime lastModifiedDate,
+      required int active}) = _$_ProgramWeekTemplates;
+  _ProgramWeekTemplates._() : super._();
+
+  @override
+  int get programWeekTemplateId => throw _privateConstructorUsedError;
+  @override
+  StringSingleLine get createdBy => throw _privateConstructorUsedError;
+  @override
+  DateTime get createdDate => throw _privateConstructorUsedError;
+  @override
+  StringSingleLine get lastModifiedBy => throw _privateConstructorUsedError;
+  @override
+  DateTime get lastModifiedDate => throw _privateConstructorUsedError;
+  @override
+  int get active => throw _privateConstructorUsedError;
+  @override
+  @JsonKey(ignore: true)
+  _$ProgramWeekTemplatesCopyWith<_ProgramWeekTemplates> get copyWith =>
+      throw _privateConstructorUsedError;
+}

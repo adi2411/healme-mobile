28354965a3c5d8aee40bac7e3cdb83866a8c079a
aditya <aditya_jinegar@yahoo.com>
2021-08-13 12:08:47 -0400

    Create mindfulEnergyTemplates.freezed.dart

---
+++ lib/domain/Templates/MindfulEnergys/MindfulEnergyTemplates/mindfulEnergyTemplates.freezed.dart
@@ -0,0 +1,381 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides
+
+part of 'mindfulEnergyTemplates.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more informations: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+class _$MindfulEnergyTemplatesTearOff {
+  const _$MindfulEnergyTemplatesTearOff();
+
+  _MindfulEnergyTemplates call(
+      {required int mindfulEnergyTemplateId,
+      required StringSingleLine createdBy,
+      required DateTime createdDate,
+      required StringSingleLine lastModifiedBy,
+      required DateTime lastModifiedDate,
+      required int active,
+      required StringMultiLine additionalInstructions,
+      required int groupSessions,
+      required int independentMeditationSessions,
+      required int independentSessions}) {
+    return _MindfulEnergyTemplates(
+      mindfulEnergyTemplateId: mindfulEnergyTemplateId,
+      createdBy: createdBy,
+      createdDate: createdDate,
+      lastModifiedBy: lastModifiedBy,
+      lastModifiedDate: lastModifiedDate,
+      active: active,
+      additionalInstructions: additionalInstructions,
+      groupSessions: groupSessions,
+      independentMeditationSessions: independentMeditationSessions,
+      independentSessions: independentSessions,
+    );
+  }
+}
+
+/// @nodoc
+const $MindfulEnergyTemplates = _$MindfulEnergyTemplatesTearOff();
+
+/// @nodoc
+mixin _$MindfulEnergyTemplates {
+  int get mindfulEnergyTemplateId => throw _privateConstructorUsedError;
+  StringSingleLine get createdBy => throw _privateConstructorUsedError;
+  DateTime get createdDate => throw _privateConstructorUsedError;
+  StringSingleLine get lastModifiedBy => throw _privateConstructorUsedError;
+  DateTime get lastModifiedDate => throw _privateConstructorUsedError;
+  int get active => throw _privateConstructorUsedError;
+  StringMultiLine get additionalInstructions =>
+      throw _privateConstructorUsedError;
+  int get groupSessions => throw _privateConstructorUsedError;
+  int get independentMeditationSessions => throw _privateConstructorUsedError;
+  int get independentSessions => throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $MindfulEnergyTemplatesCopyWith<MindfulEnergyTemplates> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $MindfulEnergyTemplatesCopyWith<$Res> {
+  factory $MindfulEnergyTemplatesCopyWith(MindfulEnergyTemplates value,
+          $Res Function(MindfulEnergyTemplates) then) =
+      _$MindfulEnergyTemplatesCopyWithImpl<$Res>;
+  $Res call(
+      {int mindfulEnergyTemplateId,
+      StringSingleLine createdBy,
+      DateTime createdDate,
+      StringSingleLine lastModifiedBy,
+      DateTime lastModifiedDate,
+      int active,
+      StringMultiLine additionalInstructions,
+      int groupSessions,
+      int independentMeditationSessions,
+      int independentSessions});
+}
+
+/// @nodoc
+class _$MindfulEnergyTemplatesCopyWithImpl<$Res>
+    implements $MindfulEnergyTemplatesCopyWith<$Res> {
+  _$MindfulEnergyTemplatesCopyWithImpl(this._value, this._then);
+
+  final MindfulEnergyTemplates _value;
+  // ignore: unused_field
+  final $Res Function(MindfulEnergyTemplates) _then;
+
+  @override
+  $Res call({
+    Object? mindfulEnergyTemplateId = freezed,
+    Object? createdBy = freezed,
+    Object? createdDate = freezed,
+    Object? lastModifiedBy = freezed,
+    Object? lastModifiedDate = freezed,
+    Object? active = freezed,
+    Object? additionalInstructions = freezed,
+    Object? groupSessions = freezed,
+    Object? independentMeditationSessions = freezed,
+    Object? independentSessions = freezed,
+  }) {
+    return _then(_value.copyWith(
+      mindfulEnergyTemplateId: mindfulEnergyTemplateId == freezed
+          ? _value.mindfulEnergyTemplateId
+          : mindfulEnergyTemplateId // ignore: cast_nullable_to_non_nullable
+              as int,
+      createdBy: createdBy == freezed
+          ? _value.createdBy
+          : createdBy // ignore: cast_nullable_to_non_nullable
+              as StringSingleLine,
+      createdDate: createdDate == freezed
+          ? _value.createdDate
+          : createdDate // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      lastModifiedBy: lastModifiedBy == freezed
+          ? _value.lastModifiedBy
+          : lastModifiedBy // ignore: cast_nullable_to_non_nullable
+              as StringSingleLine,
+      lastModifiedDate: lastModifiedDate == freezed
+          ? _value.lastModifiedDate
+          : lastModifiedDate // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      active: active == freezed
+          ? _value.active
+          : active // ignore: cast_nullable_to_non_nullable
+              as int,
+      additionalInstructions: additionalInstructions == freezed
+          ? _value.additionalInstructions
+          : additionalInstructions // ignore: cast_nullable_to_non_nullable
+              as StringMultiLine,
+      groupSessions: groupSessions == freezed
+          ? _value.groupSessions
+          : groupSessions // ignore: cast_nullable_to_non_nullable
+              as int,
+      independentMeditationSessions: independentMeditationSessions == freezed
+          ? _value.independentMeditationSessions
+          : independentMeditationSessions // ignore: cast_nullable_to_non_nullable
+              as int,
+      independentSessions: independentSessions == freezed
+          ? _value.independentSessions
+          : independentSessions // ignore: cast_nullable_to_non_nullable
+              as int,
+    ));
+  }
+}
+
+/// @nodoc
+abstract class _$MindfulEnergyTemplatesCopyWith<$Res>
+    implements $MindfulEnergyTemplatesCopyWith<$Res> {
+  factory _$MindfulEnergyTemplatesCopyWith(_MindfulEnergyTemplates value,
+          $Res Function(_MindfulEnergyTemplates) then) =
+      __$MindfulEnergyTemplatesCopyWithImpl<$Res>;
+  @override
+  $Res call(
+      {int mindfulEnergyTemplateId,
+      StringSingleLine createdBy,
+      DateTime createdDate,
+      StringSingleLine lastModifiedBy,
+      DateTime lastModifiedDate,
+      int active,
+      StringMultiLine additionalInstructions,
+      int groupSessions,
+      int independentMeditationSessions,
+      int independentSessions});
+}
+
+/// @nodoc
+class __$MindfulEnergyTemplatesCopyWithImpl<$Res>
+    extends _$MindfulEnergyTemplatesCopyWithImpl<$Res>
+    implements _$MindfulEnergyTemplatesCopyWith<$Res> {
+  __$MindfulEnergyTemplatesCopyWithImpl(_MindfulEnergyTemplates _value,
+      $Res Function(_MindfulEnergyTemplates) _then)
+      : super(_value, (v) => _then(v as _MindfulEnergyTemplates));
+
+  @override
+  _MindfulEnergyTemplates get _value => super._value as _MindfulEnergyTemplates;
+
+  @override
+  $Res call({
+    Object? mindfulEnergyTemplateId = freezed,
+    Object? createdBy = freezed,
+    Object? createdDate = freezed,
+    Object? lastModifiedBy = freezed,
+    Object? lastModifiedDate = freezed,
+    Object? active = freezed,
+    Object? additionalInstructions = freezed,
+    Object? groupSessions = freezed,
+    Object? independentMeditationSessions = freezed,
+    Object? independentSessions = freezed,
+  }) {
+    return _then(_MindfulEnergyTemplates(
+      mindfulEnergyTemplateId: mindfulEnergyTemplateId == freezed
+          ? _value.mindfulEnergyTemplateId
+          : mindfulEnergyTemplateId // ignore: cast_nullable_to_non_nullable
+              as int,
+      createdBy: createdBy == freezed
+          ? _value.createdBy
+          : createdBy // ignore: cast_nullable_to_non_nullable
+              as StringSingleLine,
+      createdDate: createdDate == freezed
+          ? _value.createdDate
+          : createdDate // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      lastModifiedBy: lastModifiedBy == freezed
+          ? _value.lastModifiedBy
+          : lastModifiedBy // ignore: cast_nullable_to_non_nullable
+              as StringSingleLine,
+      lastModifiedDate: lastModifiedDate == freezed
+          ? _value.lastModifiedDate
+          : lastModifiedDate // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      active: active == freezed
+          ? _value.active
+          : active // ignore: cast_nullable_to_non_nullable
+              as int,
+      additionalInstructions: additionalInstructions == freezed
+          ? _value.additionalInstructions
+          : additionalInstructions // ignore: cast_nullable_to_non_nullable
+              as StringMultiLine,
+      groupSessions: groupSessions == freezed
+          ? _value.groupSessions
+          : groupSessions // ignore: cast_nullable_to_non_nullable
+              as int,
+      independentMeditationSessions: independentMeditationSessions == freezed
+          ? _value.independentMeditationSessions
+          : independentMeditationSessions // ignore: cast_nullable_to_non_nullable
+              as int,
+      independentSessions: independentSessions == freezed
+          ? _value.independentSessions
+          : independentSessions // ignore: cast_nullable_to_non_nullable
+              as int,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$_MindfulEnergyTemplates extends _MindfulEnergyTemplates {
+  _$_MindfulEnergyTemplates(
+      {required this.mindfulEnergyTemplateId,
+      required this.createdBy,
+      required this.createdDate,
+      required this.lastModifiedBy,
+      required this.lastModifiedDate,
+      required this.active,
+      required this.additionalInstructions,
+      required this.groupSessions,
+      required this.independentMeditationSessions,
+      required this.independentSessions})
+      : super._();
+
+  @override
+  final int mindfulEnergyTemplateId;
+  @override
+  final StringSingleLine createdBy;
+  @override
+  final DateTime createdDate;
+  @override
+  final StringSingleLine lastModifiedBy;
+  @override
+  final DateTime lastModifiedDate;
+  @override
+  final int active;
+  @override
+  final StringMultiLine additionalInstructions;
+  @override
+  final int groupSessions;
+  @override
+  final int independentMeditationSessions;
+  @override
+  final int independentSessions;
+
+  @override
+  String toString() {
+    return 'MindfulEnergyTemplates(mindfulEnergyTemplateId: $mindfulEnergyTemplateId, createdBy: $createdBy, createdDate: $createdDate, lastModifiedBy: $lastModifiedBy, lastModifiedDate: $lastModifiedDate, active: $active, additionalInstructions: $additionalInstructions, groupSessions: $groupSessions, independentMeditationSessions: $independentMeditationSessions, independentSessions: $independentSessions)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other is _MindfulEnergyTemplates &&
+            (identical(
+                    other.mindfulEnergyTemplateId, mindfulEnergyTemplateId) ||
+                const DeepCollectionEquality().equals(
+                    other.mindfulEnergyTemplateId, mindfulEnergyTemplateId)) &&
+            (identical(other.createdBy, createdBy) ||
+                const DeepCollectionEquality()
+                    .equals(other.createdBy, createdBy)) &&
+            (identical(other.createdDate, createdDate) ||
+                const DeepCollectionEquality()
+                    .equals(other.createdDate, createdDate)) &&
+            (identical(other.lastModifiedBy, lastModifiedBy) ||
+                const DeepCollectionEquality()
+                    .equals(other.lastModifiedBy, lastModifiedBy)) &&
+            (identical(other.lastModifiedDate, lastModifiedDate) ||
+                const DeepCollectionEquality()
+                    .equals(other.lastModifiedDate, lastModifiedDate)) &&
+            (identical(other.active, active) ||
+                const DeepCollectionEquality().equals(other.active, active)) &&
+            (identical(other.additionalInstructions, additionalInstructions) ||
+                const DeepCollectionEquality().equals(
+                    other.additionalInstructions, additionalInstructions)) &&
+            (identical(other.groupSessions, groupSessions) ||
+                const DeepCollectionEquality()
+                    .equals(other.groupSessions, groupSessions)) &&
+            (identical(other.independentMeditationSessions,
+                    independentMeditationSessions) ||
+                const DeepCollectionEquality().equals(
+                    other.independentMeditationSessions,
+                    independentMeditationSessions)) &&
+            (identical(other.independentSessions, independentSessions) ||
+                const DeepCollectionEquality()
+                    .equals(other.independentSessions, independentSessions)));
+  }
+
+  @override
+  int get hashCode =>
+      runtimeType.hashCode ^
+      const DeepCollectionEquality().hash(mindfulEnergyTemplateId) ^
+      const DeepCollectionEquality().hash(createdBy) ^
+      const DeepCollectionEquality().hash(createdDate) ^
+      const DeepCollectionEquality().hash(lastModifiedBy) ^
+      const DeepCollectionEquality().hash(lastModifiedDate) ^
+      const DeepCollectionEquality().hash(active) ^
+      const DeepCollectionEquality().hash(additionalInstructions) ^
+      const DeepCollectionEquality().hash(groupSessions) ^
+      const DeepCollectionEquality().hash(independentMeditationSessions) ^
+      const DeepCollectionEquality().hash(independentSessions);
+
+  @JsonKey(ignore: true)
+  @override
+  _$MindfulEnergyTemplatesCopyWith<_MindfulEnergyTemplates> get copyWith =>
+      __$MindfulEnergyTemplatesCopyWithImpl<_MindfulEnergyTemplates>(
+          this, _$identity);
+}
+
+abstract class _MindfulEnergyTemplates extends MindfulEnergyTemplates {
+  factory _MindfulEnergyTemplates(
+      {required int mindfulEnergyTemplateId,
+      required StringSingleLine createdBy,
+      required DateTime createdDate,
+      required StringSingleLine lastModifiedBy,
+      required DateTime lastModifiedDate,
+      required int active,
+      required StringMultiLine additionalInstructions,
+      required int groupSessions,
+      required int independentMeditationSessions,
+      required int independentSessions}) = _$_MindfulEnergyTemplates;
+  _MindfulEnergyTemplates._() : super._();
+
+  @override
+  int get mindfulEnergyTemplateId => throw _privateConstructorUsedError;
+  @override
+  StringSingleLine get createdBy => throw _privateConstructorUsedError;
+  @override
+  DateTime get createdDate => throw _privateConstructorUsedError;
+  @override
+  StringSingleLine get lastModifiedBy => throw _privateConstructorUsedError;
+  @override
+  DateTime get lastModifiedDate => throw _privateConstructorUsedError;
+  @override
+  int get active => throw _privateConstructorUsedError;
+  @override
+  StringMultiLine get additionalInstructions =>
+      throw _privateConstructorUsedError;
+  @override
+  int get groupSessions => throw _privateConstructorUsedError;
+  @override
+  int get independentMeditationSessions => throw _privateConstructorUsedError;
+  @override
+  int get independentSessions => throw _privateConstructorUsedError;
+  @override
+  @JsonKey(ignore: true)
+  _$MindfulEnergyTemplatesCopyWith<_MindfulEnergyTemplates> get copyWith =>
+      throw _privateConstructorUsedError;
+}

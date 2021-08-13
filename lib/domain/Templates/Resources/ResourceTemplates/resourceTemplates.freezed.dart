26f308e33d15dbe7aa73f35f84d6b493c9a1fbe3
aditya <aditya_jinegar@yahoo.com>
2021-08-13 13:53:08 -0400

    Create resourceTemplates.freezed.dart

---
+++ lib/domain/Templates/Resources/ResourceTemplates/resourceTemplates.freezed.dart
@@ -0,0 +1,275 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides
+
+part of 'resourceTemplates.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more informations: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+class _$ResourceTemplatesTearOff {
+  const _$ResourceTemplatesTearOff();
+
+  _ResourceTemplates call(
+      {required int resourceTemplateId,
+      required StringSingleLine createdBy,
+      required DateTime createdDate,
+      required StringSingleLine lastModifiedBy,
+      required DateTime lastModifiedDate,
+      required int active}) {
+    return _ResourceTemplates(
+      resourceTemplateId: resourceTemplateId,
+      createdBy: createdBy,
+      createdDate: createdDate,
+      lastModifiedBy: lastModifiedBy,
+      lastModifiedDate: lastModifiedDate,
+      active: active,
+    );
+  }
+}
+
+/// @nodoc
+const $ResourceTemplates = _$ResourceTemplatesTearOff();
+
+/// @nodoc
+mixin _$ResourceTemplates {
+  int get resourceTemplateId => throw _privateConstructorUsedError;
+  StringSingleLine get createdBy => throw _privateConstructorUsedError;
+  DateTime get createdDate => throw _privateConstructorUsedError;
+  StringSingleLine get lastModifiedBy => throw _privateConstructorUsedError;
+  DateTime get lastModifiedDate => throw _privateConstructorUsedError;
+  int get active => throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $ResourceTemplatesCopyWith<ResourceTemplates> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $ResourceTemplatesCopyWith<$Res> {
+  factory $ResourceTemplatesCopyWith(
+          ResourceTemplates value, $Res Function(ResourceTemplates) then) =
+      _$ResourceTemplatesCopyWithImpl<$Res>;
+  $Res call(
+      {int resourceTemplateId,
+      StringSingleLine createdBy,
+      DateTime createdDate,
+      StringSingleLine lastModifiedBy,
+      DateTime lastModifiedDate,
+      int active});
+}
+
+/// @nodoc
+class _$ResourceTemplatesCopyWithImpl<$Res>
+    implements $ResourceTemplatesCopyWith<$Res> {
+  _$ResourceTemplatesCopyWithImpl(this._value, this._then);
+
+  final ResourceTemplates _value;
+  // ignore: unused_field
+  final $Res Function(ResourceTemplates) _then;
+
+  @override
+  $Res call({
+    Object? resourceTemplateId = freezed,
+    Object? createdBy = freezed,
+    Object? createdDate = freezed,
+    Object? lastModifiedBy = freezed,
+    Object? lastModifiedDate = freezed,
+    Object? active = freezed,
+  }) {
+    return _then(_value.copyWith(
+      resourceTemplateId: resourceTemplateId == freezed
+          ? _value.resourceTemplateId
+          : resourceTemplateId // ignore: cast_nullable_to_non_nullable
+              as int,
+      createdBy: createdBy == freezed
+          ? _value.createdBy
+          : createdBy // ignore: cast_nullable_to_non_nullable
+              as StringSingleLine,
+      createdDate: createdDate == freezed
+          ? _value.createdDate
+          : createdDate // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      lastModifiedBy: lastModifiedBy == freezed
+          ? _value.lastModifiedBy
+          : lastModifiedBy // ignore: cast_nullable_to_non_nullable
+              as StringSingleLine,
+      lastModifiedDate: lastModifiedDate == freezed
+          ? _value.lastModifiedDate
+          : lastModifiedDate // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      active: active == freezed
+          ? _value.active
+          : active // ignore: cast_nullable_to_non_nullable
+              as int,
+    ));
+  }
+}
+
+/// @nodoc
+abstract class _$ResourceTemplatesCopyWith<$Res>
+    implements $ResourceTemplatesCopyWith<$Res> {
+  factory _$ResourceTemplatesCopyWith(
+          _ResourceTemplates value, $Res Function(_ResourceTemplates) then) =
+      __$ResourceTemplatesCopyWithImpl<$Res>;
+  @override
+  $Res call(
+      {int resourceTemplateId,
+      StringSingleLine createdBy,
+      DateTime createdDate,
+      StringSingleLine lastModifiedBy,
+      DateTime lastModifiedDate,
+      int active});
+}
+
+/// @nodoc
+class __$ResourceTemplatesCopyWithImpl<$Res>
+    extends _$ResourceTemplatesCopyWithImpl<$Res>
+    implements _$ResourceTemplatesCopyWith<$Res> {
+  __$ResourceTemplatesCopyWithImpl(
+      _ResourceTemplates _value, $Res Function(_ResourceTemplates) _then)
+      : super(_value, (v) => _then(v as _ResourceTemplates));
+
+  @override
+  _ResourceTemplates get _value => super._value as _ResourceTemplates;
+
+  @override
+  $Res call({
+    Object? resourceTemplateId = freezed,
+    Object? createdBy = freezed,
+    Object? createdDate = freezed,
+    Object? lastModifiedBy = freezed,
+    Object? lastModifiedDate = freezed,
+    Object? active = freezed,
+  }) {
+    return _then(_ResourceTemplates(
+      resourceTemplateId: resourceTemplateId == freezed
+          ? _value.resourceTemplateId
+          : resourceTemplateId // ignore: cast_nullable_to_non_nullable
+              as int,
+      createdBy: createdBy == freezed
+          ? _value.createdBy
+          : createdBy // ignore: cast_nullable_to_non_nullable
+              as StringSingleLine,
+      createdDate: createdDate == freezed
+          ? _value.createdDate
+          : createdDate // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      lastModifiedBy: lastModifiedBy == freezed
+          ? _value.lastModifiedBy
+          : lastModifiedBy // ignore: cast_nullable_to_non_nullable
+              as StringSingleLine,
+      lastModifiedDate: lastModifiedDate == freezed
+          ? _value.lastModifiedDate
+          : lastModifiedDate // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      active: active == freezed
+          ? _value.active
+          : active // ignore: cast_nullable_to_non_nullable
+              as int,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$_ResourceTemplates extends _ResourceTemplates {
+  _$_ResourceTemplates(
+      {required this.resourceTemplateId,
+      required this.createdBy,
+      required this.createdDate,
+      required this.lastModifiedBy,
+      required this.lastModifiedDate,
+      required this.active})
+      : super._();
+
+  @override
+  final int resourceTemplateId;
+  @override
+  final StringSingleLine createdBy;
+  @override
+  final DateTime createdDate;
+  @override
+  final StringSingleLine lastModifiedBy;
+  @override
+  final DateTime lastModifiedDate;
+  @override
+  final int active;
+
+  @override
+  String toString() {
+    return 'ResourceTemplates(resourceTemplateId: $resourceTemplateId, createdBy: $createdBy, createdDate: $createdDate, lastModifiedBy: $lastModifiedBy, lastModifiedDate: $lastModifiedDate, active: $active)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other is _ResourceTemplates &&
+            (identical(other.resourceTemplateId, resourceTemplateId) ||
+                const DeepCollectionEquality()
+                    .equals(other.resourceTemplateId, resourceTemplateId)) &&
+            (identical(other.createdBy, createdBy) ||
+                const DeepCollectionEquality()
+                    .equals(other.createdBy, createdBy)) &&
+            (identical(other.createdDate, createdDate) ||
+                const DeepCollectionEquality()
+                    .equals(other.createdDate, createdDate)) &&
+            (identical(other.lastModifiedBy, lastModifiedBy) ||
+                const DeepCollectionEquality()
+                    .equals(other.lastModifiedBy, lastModifiedBy)) &&
+            (identical(other.lastModifiedDate, lastModifiedDate) ||
+                const DeepCollectionEquality()
+                    .equals(other.lastModifiedDate, lastModifiedDate)) &&
+            (identical(other.active, active) ||
+                const DeepCollectionEquality().equals(other.active, active)));
+  }
+
+  @override
+  int get hashCode =>
+      runtimeType.hashCode ^
+      const DeepCollectionEquality().hash(resourceTemplateId) ^
+      const DeepCollectionEquality().hash(createdBy) ^
+      const DeepCollectionEquality().hash(createdDate) ^
+      const DeepCollectionEquality().hash(lastModifiedBy) ^
+      const DeepCollectionEquality().hash(lastModifiedDate) ^
+      const DeepCollectionEquality().hash(active);
+
+  @JsonKey(ignore: true)
+  @override
+  _$ResourceTemplatesCopyWith<_ResourceTemplates> get copyWith =>
+      __$ResourceTemplatesCopyWithImpl<_ResourceTemplates>(this, _$identity);
+}
+
+abstract class _ResourceTemplates extends ResourceTemplates {
+  factory _ResourceTemplates(
+      {required int resourceTemplateId,
+      required StringSingleLine createdBy,
+      required DateTime createdDate,
+      required StringSingleLine lastModifiedBy,
+      required DateTime lastModifiedDate,
+      required int active}) = _$_ResourceTemplates;
+  _ResourceTemplates._() : super._();
+
+  @override
+  int get resourceTemplateId => throw _privateConstructorUsedError;
+  @override
+  StringSingleLine get createdBy => throw _privateConstructorUsedError;
+  @override
+  DateTime get createdDate => throw _privateConstructorUsedError;
+  @override
+  StringSingleLine get lastModifiedBy => throw _privateConstructorUsedError;
+  @override
+  DateTime get lastModifiedDate => throw _privateConstructorUsedError;
+  @override
+  int get active => throw _privateConstructorUsedError;
+  @override
+  @JsonKey(ignore: true)
+  _$ResourceTemplatesCopyWith<_ResourceTemplates> get copyWith =>
+      throw _privateConstructorUsedError;
+}

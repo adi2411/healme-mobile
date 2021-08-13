9997bc3b63bf9045a4e75fd7fe78a246ab45a506
aditya <aditya_jinegar@yahoo.com>
2021-08-13 13:53:04 -0400

    Create mindfulEnergyTemplatesRoutines.freezed.dart

---
+++ lib/domain/Templates/Resources/MindfulEnergyTemplatesRoutines/mindfulEnergyTemplatesRoutines.freezed.dart
@@ -0,0 +1,195 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides
+
+part of 'mindfulEnergyTemplatesRoutines.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more informations: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+class _$MindfulEnergyTemplatesRoutinesTearOff {
+  const _$MindfulEnergyTemplatesRoutinesTearOff();
+
+  _MindfulEnergyTemplatesRoutines call(
+      {required int mindfulEnergyTemplateMindfulEnergyTemplateId,
+      required int routinesRoutineTemplateId}) {
+    return _MindfulEnergyTemplatesRoutines(
+      mindfulEnergyTemplateMindfulEnergyTemplateId:
+          mindfulEnergyTemplateMindfulEnergyTemplateId,
+      routinesRoutineTemplateId: routinesRoutineTemplateId,
+    );
+  }
+}
+
+/// @nodoc
+const $MindfulEnergyTemplatesRoutines =
+    _$MindfulEnergyTemplatesRoutinesTearOff();
+
+/// @nodoc
+mixin _$MindfulEnergyTemplatesRoutines {
+  int get mindfulEnergyTemplateMindfulEnergyTemplateId =>
+      throw _privateConstructorUsedError;
+  int get routinesRoutineTemplateId => throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $MindfulEnergyTemplatesRoutinesCopyWith<MindfulEnergyTemplatesRoutines>
+      get copyWith => throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $MindfulEnergyTemplatesRoutinesCopyWith<$Res> {
+  factory $MindfulEnergyTemplatesRoutinesCopyWith(
+          MindfulEnergyTemplatesRoutines value,
+          $Res Function(MindfulEnergyTemplatesRoutines) then) =
+      _$MindfulEnergyTemplatesRoutinesCopyWithImpl<$Res>;
+  $Res call(
+      {int mindfulEnergyTemplateMindfulEnergyTemplateId,
+      int routinesRoutineTemplateId});
+}
+
+/// @nodoc
+class _$MindfulEnergyTemplatesRoutinesCopyWithImpl<$Res>
+    implements $MindfulEnergyTemplatesRoutinesCopyWith<$Res> {
+  _$MindfulEnergyTemplatesRoutinesCopyWithImpl(this._value, this._then);
+
+  final MindfulEnergyTemplatesRoutines _value;
+  // ignore: unused_field
+  final $Res Function(MindfulEnergyTemplatesRoutines) _then;
+
+  @override
+  $Res call({
+    Object? mindfulEnergyTemplateMindfulEnergyTemplateId = freezed,
+    Object? routinesRoutineTemplateId = freezed,
+  }) {
+    return _then(_value.copyWith(
+      mindfulEnergyTemplateMindfulEnergyTemplateId:
+          mindfulEnergyTemplateMindfulEnergyTemplateId == freezed
+              ? _value.mindfulEnergyTemplateMindfulEnergyTemplateId
+              : mindfulEnergyTemplateMindfulEnergyTemplateId // ignore: cast_nullable_to_non_nullable
+                  as int,
+      routinesRoutineTemplateId: routinesRoutineTemplateId == freezed
+          ? _value.routinesRoutineTemplateId
+          : routinesRoutineTemplateId // ignore: cast_nullable_to_non_nullable
+              as int,
+    ));
+  }
+}
+
+/// @nodoc
+abstract class _$MindfulEnergyTemplatesRoutinesCopyWith<$Res>
+    implements $MindfulEnergyTemplatesRoutinesCopyWith<$Res> {
+  factory _$MindfulEnergyTemplatesRoutinesCopyWith(
+          _MindfulEnergyTemplatesRoutines value,
+          $Res Function(_MindfulEnergyTemplatesRoutines) then) =
+      __$MindfulEnergyTemplatesRoutinesCopyWithImpl<$Res>;
+  @override
+  $Res call(
+      {int mindfulEnergyTemplateMindfulEnergyTemplateId,
+      int routinesRoutineTemplateId});
+}
+
+/// @nodoc
+class __$MindfulEnergyTemplatesRoutinesCopyWithImpl<$Res>
+    extends _$MindfulEnergyTemplatesRoutinesCopyWithImpl<$Res>
+    implements _$MindfulEnergyTemplatesRoutinesCopyWith<$Res> {
+  __$MindfulEnergyTemplatesRoutinesCopyWithImpl(
+      _MindfulEnergyTemplatesRoutines _value,
+      $Res Function(_MindfulEnergyTemplatesRoutines) _then)
+      : super(_value, (v) => _then(v as _MindfulEnergyTemplatesRoutines));
+
+  @override
+  _MindfulEnergyTemplatesRoutines get _value =>
+      super._value as _MindfulEnergyTemplatesRoutines;
+
+  @override
+  $Res call({
+    Object? mindfulEnergyTemplateMindfulEnergyTemplateId = freezed,
+    Object? routinesRoutineTemplateId = freezed,
+  }) {
+    return _then(_MindfulEnergyTemplatesRoutines(
+      mindfulEnergyTemplateMindfulEnergyTemplateId:
+          mindfulEnergyTemplateMindfulEnergyTemplateId == freezed
+              ? _value.mindfulEnergyTemplateMindfulEnergyTemplateId
+              : mindfulEnergyTemplateMindfulEnergyTemplateId // ignore: cast_nullable_to_non_nullable
+                  as int,
+      routinesRoutineTemplateId: routinesRoutineTemplateId == freezed
+          ? _value.routinesRoutineTemplateId
+          : routinesRoutineTemplateId // ignore: cast_nullable_to_non_nullable
+              as int,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$_MindfulEnergyTemplatesRoutines
+    extends _MindfulEnergyTemplatesRoutines {
+  _$_MindfulEnergyTemplatesRoutines(
+      {required this.mindfulEnergyTemplateMindfulEnergyTemplateId,
+      required this.routinesRoutineTemplateId})
+      : super._();
+
+  @override
+  final int mindfulEnergyTemplateMindfulEnergyTemplateId;
+  @override
+  final int routinesRoutineTemplateId;
+
+  @override
+  String toString() {
+    return 'MindfulEnergyTemplatesRoutines(mindfulEnergyTemplateMindfulEnergyTemplateId: $mindfulEnergyTemplateMindfulEnergyTemplateId, routinesRoutineTemplateId: $routinesRoutineTemplateId)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other is _MindfulEnergyTemplatesRoutines &&
+            (identical(other.mindfulEnergyTemplateMindfulEnergyTemplateId,
+                    mindfulEnergyTemplateMindfulEnergyTemplateId) ||
+                const DeepCollectionEquality().equals(
+                    other.mindfulEnergyTemplateMindfulEnergyTemplateId,
+                    mindfulEnergyTemplateMindfulEnergyTemplateId)) &&
+            (identical(other.routinesRoutineTemplateId,
+                    routinesRoutineTemplateId) ||
+                const DeepCollectionEquality().equals(
+                    other.routinesRoutineTemplateId,
+                    routinesRoutineTemplateId)));
+  }
+
+  @override
+  int get hashCode =>
+      runtimeType.hashCode ^
+      const DeepCollectionEquality()
+          .hash(mindfulEnergyTemplateMindfulEnergyTemplateId) ^
+      const DeepCollectionEquality().hash(routinesRoutineTemplateId);
+
+  @JsonKey(ignore: true)
+  @override
+  _$MindfulEnergyTemplatesRoutinesCopyWith<_MindfulEnergyTemplatesRoutines>
+      get copyWith => __$MindfulEnergyTemplatesRoutinesCopyWithImpl<
+          _MindfulEnergyTemplatesRoutines>(this, _$identity);
+}
+
+abstract class _MindfulEnergyTemplatesRoutines
+    extends MindfulEnergyTemplatesRoutines {
+  factory _MindfulEnergyTemplatesRoutines(
+          {required int mindfulEnergyTemplateMindfulEnergyTemplateId,
+          required int routinesRoutineTemplateId}) =
+      _$_MindfulEnergyTemplatesRoutines;
+  _MindfulEnergyTemplatesRoutines._() : super._();
+
+  @override
+  int get mindfulEnergyTemplateMindfulEnergyTemplateId =>
+      throw _privateConstructorUsedError;
+  @override
+  int get routinesRoutineTemplateId => throw _privateConstructorUsedError;
+  @override
+  @JsonKey(ignore: true)
+  _$MindfulEnergyTemplatesRoutinesCopyWith<_MindfulEnergyTemplatesRoutines>
+      get copyWith => throw _privateConstructorUsedError;
+}

caa7d1c4844feeb5431cefe57f47ede80fc8091e
aditya <aditya_jinegar@yahoo.com>
2021-08-13 11:35:09 -0400

    Create activeLivingTemplatesCustomGoals.freezed.dart

---
+++ lib/domain/Templates/ActiveLivingTemplates/ActiveLivingTemplatesCustomGoals/activeLivingTemplatesCustomGoals.freezed.dart
@@ -0,0 +1,195 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides
+
+part of 'activeLivingTemplatesCustomGoals.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more informations: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+class _$ActiveLivingTemplatesCustomGoalsTearOff {
+  const _$ActiveLivingTemplatesCustomGoalsTearOff();
+
+  _ActiveLivingTemplatesCustomGoals call(
+      {required int activeLivingTemplateActiveLivingTemplateId,
+      required int customGoalsGoalTemplateId}) {
+    return _ActiveLivingTemplatesCustomGoals(
+      activeLivingTemplateActiveLivingTemplateId:
+          activeLivingTemplateActiveLivingTemplateId,
+      customGoalsGoalTemplateId: customGoalsGoalTemplateId,
+    );
+  }
+}
+
+/// @nodoc
+const $ActiveLivingTemplatesCustomGoals =
+    _$ActiveLivingTemplatesCustomGoalsTearOff();
+
+/// @nodoc
+mixin _$ActiveLivingTemplatesCustomGoals {
+  int get activeLivingTemplateActiveLivingTemplateId =>
+      throw _privateConstructorUsedError;
+  int get customGoalsGoalTemplateId => throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $ActiveLivingTemplatesCustomGoalsCopyWith<ActiveLivingTemplatesCustomGoals>
+      get copyWith => throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $ActiveLivingTemplatesCustomGoalsCopyWith<$Res> {
+  factory $ActiveLivingTemplatesCustomGoalsCopyWith(
+          ActiveLivingTemplatesCustomGoals value,
+          $Res Function(ActiveLivingTemplatesCustomGoals) then) =
+      _$ActiveLivingTemplatesCustomGoalsCopyWithImpl<$Res>;
+  $Res call(
+      {int activeLivingTemplateActiveLivingTemplateId,
+      int customGoalsGoalTemplateId});
+}
+
+/// @nodoc
+class _$ActiveLivingTemplatesCustomGoalsCopyWithImpl<$Res>
+    implements $ActiveLivingTemplatesCustomGoalsCopyWith<$Res> {
+  _$ActiveLivingTemplatesCustomGoalsCopyWithImpl(this._value, this._then);
+
+  final ActiveLivingTemplatesCustomGoals _value;
+  // ignore: unused_field
+  final $Res Function(ActiveLivingTemplatesCustomGoals) _then;
+
+  @override
+  $Res call({
+    Object? activeLivingTemplateActiveLivingTemplateId = freezed,
+    Object? customGoalsGoalTemplateId = freezed,
+  }) {
+    return _then(_value.copyWith(
+      activeLivingTemplateActiveLivingTemplateId:
+          activeLivingTemplateActiveLivingTemplateId == freezed
+              ? _value.activeLivingTemplateActiveLivingTemplateId
+              : activeLivingTemplateActiveLivingTemplateId // ignore: cast_nullable_to_non_nullable
+                  as int,
+      customGoalsGoalTemplateId: customGoalsGoalTemplateId == freezed
+          ? _value.customGoalsGoalTemplateId
+          : customGoalsGoalTemplateId // ignore: cast_nullable_to_non_nullable
+              as int,
+    ));
+  }
+}
+
+/// @nodoc
+abstract class _$ActiveLivingTemplatesCustomGoalsCopyWith<$Res>
+    implements $ActiveLivingTemplatesCustomGoalsCopyWith<$Res> {
+  factory _$ActiveLivingTemplatesCustomGoalsCopyWith(
+          _ActiveLivingTemplatesCustomGoals value,
+          $Res Function(_ActiveLivingTemplatesCustomGoals) then) =
+      __$ActiveLivingTemplatesCustomGoalsCopyWithImpl<$Res>;
+  @override
+  $Res call(
+      {int activeLivingTemplateActiveLivingTemplateId,
+      int customGoalsGoalTemplateId});
+}
+
+/// @nodoc
+class __$ActiveLivingTemplatesCustomGoalsCopyWithImpl<$Res>
+    extends _$ActiveLivingTemplatesCustomGoalsCopyWithImpl<$Res>
+    implements _$ActiveLivingTemplatesCustomGoalsCopyWith<$Res> {
+  __$ActiveLivingTemplatesCustomGoalsCopyWithImpl(
+      _ActiveLivingTemplatesCustomGoals _value,
+      $Res Function(_ActiveLivingTemplatesCustomGoals) _then)
+      : super(_value, (v) => _then(v as _ActiveLivingTemplatesCustomGoals));
+
+  @override
+  _ActiveLivingTemplatesCustomGoals get _value =>
+      super._value as _ActiveLivingTemplatesCustomGoals;
+
+  @override
+  $Res call({
+    Object? activeLivingTemplateActiveLivingTemplateId = freezed,
+    Object? customGoalsGoalTemplateId = freezed,
+  }) {
+    return _then(_ActiveLivingTemplatesCustomGoals(
+      activeLivingTemplateActiveLivingTemplateId:
+          activeLivingTemplateActiveLivingTemplateId == freezed
+              ? _value.activeLivingTemplateActiveLivingTemplateId
+              : activeLivingTemplateActiveLivingTemplateId // ignore: cast_nullable_to_non_nullable
+                  as int,
+      customGoalsGoalTemplateId: customGoalsGoalTemplateId == freezed
+          ? _value.customGoalsGoalTemplateId
+          : customGoalsGoalTemplateId // ignore: cast_nullable_to_non_nullable
+              as int,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$_ActiveLivingTemplatesCustomGoals
+    extends _ActiveLivingTemplatesCustomGoals {
+  _$_ActiveLivingTemplatesCustomGoals(
+      {required this.activeLivingTemplateActiveLivingTemplateId,
+      required this.customGoalsGoalTemplateId})
+      : super._();
+
+  @override
+  final int activeLivingTemplateActiveLivingTemplateId;
+  @override
+  final int customGoalsGoalTemplateId;
+
+  @override
+  String toString() {
+    return 'ActiveLivingTemplatesCustomGoals(activeLivingTemplateActiveLivingTemplateId: $activeLivingTemplateActiveLivingTemplateId, customGoalsGoalTemplateId: $customGoalsGoalTemplateId)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other is _ActiveLivingTemplatesCustomGoals &&
+            (identical(other.activeLivingTemplateActiveLivingTemplateId,
+                    activeLivingTemplateActiveLivingTemplateId) ||
+                const DeepCollectionEquality().equals(
+                    other.activeLivingTemplateActiveLivingTemplateId,
+                    activeLivingTemplateActiveLivingTemplateId)) &&
+            (identical(other.customGoalsGoalTemplateId,
+                    customGoalsGoalTemplateId) ||
+                const DeepCollectionEquality().equals(
+                    other.customGoalsGoalTemplateId,
+                    customGoalsGoalTemplateId)));
+  }
+
+  @override
+  int get hashCode =>
+      runtimeType.hashCode ^
+      const DeepCollectionEquality()
+          .hash(activeLivingTemplateActiveLivingTemplateId) ^
+      const DeepCollectionEquality().hash(customGoalsGoalTemplateId);
+
+  @JsonKey(ignore: true)
+  @override
+  _$ActiveLivingTemplatesCustomGoalsCopyWith<_ActiveLivingTemplatesCustomGoals>
+      get copyWith => __$ActiveLivingTemplatesCustomGoalsCopyWithImpl<
+          _ActiveLivingTemplatesCustomGoals>(this, _$identity);
+}
+
+abstract class _ActiveLivingTemplatesCustomGoals
+    extends ActiveLivingTemplatesCustomGoals {
+  factory _ActiveLivingTemplatesCustomGoals(
+          {required int activeLivingTemplateActiveLivingTemplateId,
+          required int customGoalsGoalTemplateId}) =
+      _$_ActiveLivingTemplatesCustomGoals;
+  _ActiveLivingTemplatesCustomGoals._() : super._();
+
+  @override
+  int get activeLivingTemplateActiveLivingTemplateId =>
+      throw _privateConstructorUsedError;
+  @override
+  int get customGoalsGoalTemplateId => throw _privateConstructorUsedError;
+  @override
+  @JsonKey(ignore: true)
+  _$ActiveLivingTemplatesCustomGoalsCopyWith<_ActiveLivingTemplatesCustomGoals>
+      get copyWith => throw _privateConstructorUsedError;
+}

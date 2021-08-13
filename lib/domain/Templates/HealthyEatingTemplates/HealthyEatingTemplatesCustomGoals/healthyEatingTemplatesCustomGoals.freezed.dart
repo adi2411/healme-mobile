fee2eab024dce396af10be99b291035a06bd2601
aditya <aditya_jinegar@yahoo.com>
2021-08-13 12:08:14 -0400

    Create healthyEatingTemplatesCustomGoals.freezed.dart

---
+++ lib/domain/Templates/HealthyEatingTemplates/HealthyEatingTemplatesCustomGoals/healthyEatingTemplatesCustomGoals.freezed.dart
@@ -0,0 +1,197 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides
+
+part of 'healthyEatingTemplatesCustomGoals.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more informations: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+class _$HealthyEatingTemplatesCustomGoalsTearOff {
+  const _$HealthyEatingTemplatesCustomGoalsTearOff();
+
+  _HealthyEatingTemplatesCustomGoals call(
+      {required int healthyEatingTemplateHealthyEatingTemplateId,
+      required int customGoalsGoalTemplateId}) {
+    return _HealthyEatingTemplatesCustomGoals(
+      healthyEatingTemplateHealthyEatingTemplateId:
+          healthyEatingTemplateHealthyEatingTemplateId,
+      customGoalsGoalTemplateId: customGoalsGoalTemplateId,
+    );
+  }
+}
+
+/// @nodoc
+const $HealthyEatingTemplatesCustomGoals =
+    _$HealthyEatingTemplatesCustomGoalsTearOff();
+
+/// @nodoc
+mixin _$HealthyEatingTemplatesCustomGoals {
+  int get healthyEatingTemplateHealthyEatingTemplateId =>
+      throw _privateConstructorUsedError;
+  int get customGoalsGoalTemplateId => throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $HealthyEatingTemplatesCustomGoalsCopyWith<HealthyEatingTemplatesCustomGoals>
+      get copyWith => throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $HealthyEatingTemplatesCustomGoalsCopyWith<$Res> {
+  factory $HealthyEatingTemplatesCustomGoalsCopyWith(
+          HealthyEatingTemplatesCustomGoals value,
+          $Res Function(HealthyEatingTemplatesCustomGoals) then) =
+      _$HealthyEatingTemplatesCustomGoalsCopyWithImpl<$Res>;
+  $Res call(
+      {int healthyEatingTemplateHealthyEatingTemplateId,
+      int customGoalsGoalTemplateId});
+}
+
+/// @nodoc
+class _$HealthyEatingTemplatesCustomGoalsCopyWithImpl<$Res>
+    implements $HealthyEatingTemplatesCustomGoalsCopyWith<$Res> {
+  _$HealthyEatingTemplatesCustomGoalsCopyWithImpl(this._value, this._then);
+
+  final HealthyEatingTemplatesCustomGoals _value;
+  // ignore: unused_field
+  final $Res Function(HealthyEatingTemplatesCustomGoals) _then;
+
+  @override
+  $Res call({
+    Object? healthyEatingTemplateHealthyEatingTemplateId = freezed,
+    Object? customGoalsGoalTemplateId = freezed,
+  }) {
+    return _then(_value.copyWith(
+      healthyEatingTemplateHealthyEatingTemplateId:
+          healthyEatingTemplateHealthyEatingTemplateId == freezed
+              ? _value.healthyEatingTemplateHealthyEatingTemplateId
+              : healthyEatingTemplateHealthyEatingTemplateId // ignore: cast_nullable_to_non_nullable
+                  as int,
+      customGoalsGoalTemplateId: customGoalsGoalTemplateId == freezed
+          ? _value.customGoalsGoalTemplateId
+          : customGoalsGoalTemplateId // ignore: cast_nullable_to_non_nullable
+              as int,
+    ));
+  }
+}
+
+/// @nodoc
+abstract class _$HealthyEatingTemplatesCustomGoalsCopyWith<$Res>
+    implements $HealthyEatingTemplatesCustomGoalsCopyWith<$Res> {
+  factory _$HealthyEatingTemplatesCustomGoalsCopyWith(
+          _HealthyEatingTemplatesCustomGoals value,
+          $Res Function(_HealthyEatingTemplatesCustomGoals) then) =
+      __$HealthyEatingTemplatesCustomGoalsCopyWithImpl<$Res>;
+  @override
+  $Res call(
+      {int healthyEatingTemplateHealthyEatingTemplateId,
+      int customGoalsGoalTemplateId});
+}
+
+/// @nodoc
+class __$HealthyEatingTemplatesCustomGoalsCopyWithImpl<$Res>
+    extends _$HealthyEatingTemplatesCustomGoalsCopyWithImpl<$Res>
+    implements _$HealthyEatingTemplatesCustomGoalsCopyWith<$Res> {
+  __$HealthyEatingTemplatesCustomGoalsCopyWithImpl(
+      _HealthyEatingTemplatesCustomGoals _value,
+      $Res Function(_HealthyEatingTemplatesCustomGoals) _then)
+      : super(_value, (v) => _then(v as _HealthyEatingTemplatesCustomGoals));
+
+  @override
+  _HealthyEatingTemplatesCustomGoals get _value =>
+      super._value as _HealthyEatingTemplatesCustomGoals;
+
+  @override
+  $Res call({
+    Object? healthyEatingTemplateHealthyEatingTemplateId = freezed,
+    Object? customGoalsGoalTemplateId = freezed,
+  }) {
+    return _then(_HealthyEatingTemplatesCustomGoals(
+      healthyEatingTemplateHealthyEatingTemplateId:
+          healthyEatingTemplateHealthyEatingTemplateId == freezed
+              ? _value.healthyEatingTemplateHealthyEatingTemplateId
+              : healthyEatingTemplateHealthyEatingTemplateId // ignore: cast_nullable_to_non_nullable
+                  as int,
+      customGoalsGoalTemplateId: customGoalsGoalTemplateId == freezed
+          ? _value.customGoalsGoalTemplateId
+          : customGoalsGoalTemplateId // ignore: cast_nullable_to_non_nullable
+              as int,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$_HealthyEatingTemplatesCustomGoals
+    extends _HealthyEatingTemplatesCustomGoals {
+  _$_HealthyEatingTemplatesCustomGoals(
+      {required this.healthyEatingTemplateHealthyEatingTemplateId,
+      required this.customGoalsGoalTemplateId})
+      : super._();
+
+  @override
+  final int healthyEatingTemplateHealthyEatingTemplateId;
+  @override
+  final int customGoalsGoalTemplateId;
+
+  @override
+  String toString() {
+    return 'HealthyEatingTemplatesCustomGoals(healthyEatingTemplateHealthyEatingTemplateId: $healthyEatingTemplateHealthyEatingTemplateId, customGoalsGoalTemplateId: $customGoalsGoalTemplateId)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other is _HealthyEatingTemplatesCustomGoals &&
+            (identical(other.healthyEatingTemplateHealthyEatingTemplateId,
+                    healthyEatingTemplateHealthyEatingTemplateId) ||
+                const DeepCollectionEquality().equals(
+                    other.healthyEatingTemplateHealthyEatingTemplateId,
+                    healthyEatingTemplateHealthyEatingTemplateId)) &&
+            (identical(other.customGoalsGoalTemplateId,
+                    customGoalsGoalTemplateId) ||
+                const DeepCollectionEquality().equals(
+                    other.customGoalsGoalTemplateId,
+                    customGoalsGoalTemplateId)));
+  }
+
+  @override
+  int get hashCode =>
+      runtimeType.hashCode ^
+      const DeepCollectionEquality()
+          .hash(healthyEatingTemplateHealthyEatingTemplateId) ^
+      const DeepCollectionEquality().hash(customGoalsGoalTemplateId);
+
+  @JsonKey(ignore: true)
+  @override
+  _$HealthyEatingTemplatesCustomGoalsCopyWith<
+          _HealthyEatingTemplatesCustomGoals>
+      get copyWith => __$HealthyEatingTemplatesCustomGoalsCopyWithImpl<
+          _HealthyEatingTemplatesCustomGoals>(this, _$identity);
+}
+
+abstract class _HealthyEatingTemplatesCustomGoals
+    extends HealthyEatingTemplatesCustomGoals {
+  factory _HealthyEatingTemplatesCustomGoals(
+          {required int healthyEatingTemplateHealthyEatingTemplateId,
+          required int customGoalsGoalTemplateId}) =
+      _$_HealthyEatingTemplatesCustomGoals;
+  _HealthyEatingTemplatesCustomGoals._() : super._();
+
+  @override
+  int get healthyEatingTemplateHealthyEatingTemplateId =>
+      throw _privateConstructorUsedError;
+  @override
+  int get customGoalsGoalTemplateId => throw _privateConstructorUsedError;
+  @override
+  @JsonKey(ignore: true)
+  _$HealthyEatingTemplatesCustomGoalsCopyWith<
+          _HealthyEatingTemplatesCustomGoals>
+      get copyWith => throw _privateConstructorUsedError;
+}

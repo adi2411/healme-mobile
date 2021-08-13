51170893b4b719eab4d8288f3b3955be47c56d87
aditya <aditya_jinegar@yahoo.com>
2021-08-13 11:35:43 -0400

    Create exerciseTemplates.freezed.dart

---
+++ lib/domain/Templates/AppOrganizations/ExerciseTemplates/exerciseTemplates.freezed.dart
@@ -0,0 +1,275 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides
+
+part of 'exerciseTemplates.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more informations: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+class _$ExerciseTemplatesTearOff {
+  const _$ExerciseTemplatesTearOff();
+
+  _ExerciseTemplates call(
+      {required int exerciseTemplateId,
+      required StringSingleLine createdBy,
+      required DateTime createdDate,
+      required StringSingleLine lastModifiedBy,
+      required DateTime lastModifiedDate,
+      required int active}) {
+    return _ExerciseTemplates(
+      exerciseTemplateId: exerciseTemplateId,
+      createdBy: createdBy,
+      createdDate: createdDate,
+      lastModifiedBy: lastModifiedBy,
+      lastModifiedDate: lastModifiedDate,
+      active: active,
+    );
+  }
+}
+
+/// @nodoc
+const $ExerciseTemplates = _$ExerciseTemplatesTearOff();
+
+/// @nodoc
+mixin _$ExerciseTemplates {
+  int get exerciseTemplateId => throw _privateConstructorUsedError;
+  StringSingleLine get createdBy => throw _privateConstructorUsedError;
+  DateTime get createdDate => throw _privateConstructorUsedError;
+  StringSingleLine get lastModifiedBy => throw _privateConstructorUsedError;
+  DateTime get lastModifiedDate => throw _privateConstructorUsedError;
+  int get active => throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $ExerciseTemplatesCopyWith<ExerciseTemplates> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $ExerciseTemplatesCopyWith<$Res> {
+  factory $ExerciseTemplatesCopyWith(
+          ExerciseTemplates value, $Res Function(ExerciseTemplates) then) =
+      _$ExerciseTemplatesCopyWithImpl<$Res>;
+  $Res call(
+      {int exerciseTemplateId,
+      StringSingleLine createdBy,
+      DateTime createdDate,
+      StringSingleLine lastModifiedBy,
+      DateTime lastModifiedDate,
+      int active});
+}
+
+/// @nodoc
+class _$ExerciseTemplatesCopyWithImpl<$Res>
+    implements $ExerciseTemplatesCopyWith<$Res> {
+  _$ExerciseTemplatesCopyWithImpl(this._value, this._then);
+
+  final ExerciseTemplates _value;
+  // ignore: unused_field
+  final $Res Function(ExerciseTemplates) _then;
+
+  @override
+  $Res call({
+    Object? exerciseTemplateId = freezed,
+    Object? createdBy = freezed,
+    Object? createdDate = freezed,
+    Object? lastModifiedBy = freezed,
+    Object? lastModifiedDate = freezed,
+    Object? active = freezed,
+  }) {
+    return _then(_value.copyWith(
+      exerciseTemplateId: exerciseTemplateId == freezed
+          ? _value.exerciseTemplateId
+          : exerciseTemplateId // ignore: cast_nullable_to_non_nullable
+              as int,
+      createdBy: createdBy == freezed
+          ? _value.createdBy
+          : createdBy // ignore: cast_nullable_to_non_nullable
+              as StringSingleLine,
+      createdDate: createdDate == freezed
+          ? _value.createdDate
+          : createdDate // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      lastModifiedBy: lastModifiedBy == freezed
+          ? _value.lastModifiedBy
+          : lastModifiedBy // ignore: cast_nullable_to_non_nullable
+              as StringSingleLine,
+      lastModifiedDate: lastModifiedDate == freezed
+          ? _value.lastModifiedDate
+          : lastModifiedDate // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      active: active == freezed
+          ? _value.active
+          : active // ignore: cast_nullable_to_non_nullable
+              as int,
+    ));
+  }
+}
+
+/// @nodoc
+abstract class _$ExerciseTemplatesCopyWith<$Res>
+    implements $ExerciseTemplatesCopyWith<$Res> {
+  factory _$ExerciseTemplatesCopyWith(
+          _ExerciseTemplates value, $Res Function(_ExerciseTemplates) then) =
+      __$ExerciseTemplatesCopyWithImpl<$Res>;
+  @override
+  $Res call(
+      {int exerciseTemplateId,
+      StringSingleLine createdBy,
+      DateTime createdDate,
+      StringSingleLine lastModifiedBy,
+      DateTime lastModifiedDate,
+      int active});
+}
+
+/// @nodoc
+class __$ExerciseTemplatesCopyWithImpl<$Res>
+    extends _$ExerciseTemplatesCopyWithImpl<$Res>
+    implements _$ExerciseTemplatesCopyWith<$Res> {
+  __$ExerciseTemplatesCopyWithImpl(
+      _ExerciseTemplates _value, $Res Function(_ExerciseTemplates) _then)
+      : super(_value, (v) => _then(v as _ExerciseTemplates));
+
+  @override
+  _ExerciseTemplates get _value => super._value as _ExerciseTemplates;
+
+  @override
+  $Res call({
+    Object? exerciseTemplateId = freezed,
+    Object? createdBy = freezed,
+    Object? createdDate = freezed,
+    Object? lastModifiedBy = freezed,
+    Object? lastModifiedDate = freezed,
+    Object? active = freezed,
+  }) {
+    return _then(_ExerciseTemplates(
+      exerciseTemplateId: exerciseTemplateId == freezed
+          ? _value.exerciseTemplateId
+          : exerciseTemplateId // ignore: cast_nullable_to_non_nullable
+              as int,
+      createdBy: createdBy == freezed
+          ? _value.createdBy
+          : createdBy // ignore: cast_nullable_to_non_nullable
+              as StringSingleLine,
+      createdDate: createdDate == freezed
+          ? _value.createdDate
+          : createdDate // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      lastModifiedBy: lastModifiedBy == freezed
+          ? _value.lastModifiedBy
+          : lastModifiedBy // ignore: cast_nullable_to_non_nullable
+              as StringSingleLine,
+      lastModifiedDate: lastModifiedDate == freezed
+          ? _value.lastModifiedDate
+          : lastModifiedDate // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      active: active == freezed
+          ? _value.active
+          : active // ignore: cast_nullable_to_non_nullable
+              as int,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$_ExerciseTemplates extends _ExerciseTemplates {
+  _$_ExerciseTemplates(
+      {required this.exerciseTemplateId,
+      required this.createdBy,
+      required this.createdDate,
+      required this.lastModifiedBy,
+      required this.lastModifiedDate,
+      required this.active})
+      : super._();
+
+  @override
+  final int exerciseTemplateId;
+  @override
+  final StringSingleLine createdBy;
+  @override
+  final DateTime createdDate;
+  @override
+  final StringSingleLine lastModifiedBy;
+  @override
+  final DateTime lastModifiedDate;
+  @override
+  final int active;
+
+  @override
+  String toString() {
+    return 'ExerciseTemplates(exerciseTemplateId: $exerciseTemplateId, createdBy: $createdBy, createdDate: $createdDate, lastModifiedBy: $lastModifiedBy, lastModifiedDate: $lastModifiedDate, active: $active)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other is _ExerciseTemplates &&
+            (identical(other.exerciseTemplateId, exerciseTemplateId) ||
+                const DeepCollectionEquality()
+                    .equals(other.exerciseTemplateId, exerciseTemplateId)) &&
+            (identical(other.createdBy, createdBy) ||
+                const DeepCollectionEquality()
+                    .equals(other.createdBy, createdBy)) &&
+            (identical(other.createdDate, createdDate) ||
+                const DeepCollectionEquality()
+                    .equals(other.createdDate, createdDate)) &&
+            (identical(other.lastModifiedBy, lastModifiedBy) ||
+                const DeepCollectionEquality()
+                    .equals(other.lastModifiedBy, lastModifiedBy)) &&
+            (identical(other.lastModifiedDate, lastModifiedDate) ||
+                const DeepCollectionEquality()
+                    .equals(other.lastModifiedDate, lastModifiedDate)) &&
+            (identical(other.active, active) ||
+                const DeepCollectionEquality().equals(other.active, active)));
+  }
+
+  @override
+  int get hashCode =>
+      runtimeType.hashCode ^
+      const DeepCollectionEquality().hash(exerciseTemplateId) ^
+      const DeepCollectionEquality().hash(createdBy) ^
+      const DeepCollectionEquality().hash(createdDate) ^
+      const DeepCollectionEquality().hash(lastModifiedBy) ^
+      const DeepCollectionEquality().hash(lastModifiedDate) ^
+      const DeepCollectionEquality().hash(active);
+
+  @JsonKey(ignore: true)
+  @override
+  _$ExerciseTemplatesCopyWith<_ExerciseTemplates> get copyWith =>
+      __$ExerciseTemplatesCopyWithImpl<_ExerciseTemplates>(this, _$identity);
+}
+
+abstract class _ExerciseTemplates extends ExerciseTemplates {
+  factory _ExerciseTemplates(
+      {required int exerciseTemplateId,
+      required StringSingleLine createdBy,
+      required DateTime createdDate,
+      required StringSingleLine lastModifiedBy,
+      required DateTime lastModifiedDate,
+      required int active}) = _$_ExerciseTemplates;
+  _ExerciseTemplates._() : super._();
+
+  @override
+  int get exerciseTemplateId => throw _privateConstructorUsedError;
+  @override
+  StringSingleLine get createdBy => throw _privateConstructorUsedError;
+  @override
+  DateTime get createdDate => throw _privateConstructorUsedError;
+  @override
+  StringSingleLine get lastModifiedBy => throw _privateConstructorUsedError;
+  @override
+  DateTime get lastModifiedDate => throw _privateConstructorUsedError;
+  @override
+  int get active => throw _privateConstructorUsedError;
+  @override
+  @JsonKey(ignore: true)
+  _$ExerciseTemplatesCopyWith<_ExerciseTemplates> get copyWith =>
+      throw _privateConstructorUsedError;
+}

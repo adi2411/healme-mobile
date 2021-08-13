73e042a0f7571224727c20714f5896c7243d5994
aditya <aditya_jinegar@yahoo.com>
2021-08-13 12:08:54 -0400

    Create motivationsMessageTemplates.freezed.dart

---
+++ lib/domain/Templates/ProgramTemplates/MotivationsMessageTemplates/motivationsMessageTemplates.freezed.dart
@@ -0,0 +1,284 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides
+
+part of 'motivationsMessageTemplates.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more informations: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+class _$MotivationsMessageTemplatesTearOff {
+  const _$MotivationsMessageTemplatesTearOff();
+
+  _MotivationsMessageTemplates call(
+      {required int motivationsMessageTemplateId,
+      required StringSingleLine createdBy,
+      required DateTime createdDate,
+      required StringSingleLine lastModifiedBy,
+      required DateTime lastModifiedDate,
+      required StringMultiLine messageDesc}) {
+    return _MotivationsMessageTemplates(
+      motivationsMessageTemplateId: motivationsMessageTemplateId,
+      createdBy: createdBy,
+      createdDate: createdDate,
+      lastModifiedBy: lastModifiedBy,
+      lastModifiedDate: lastModifiedDate,
+      messageDesc: messageDesc,
+    );
+  }
+}
+
+/// @nodoc
+const $MotivationsMessageTemplates = _$MotivationsMessageTemplatesTearOff();
+
+/// @nodoc
+mixin _$MotivationsMessageTemplates {
+  int get motivationsMessageTemplateId => throw _privateConstructorUsedError;
+  StringSingleLine get createdBy => throw _privateConstructorUsedError;
+  DateTime get createdDate => throw _privateConstructorUsedError;
+  StringSingleLine get lastModifiedBy => throw _privateConstructorUsedError;
+  DateTime get lastModifiedDate => throw _privateConstructorUsedError;
+  StringMultiLine get messageDesc => throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $MotivationsMessageTemplatesCopyWith<MotivationsMessageTemplates>
+      get copyWith => throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $MotivationsMessageTemplatesCopyWith<$Res> {
+  factory $MotivationsMessageTemplatesCopyWith(
+          MotivationsMessageTemplates value,
+          $Res Function(MotivationsMessageTemplates) then) =
+      _$MotivationsMessageTemplatesCopyWithImpl<$Res>;
+  $Res call(
+      {int motivationsMessageTemplateId,
+      StringSingleLine createdBy,
+      DateTime createdDate,
+      StringSingleLine lastModifiedBy,
+      DateTime lastModifiedDate,
+      StringMultiLine messageDesc});
+}
+
+/// @nodoc
+class _$MotivationsMessageTemplatesCopyWithImpl<$Res>
+    implements $MotivationsMessageTemplatesCopyWith<$Res> {
+  _$MotivationsMessageTemplatesCopyWithImpl(this._value, this._then);
+
+  final MotivationsMessageTemplates _value;
+  // ignore: unused_field
+  final $Res Function(MotivationsMessageTemplates) _then;
+
+  @override
+  $Res call({
+    Object? motivationsMessageTemplateId = freezed,
+    Object? createdBy = freezed,
+    Object? createdDate = freezed,
+    Object? lastModifiedBy = freezed,
+    Object? lastModifiedDate = freezed,
+    Object? messageDesc = freezed,
+  }) {
+    return _then(_value.copyWith(
+      motivationsMessageTemplateId: motivationsMessageTemplateId == freezed
+          ? _value.motivationsMessageTemplateId
+          : motivationsMessageTemplateId // ignore: cast_nullable_to_non_nullable
+              as int,
+      createdBy: createdBy == freezed
+          ? _value.createdBy
+          : createdBy // ignore: cast_nullable_to_non_nullable
+              as StringSingleLine,
+      createdDate: createdDate == freezed
+          ? _value.createdDate
+          : createdDate // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      lastModifiedBy: lastModifiedBy == freezed
+          ? _value.lastModifiedBy
+          : lastModifiedBy // ignore: cast_nullable_to_non_nullable
+              as StringSingleLine,
+      lastModifiedDate: lastModifiedDate == freezed
+          ? _value.lastModifiedDate
+          : lastModifiedDate // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      messageDesc: messageDesc == freezed
+          ? _value.messageDesc
+          : messageDesc // ignore: cast_nullable_to_non_nullable
+              as StringMultiLine,
+    ));
+  }
+}
+
+/// @nodoc
+abstract class _$MotivationsMessageTemplatesCopyWith<$Res>
+    implements $MotivationsMessageTemplatesCopyWith<$Res> {
+  factory _$MotivationsMessageTemplatesCopyWith(
+          _MotivationsMessageTemplates value,
+          $Res Function(_MotivationsMessageTemplates) then) =
+      __$MotivationsMessageTemplatesCopyWithImpl<$Res>;
+  @override
+  $Res call(
+      {int motivationsMessageTemplateId,
+      StringSingleLine createdBy,
+      DateTime createdDate,
+      StringSingleLine lastModifiedBy,
+      DateTime lastModifiedDate,
+      StringMultiLine messageDesc});
+}
+
+/// @nodoc
+class __$MotivationsMessageTemplatesCopyWithImpl<$Res>
+    extends _$MotivationsMessageTemplatesCopyWithImpl<$Res>
+    implements _$MotivationsMessageTemplatesCopyWith<$Res> {
+  __$MotivationsMessageTemplatesCopyWithImpl(
+      _MotivationsMessageTemplates _value,
+      $Res Function(_MotivationsMessageTemplates) _then)
+      : super(_value, (v) => _then(v as _MotivationsMessageTemplates));
+
+  @override
+  _MotivationsMessageTemplates get _value =>
+      super._value as _MotivationsMessageTemplates;
+
+  @override
+  $Res call({
+    Object? motivationsMessageTemplateId = freezed,
+    Object? createdBy = freezed,
+    Object? createdDate = freezed,
+    Object? lastModifiedBy = freezed,
+    Object? lastModifiedDate = freezed,
+    Object? messageDesc = freezed,
+  }) {
+    return _then(_MotivationsMessageTemplates(
+      motivationsMessageTemplateId: motivationsMessageTemplateId == freezed
+          ? _value.motivationsMessageTemplateId
+          : motivationsMessageTemplateId // ignore: cast_nullable_to_non_nullable
+              as int,
+      createdBy: createdBy == freezed
+          ? _value.createdBy
+          : createdBy // ignore: cast_nullable_to_non_nullable
+              as StringSingleLine,
+      createdDate: createdDate == freezed
+          ? _value.createdDate
+          : createdDate // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      lastModifiedBy: lastModifiedBy == freezed
+          ? _value.lastModifiedBy
+          : lastModifiedBy // ignore: cast_nullable_to_non_nullable
+              as StringSingleLine,
+      lastModifiedDate: lastModifiedDate == freezed
+          ? _value.lastModifiedDate
+          : lastModifiedDate // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      messageDesc: messageDesc == freezed
+          ? _value.messageDesc
+          : messageDesc // ignore: cast_nullable_to_non_nullable
+              as StringMultiLine,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$_MotivationsMessageTemplates extends _MotivationsMessageTemplates {
+  _$_MotivationsMessageTemplates(
+      {required this.motivationsMessageTemplateId,
+      required this.createdBy,
+      required this.createdDate,
+      required this.lastModifiedBy,
+      required this.lastModifiedDate,
+      required this.messageDesc})
+      : super._();
+
+  @override
+  final int motivationsMessageTemplateId;
+  @override
+  final StringSingleLine createdBy;
+  @override
+  final DateTime createdDate;
+  @override
+  final StringSingleLine lastModifiedBy;
+  @override
+  final DateTime lastModifiedDate;
+  @override
+  final StringMultiLine messageDesc;
+
+  @override
+  String toString() {
+    return 'MotivationsMessageTemplates(motivationsMessageTemplateId: $motivationsMessageTemplateId, createdBy: $createdBy, createdDate: $createdDate, lastModifiedBy: $lastModifiedBy, lastModifiedDate: $lastModifiedDate, messageDesc: $messageDesc)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other is _MotivationsMessageTemplates &&
+            (identical(other.motivationsMessageTemplateId,
+                    motivationsMessageTemplateId) ||
+                const DeepCollectionEquality().equals(
+                    other.motivationsMessageTemplateId,
+                    motivationsMessageTemplateId)) &&
+            (identical(other.createdBy, createdBy) ||
+                const DeepCollectionEquality()
+                    .equals(other.createdBy, createdBy)) &&
+            (identical(other.createdDate, createdDate) ||
+                const DeepCollectionEquality()
+                    .equals(other.createdDate, createdDate)) &&
+            (identical(other.lastModifiedBy, lastModifiedBy) ||
+                const DeepCollectionEquality()
+                    .equals(other.lastModifiedBy, lastModifiedBy)) &&
+            (identical(other.lastModifiedDate, lastModifiedDate) ||
+                const DeepCollectionEquality()
+                    .equals(other.lastModifiedDate, lastModifiedDate)) &&
+            (identical(other.messageDesc, messageDesc) ||
+                const DeepCollectionEquality()
+                    .equals(other.messageDesc, messageDesc)));
+  }
+
+  @override
+  int get hashCode =>
+      runtimeType.hashCode ^
+      const DeepCollectionEquality().hash(motivationsMessageTemplateId) ^
+      const DeepCollectionEquality().hash(createdBy) ^
+      const DeepCollectionEquality().hash(createdDate) ^
+      const DeepCollectionEquality().hash(lastModifiedBy) ^
+      const DeepCollectionEquality().hash(lastModifiedDate) ^
+      const DeepCollectionEquality().hash(messageDesc);
+
+  @JsonKey(ignore: true)
+  @override
+  _$MotivationsMessageTemplatesCopyWith<_MotivationsMessageTemplates>
+      get copyWith => __$MotivationsMessageTemplatesCopyWithImpl<
+          _MotivationsMessageTemplates>(this, _$identity);
+}
+
+abstract class _MotivationsMessageTemplates
+    extends MotivationsMessageTemplates {
+  factory _MotivationsMessageTemplates(
+      {required int motivationsMessageTemplateId,
+      required StringSingleLine createdBy,
+      required DateTime createdDate,
+      required StringSingleLine lastModifiedBy,
+      required DateTime lastModifiedDate,
+      required StringMultiLine messageDesc}) = _$_MotivationsMessageTemplates;
+  _MotivationsMessageTemplates._() : super._();
+
+  @override
+  int get motivationsMessageTemplateId => throw _privateConstructorUsedError;
+  @override
+  StringSingleLine get createdBy => throw _privateConstructorUsedError;
+  @override
+  DateTime get createdDate => throw _privateConstructorUsedError;
+  @override
+  StringSingleLine get lastModifiedBy => throw _privateConstructorUsedError;
+  @override
+  DateTime get lastModifiedDate => throw _privateConstructorUsedError;
+  @override
+  StringMultiLine get messageDesc => throw _privateConstructorUsedError;
+  @override
+  @JsonKey(ignore: true)
+  _$MotivationsMessageTemplatesCopyWith<_MotivationsMessageTemplates>
+      get copyWith => throw _privateConstructorUsedError;
+}

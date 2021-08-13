9a79eff7eb305d7b61e28536a1f0e788d8297bcc
aditya <aditya_jinegar@yahoo.com>
2021-08-13 13:53:45 -0400

    Create symptomTemplates.freezed.dart

---
+++ lib/domain/Templates/RoutineSurveys_Symptoms/SymptomTemplates/symptomTemplates.freezed.dart
@@ -0,0 +1,276 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides
+
+part of 'symptomTemplates.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more informations: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+class _$SymptomTemplatesTearOff {
+  const _$SymptomTemplatesTearOff();
+
+  _SymptomTemplates call(
+      {required int symptomTemplateId,
+      required StringSingleLine createdBy,
+      required DateTime createdDate,
+      required StringSingleLine lastModifiedBy,
+      required DateTime lastModifiedDate,
+      required StringSingleLine symptomName}) {
+    return _SymptomTemplates(
+      symptomTemplateId: symptomTemplateId,
+      createdBy: createdBy,
+      createdDate: createdDate,
+      lastModifiedBy: lastModifiedBy,
+      lastModifiedDate: lastModifiedDate,
+      symptomName: symptomName,
+    );
+  }
+}
+
+/// @nodoc
+const $SymptomTemplates = _$SymptomTemplatesTearOff();
+
+/// @nodoc
+mixin _$SymptomTemplates {
+  int get symptomTemplateId => throw _privateConstructorUsedError;
+  StringSingleLine get createdBy => throw _privateConstructorUsedError;
+  DateTime get createdDate => throw _privateConstructorUsedError;
+  StringSingleLine get lastModifiedBy => throw _privateConstructorUsedError;
+  DateTime get lastModifiedDate => throw _privateConstructorUsedError;
+  StringSingleLine get symptomName => throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $SymptomTemplatesCopyWith<SymptomTemplates> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $SymptomTemplatesCopyWith<$Res> {
+  factory $SymptomTemplatesCopyWith(
+          SymptomTemplates value, $Res Function(SymptomTemplates) then) =
+      _$SymptomTemplatesCopyWithImpl<$Res>;
+  $Res call(
+      {int symptomTemplateId,
+      StringSingleLine createdBy,
+      DateTime createdDate,
+      StringSingleLine lastModifiedBy,
+      DateTime lastModifiedDate,
+      StringSingleLine symptomName});
+}
+
+/// @nodoc
+class _$SymptomTemplatesCopyWithImpl<$Res>
+    implements $SymptomTemplatesCopyWith<$Res> {
+  _$SymptomTemplatesCopyWithImpl(this._value, this._then);
+
+  final SymptomTemplates _value;
+  // ignore: unused_field
+  final $Res Function(SymptomTemplates) _then;
+
+  @override
+  $Res call({
+    Object? symptomTemplateId = freezed,
+    Object? createdBy = freezed,
+    Object? createdDate = freezed,
+    Object? lastModifiedBy = freezed,
+    Object? lastModifiedDate = freezed,
+    Object? symptomName = freezed,
+  }) {
+    return _then(_value.copyWith(
+      symptomTemplateId: symptomTemplateId == freezed
+          ? _value.symptomTemplateId
+          : symptomTemplateId // ignore: cast_nullable_to_non_nullable
+              as int,
+      createdBy: createdBy == freezed
+          ? _value.createdBy
+          : createdBy // ignore: cast_nullable_to_non_nullable
+              as StringSingleLine,
+      createdDate: createdDate == freezed
+          ? _value.createdDate
+          : createdDate // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      lastModifiedBy: lastModifiedBy == freezed
+          ? _value.lastModifiedBy
+          : lastModifiedBy // ignore: cast_nullable_to_non_nullable
+              as StringSingleLine,
+      lastModifiedDate: lastModifiedDate == freezed
+          ? _value.lastModifiedDate
+          : lastModifiedDate // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      symptomName: symptomName == freezed
+          ? _value.symptomName
+          : symptomName // ignore: cast_nullable_to_non_nullable
+              as StringSingleLine,
+    ));
+  }
+}
+
+/// @nodoc
+abstract class _$SymptomTemplatesCopyWith<$Res>
+    implements $SymptomTemplatesCopyWith<$Res> {
+  factory _$SymptomTemplatesCopyWith(
+          _SymptomTemplates value, $Res Function(_SymptomTemplates) then) =
+      __$SymptomTemplatesCopyWithImpl<$Res>;
+  @override
+  $Res call(
+      {int symptomTemplateId,
+      StringSingleLine createdBy,
+      DateTime createdDate,
+      StringSingleLine lastModifiedBy,
+      DateTime lastModifiedDate,
+      StringSingleLine symptomName});
+}
+
+/// @nodoc
+class __$SymptomTemplatesCopyWithImpl<$Res>
+    extends _$SymptomTemplatesCopyWithImpl<$Res>
+    implements _$SymptomTemplatesCopyWith<$Res> {
+  __$SymptomTemplatesCopyWithImpl(
+      _SymptomTemplates _value, $Res Function(_SymptomTemplates) _then)
+      : super(_value, (v) => _then(v as _SymptomTemplates));
+
+  @override
+  _SymptomTemplates get _value => super._value as _SymptomTemplates;
+
+  @override
+  $Res call({
+    Object? symptomTemplateId = freezed,
+    Object? createdBy = freezed,
+    Object? createdDate = freezed,
+    Object? lastModifiedBy = freezed,
+    Object? lastModifiedDate = freezed,
+    Object? symptomName = freezed,
+  }) {
+    return _then(_SymptomTemplates(
+      symptomTemplateId: symptomTemplateId == freezed
+          ? _value.symptomTemplateId
+          : symptomTemplateId // ignore: cast_nullable_to_non_nullable
+              as int,
+      createdBy: createdBy == freezed
+          ? _value.createdBy
+          : createdBy // ignore: cast_nullable_to_non_nullable
+              as StringSingleLine,
+      createdDate: createdDate == freezed
+          ? _value.createdDate
+          : createdDate // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      lastModifiedBy: lastModifiedBy == freezed
+          ? _value.lastModifiedBy
+          : lastModifiedBy // ignore: cast_nullable_to_non_nullable
+              as StringSingleLine,
+      lastModifiedDate: lastModifiedDate == freezed
+          ? _value.lastModifiedDate
+          : lastModifiedDate // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      symptomName: symptomName == freezed
+          ? _value.symptomName
+          : symptomName // ignore: cast_nullable_to_non_nullable
+              as StringSingleLine,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$_SymptomTemplates extends _SymptomTemplates {
+  _$_SymptomTemplates(
+      {required this.symptomTemplateId,
+      required this.createdBy,
+      required this.createdDate,
+      required this.lastModifiedBy,
+      required this.lastModifiedDate,
+      required this.symptomName})
+      : super._();
+
+  @override
+  final int symptomTemplateId;
+  @override
+  final StringSingleLine createdBy;
+  @override
+  final DateTime createdDate;
+  @override
+  final StringSingleLine lastModifiedBy;
+  @override
+  final DateTime lastModifiedDate;
+  @override
+  final StringSingleLine symptomName;
+
+  @override
+  String toString() {
+    return 'SymptomTemplates(symptomTemplateId: $symptomTemplateId, createdBy: $createdBy, createdDate: $createdDate, lastModifiedBy: $lastModifiedBy, lastModifiedDate: $lastModifiedDate, symptomName: $symptomName)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other is _SymptomTemplates &&
+            (identical(other.symptomTemplateId, symptomTemplateId) ||
+                const DeepCollectionEquality()
+                    .equals(other.symptomTemplateId, symptomTemplateId)) &&
+            (identical(other.createdBy, createdBy) ||
+                const DeepCollectionEquality()
+                    .equals(other.createdBy, createdBy)) &&
+            (identical(other.createdDate, createdDate) ||
+                const DeepCollectionEquality()
+                    .equals(other.createdDate, createdDate)) &&
+            (identical(other.lastModifiedBy, lastModifiedBy) ||
+                const DeepCollectionEquality()
+                    .equals(other.lastModifiedBy, lastModifiedBy)) &&
+            (identical(other.lastModifiedDate, lastModifiedDate) ||
+                const DeepCollectionEquality()
+                    .equals(other.lastModifiedDate, lastModifiedDate)) &&
+            (identical(other.symptomName, symptomName) ||
+                const DeepCollectionEquality()
+                    .equals(other.symptomName, symptomName)));
+  }
+
+  @override
+  int get hashCode =>
+      runtimeType.hashCode ^
+      const DeepCollectionEquality().hash(symptomTemplateId) ^
+      const DeepCollectionEquality().hash(createdBy) ^
+      const DeepCollectionEquality().hash(createdDate) ^
+      const DeepCollectionEquality().hash(lastModifiedBy) ^
+      const DeepCollectionEquality().hash(lastModifiedDate) ^
+      const DeepCollectionEquality().hash(symptomName);
+
+  @JsonKey(ignore: true)
+  @override
+  _$SymptomTemplatesCopyWith<_SymptomTemplates> get copyWith =>
+      __$SymptomTemplatesCopyWithImpl<_SymptomTemplates>(this, _$identity);
+}
+
+abstract class _SymptomTemplates extends SymptomTemplates {
+  factory _SymptomTemplates(
+      {required int symptomTemplateId,
+      required StringSingleLine createdBy,
+      required DateTime createdDate,
+      required StringSingleLine lastModifiedBy,
+      required DateTime lastModifiedDate,
+      required StringSingleLine symptomName}) = _$_SymptomTemplates;
+  _SymptomTemplates._() : super._();
+
+  @override
+  int get symptomTemplateId => throw _privateConstructorUsedError;
+  @override
+  StringSingleLine get createdBy => throw _privateConstructorUsedError;
+  @override
+  DateTime get createdDate => throw _privateConstructorUsedError;
+  @override
+  StringSingleLine get lastModifiedBy => throw _privateConstructorUsedError;
+  @override
+  DateTime get lastModifiedDate => throw _privateConstructorUsedError;
+  @override
+  StringSingleLine get symptomName => throw _privateConstructorUsedError;
+  @override
+  @JsonKey(ignore: true)
+  _$SymptomTemplatesCopyWith<_SymptomTemplates> get copyWith =>
+      throw _privateConstructorUsedError;
+}

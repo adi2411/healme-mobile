90c4c68e1420e1970aa741bf6ea1b40dd64a71ea
aditya <aditya_jinegar@yahoo.com>
2021-08-13 13:51:32 -0400

    Create healthyEatingTemplatesEducationMaterials.freezed.dart

---
+++ lib/domain/Templates/HealthyEatingTemplates/HealthyEatingTemplatesEducationMaterials/healthyEatingTemplatesEducationMaterials.freezed.dart
@@ -0,0 +1,205 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides
+
+part of 'healthyEatingTemplatesEducationMaterials.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more informations: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+class _$HealthyEatingTemplatesEducationMaterialsTearOff {
+  const _$HealthyEatingTemplatesEducationMaterialsTearOff();
+
+  _HealthyEatingTemplatesEducationMaterials call(
+      {required int healthyEatingTemplatehealthyEatingTemplateId,
+      required int educationMaterialsResourceTemplateId}) {
+    return _HealthyEatingTemplatesEducationMaterials(
+      healthyEatingTemplatehealthyEatingTemplateId:
+          healthyEatingTemplatehealthyEatingTemplateId,
+      educationMaterialsResourceTemplateId:
+          educationMaterialsResourceTemplateId,
+    );
+  }
+}
+
+/// @nodoc
+const $HealthyEatingTemplatesEducationMaterials =
+    _$HealthyEatingTemplatesEducationMaterialsTearOff();
+
+/// @nodoc
+mixin _$HealthyEatingTemplatesEducationMaterials {
+  int get healthyEatingTemplatehealthyEatingTemplateId =>
+      throw _privateConstructorUsedError;
+  int get educationMaterialsResourceTemplateId =>
+      throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $HealthyEatingTemplatesEducationMaterialsCopyWith<
+          HealthyEatingTemplatesEducationMaterials>
+      get copyWith => throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $HealthyEatingTemplatesEducationMaterialsCopyWith<$Res> {
+  factory $HealthyEatingTemplatesEducationMaterialsCopyWith(
+          HealthyEatingTemplatesEducationMaterials value,
+          $Res Function(HealthyEatingTemplatesEducationMaterials) then) =
+      _$HealthyEatingTemplatesEducationMaterialsCopyWithImpl<$Res>;
+  $Res call(
+      {int healthyEatingTemplatehealthyEatingTemplateId,
+      int educationMaterialsResourceTemplateId});
+}
+
+/// @nodoc
+class _$HealthyEatingTemplatesEducationMaterialsCopyWithImpl<$Res>
+    implements $HealthyEatingTemplatesEducationMaterialsCopyWith<$Res> {
+  _$HealthyEatingTemplatesEducationMaterialsCopyWithImpl(
+      this._value, this._then);
+
+  final HealthyEatingTemplatesEducationMaterials _value;
+  // ignore: unused_field
+  final $Res Function(HealthyEatingTemplatesEducationMaterials) _then;
+
+  @override
+  $Res call({
+    Object? healthyEatingTemplatehealthyEatingTemplateId = freezed,
+    Object? educationMaterialsResourceTemplateId = freezed,
+  }) {
+    return _then(_value.copyWith(
+      healthyEatingTemplatehealthyEatingTemplateId:
+          healthyEatingTemplatehealthyEatingTemplateId == freezed
+              ? _value.healthyEatingTemplatehealthyEatingTemplateId
+              : healthyEatingTemplatehealthyEatingTemplateId // ignore: cast_nullable_to_non_nullable
+                  as int,
+      educationMaterialsResourceTemplateId: educationMaterialsResourceTemplateId ==
+              freezed
+          ? _value.educationMaterialsResourceTemplateId
+          : educationMaterialsResourceTemplateId // ignore: cast_nullable_to_non_nullable
+              as int,
+    ));
+  }
+}
+
+/// @nodoc
+abstract class _$HealthyEatingTemplatesEducationMaterialsCopyWith<$Res>
+    implements $HealthyEatingTemplatesEducationMaterialsCopyWith<$Res> {
+  factory _$HealthyEatingTemplatesEducationMaterialsCopyWith(
+          _HealthyEatingTemplatesEducationMaterials value,
+          $Res Function(_HealthyEatingTemplatesEducationMaterials) then) =
+      __$HealthyEatingTemplatesEducationMaterialsCopyWithImpl<$Res>;
+  @override
+  $Res call(
+      {int healthyEatingTemplatehealthyEatingTemplateId,
+      int educationMaterialsResourceTemplateId});
+}
+
+/// @nodoc
+class __$HealthyEatingTemplatesEducationMaterialsCopyWithImpl<$Res>
+    extends _$HealthyEatingTemplatesEducationMaterialsCopyWithImpl<$Res>
+    implements _$HealthyEatingTemplatesEducationMaterialsCopyWith<$Res> {
+  __$HealthyEatingTemplatesEducationMaterialsCopyWithImpl(
+      _HealthyEatingTemplatesEducationMaterials _value,
+      $Res Function(_HealthyEatingTemplatesEducationMaterials) _then)
+      : super(_value,
+            (v) => _then(v as _HealthyEatingTemplatesEducationMaterials));
+
+  @override
+  _HealthyEatingTemplatesEducationMaterials get _value =>
+      super._value as _HealthyEatingTemplatesEducationMaterials;
+
+  @override
+  $Res call({
+    Object? healthyEatingTemplatehealthyEatingTemplateId = freezed,
+    Object? educationMaterialsResourceTemplateId = freezed,
+  }) {
+    return _then(_HealthyEatingTemplatesEducationMaterials(
+      healthyEatingTemplatehealthyEatingTemplateId:
+          healthyEatingTemplatehealthyEatingTemplateId == freezed
+              ? _value.healthyEatingTemplatehealthyEatingTemplateId
+              : healthyEatingTemplatehealthyEatingTemplateId // ignore: cast_nullable_to_non_nullable
+                  as int,
+      educationMaterialsResourceTemplateId: educationMaterialsResourceTemplateId ==
+              freezed
+          ? _value.educationMaterialsResourceTemplateId
+          : educationMaterialsResourceTemplateId // ignore: cast_nullable_to_non_nullable
+              as int,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$_HealthyEatingTemplatesEducationMaterials
+    extends _HealthyEatingTemplatesEducationMaterials {
+  _$_HealthyEatingTemplatesEducationMaterials(
+      {required this.healthyEatingTemplatehealthyEatingTemplateId,
+      required this.educationMaterialsResourceTemplateId})
+      : super._();
+
+  @override
+  final int healthyEatingTemplatehealthyEatingTemplateId;
+  @override
+  final int educationMaterialsResourceTemplateId;
+
+  @override
+  String toString() {
+    return 'HealthyEatingTemplatesEducationMaterials(healthyEatingTemplatehealthyEatingTemplateId: $healthyEatingTemplatehealthyEatingTemplateId, educationMaterialsResourceTemplateId: $educationMaterialsResourceTemplateId)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other is _HealthyEatingTemplatesEducationMaterials &&
+            (identical(other.healthyEatingTemplatehealthyEatingTemplateId,
+                    healthyEatingTemplatehealthyEatingTemplateId) ||
+                const DeepCollectionEquality().equals(
+                    other.healthyEatingTemplatehealthyEatingTemplateId,
+                    healthyEatingTemplatehealthyEatingTemplateId)) &&
+            (identical(other.educationMaterialsResourceTemplateId,
+                    educationMaterialsResourceTemplateId) ||
+                const DeepCollectionEquality().equals(
+                    other.educationMaterialsResourceTemplateId,
+                    educationMaterialsResourceTemplateId)));
+  }
+
+  @override
+  int get hashCode =>
+      runtimeType.hashCode ^
+      const DeepCollectionEquality()
+          .hash(healthyEatingTemplatehealthyEatingTemplateId) ^
+      const DeepCollectionEquality().hash(educationMaterialsResourceTemplateId);
+
+  @JsonKey(ignore: true)
+  @override
+  _$HealthyEatingTemplatesEducationMaterialsCopyWith<
+          _HealthyEatingTemplatesEducationMaterials>
+      get copyWith => __$HealthyEatingTemplatesEducationMaterialsCopyWithImpl<
+          _HealthyEatingTemplatesEducationMaterials>(this, _$identity);
+}
+
+abstract class _HealthyEatingTemplatesEducationMaterials
+    extends HealthyEatingTemplatesEducationMaterials {
+  factory _HealthyEatingTemplatesEducationMaterials(
+          {required int healthyEatingTemplatehealthyEatingTemplateId,
+          required int educationMaterialsResourceTemplateId}) =
+      _$_HealthyEatingTemplatesEducationMaterials;
+  _HealthyEatingTemplatesEducationMaterials._() : super._();
+
+  @override
+  int get healthyEatingTemplatehealthyEatingTemplateId =>
+      throw _privateConstructorUsedError;
+  @override
+  int get educationMaterialsResourceTemplateId =>
+      throw _privateConstructorUsedError;
+  @override
+  @JsonKey(ignore: true)
+  _$HealthyEatingTemplatesEducationMaterialsCopyWith<
+          _HealthyEatingTemplatesEducationMaterials>
+      get copyWith => throw _privateConstructorUsedError;
+}

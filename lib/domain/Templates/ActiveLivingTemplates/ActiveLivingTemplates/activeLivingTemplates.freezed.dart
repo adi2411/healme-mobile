9c3c8cf39e27d1cba9b0b0788ada5b5d707870b5
aditya <aditya_jinegar@yahoo.com>
2021-08-13 13:50:43 -0400

    Create activeLivingTemplates.freezed.dart

---
+++ lib/domain/Templates/ActiveLivingTemplates/ActiveLivingTemplates/activeLivingTemplates.freezed.dart
@@ -0,0 +1,379 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides
+
+part of 'activeLivingTemplates.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more informations: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+class _$ActiveLivingTemplatesTearOff {
+  const _$ActiveLivingTemplatesTearOff();
+
+  _ActiveLivingTemplates call(
+      {required int activeLivingTemplateId,
+      required StringSingleLine createdBy,
+      required DateTime createdDate,
+      required StringSingleLine lastModifiedBy,
+      required DateTime lastModifiedDate,
+      required int active,
+      required StringMultiLine additionalInstructions,
+      required int groupExerciseSessions,
+      required int independentExerciseSessions,
+      required int steps}) {
+    return _ActiveLivingTemplates(
+      activeLivingTemplateId: activeLivingTemplateId,
+      createdBy: createdBy,
+      createdDate: createdDate,
+      lastModifiedBy: lastModifiedBy,
+      lastModifiedDate: lastModifiedDate,
+      active: active,
+      additionalInstructions: additionalInstructions,
+      groupExerciseSessions: groupExerciseSessions,
+      independentExerciseSessions: independentExerciseSessions,
+      steps: steps,
+    );
+  }
+}
+
+/// @nodoc
+const $ActiveLivingTemplates = _$ActiveLivingTemplatesTearOff();
+
+/// @nodoc
+mixin _$ActiveLivingTemplates {
+  int get activeLivingTemplateId => throw _privateConstructorUsedError;
+  StringSingleLine get createdBy => throw _privateConstructorUsedError;
+  DateTime get createdDate => throw _privateConstructorUsedError;
+  StringSingleLine get lastModifiedBy => throw _privateConstructorUsedError;
+  DateTime get lastModifiedDate => throw _privateConstructorUsedError;
+  int get active => throw _privateConstructorUsedError;
+  StringMultiLine get additionalInstructions =>
+      throw _privateConstructorUsedError;
+  int get groupExerciseSessions => throw _privateConstructorUsedError;
+  int get independentExerciseSessions => throw _privateConstructorUsedError;
+  int get steps => throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $ActiveLivingTemplatesCopyWith<ActiveLivingTemplates> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $ActiveLivingTemplatesCopyWith<$Res> {
+  factory $ActiveLivingTemplatesCopyWith(ActiveLivingTemplates value,
+          $Res Function(ActiveLivingTemplates) then) =
+      _$ActiveLivingTemplatesCopyWithImpl<$Res>;
+  $Res call(
+      {int activeLivingTemplateId,
+      StringSingleLine createdBy,
+      DateTime createdDate,
+      StringSingleLine lastModifiedBy,
+      DateTime lastModifiedDate,
+      int active,
+      StringMultiLine additionalInstructions,
+      int groupExerciseSessions,
+      int independentExerciseSessions,
+      int steps});
+}
+
+/// @nodoc
+class _$ActiveLivingTemplatesCopyWithImpl<$Res>
+    implements $ActiveLivingTemplatesCopyWith<$Res> {
+  _$ActiveLivingTemplatesCopyWithImpl(this._value, this._then);
+
+  final ActiveLivingTemplates _value;
+  // ignore: unused_field
+  final $Res Function(ActiveLivingTemplates) _then;
+
+  @override
+  $Res call({
+    Object? activeLivingTemplateId = freezed,
+    Object? createdBy = freezed,
+    Object? createdDate = freezed,
+    Object? lastModifiedBy = freezed,
+    Object? lastModifiedDate = freezed,
+    Object? active = freezed,
+    Object? additionalInstructions = freezed,
+    Object? groupExerciseSessions = freezed,
+    Object? independentExerciseSessions = freezed,
+    Object? steps = freezed,
+  }) {
+    return _then(_value.copyWith(
+      activeLivingTemplateId: activeLivingTemplateId == freezed
+          ? _value.activeLivingTemplateId
+          : activeLivingTemplateId // ignore: cast_nullable_to_non_nullable
+              as int,
+      createdBy: createdBy == freezed
+          ? _value.createdBy
+          : createdBy // ignore: cast_nullable_to_non_nullable
+              as StringSingleLine,
+      createdDate: createdDate == freezed
+          ? _value.createdDate
+          : createdDate // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      lastModifiedBy: lastModifiedBy == freezed
+          ? _value.lastModifiedBy
+          : lastModifiedBy // ignore: cast_nullable_to_non_nullable
+              as StringSingleLine,
+      lastModifiedDate: lastModifiedDate == freezed
+          ? _value.lastModifiedDate
+          : lastModifiedDate // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      active: active == freezed
+          ? _value.active
+          : active // ignore: cast_nullable_to_non_nullable
+              as int,
+      additionalInstructions: additionalInstructions == freezed
+          ? _value.additionalInstructions
+          : additionalInstructions // ignore: cast_nullable_to_non_nullable
+              as StringMultiLine,
+      groupExerciseSessions: groupExerciseSessions == freezed
+          ? _value.groupExerciseSessions
+          : groupExerciseSessions // ignore: cast_nullable_to_non_nullable
+              as int,
+      independentExerciseSessions: independentExerciseSessions == freezed
+          ? _value.independentExerciseSessions
+          : independentExerciseSessions // ignore: cast_nullable_to_non_nullable
+              as int,
+      steps: steps == freezed
+          ? _value.steps
+          : steps // ignore: cast_nullable_to_non_nullable
+              as int,
+    ));
+  }
+}
+
+/// @nodoc
+abstract class _$ActiveLivingTemplatesCopyWith<$Res>
+    implements $ActiveLivingTemplatesCopyWith<$Res> {
+  factory _$ActiveLivingTemplatesCopyWith(_ActiveLivingTemplates value,
+          $Res Function(_ActiveLivingTemplates) then) =
+      __$ActiveLivingTemplatesCopyWithImpl<$Res>;
+  @override
+  $Res call(
+      {int activeLivingTemplateId,
+      StringSingleLine createdBy,
+      DateTime createdDate,
+      StringSingleLine lastModifiedBy,
+      DateTime lastModifiedDate,
+      int active,
+      StringMultiLine additionalInstructions,
+      int groupExerciseSessions,
+      int independentExerciseSessions,
+      int steps});
+}
+
+/// @nodoc
+class __$ActiveLivingTemplatesCopyWithImpl<$Res>
+    extends _$ActiveLivingTemplatesCopyWithImpl<$Res>
+    implements _$ActiveLivingTemplatesCopyWith<$Res> {
+  __$ActiveLivingTemplatesCopyWithImpl(_ActiveLivingTemplates _value,
+      $Res Function(_ActiveLivingTemplates) _then)
+      : super(_value, (v) => _then(v as _ActiveLivingTemplates));
+
+  @override
+  _ActiveLivingTemplates get _value => super._value as _ActiveLivingTemplates;
+
+  @override
+  $Res call({
+    Object? activeLivingTemplateId = freezed,
+    Object? createdBy = freezed,
+    Object? createdDate = freezed,
+    Object? lastModifiedBy = freezed,
+    Object? lastModifiedDate = freezed,
+    Object? active = freezed,
+    Object? additionalInstructions = freezed,
+    Object? groupExerciseSessions = freezed,
+    Object? independentExerciseSessions = freezed,
+    Object? steps = freezed,
+  }) {
+    return _then(_ActiveLivingTemplates(
+      activeLivingTemplateId: activeLivingTemplateId == freezed
+          ? _value.activeLivingTemplateId
+          : activeLivingTemplateId // ignore: cast_nullable_to_non_nullable
+              as int,
+      createdBy: createdBy == freezed
+          ? _value.createdBy
+          : createdBy // ignore: cast_nullable_to_non_nullable
+              as StringSingleLine,
+      createdDate: createdDate == freezed
+          ? _value.createdDate
+          : createdDate // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      lastModifiedBy: lastModifiedBy == freezed
+          ? _value.lastModifiedBy
+          : lastModifiedBy // ignore: cast_nullable_to_non_nullable
+              as StringSingleLine,
+      lastModifiedDate: lastModifiedDate == freezed
+          ? _value.lastModifiedDate
+          : lastModifiedDate // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      active: active == freezed
+          ? _value.active
+          : active // ignore: cast_nullable_to_non_nullable
+              as int,
+      additionalInstructions: additionalInstructions == freezed
+          ? _value.additionalInstructions
+          : additionalInstructions // ignore: cast_nullable_to_non_nullable
+              as StringMultiLine,
+      groupExerciseSessions: groupExerciseSessions == freezed
+          ? _value.groupExerciseSessions
+          : groupExerciseSessions // ignore: cast_nullable_to_non_nullable
+              as int,
+      independentExerciseSessions: independentExerciseSessions == freezed
+          ? _value.independentExerciseSessions
+          : independentExerciseSessions // ignore: cast_nullable_to_non_nullable
+              as int,
+      steps: steps == freezed
+          ? _value.steps
+          : steps // ignore: cast_nullable_to_non_nullable
+              as int,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$_ActiveLivingTemplates extends _ActiveLivingTemplates {
+  _$_ActiveLivingTemplates(
+      {required this.activeLivingTemplateId,
+      required this.createdBy,
+      required this.createdDate,
+      required this.lastModifiedBy,
+      required this.lastModifiedDate,
+      required this.active,
+      required this.additionalInstructions,
+      required this.groupExerciseSessions,
+      required this.independentExerciseSessions,
+      required this.steps})
+      : super._();
+
+  @override
+  final int activeLivingTemplateId;
+  @override
+  final StringSingleLine createdBy;
+  @override
+  final DateTime createdDate;
+  @override
+  final StringSingleLine lastModifiedBy;
+  @override
+  final DateTime lastModifiedDate;
+  @override
+  final int active;
+  @override
+  final StringMultiLine additionalInstructions;
+  @override
+  final int groupExerciseSessions;
+  @override
+  final int independentExerciseSessions;
+  @override
+  final int steps;
+
+  @override
+  String toString() {
+    return 'ActiveLivingTemplates(activeLivingTemplateId: $activeLivingTemplateId, createdBy: $createdBy, createdDate: $createdDate, lastModifiedBy: $lastModifiedBy, lastModifiedDate: $lastModifiedDate, active: $active, additionalInstructions: $additionalInstructions, groupExerciseSessions: $groupExerciseSessions, independentExerciseSessions: $independentExerciseSessions, steps: $steps)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other is _ActiveLivingTemplates &&
+            (identical(other.activeLivingTemplateId, activeLivingTemplateId) ||
+                const DeepCollectionEquality().equals(
+                    other.activeLivingTemplateId, activeLivingTemplateId)) &&
+            (identical(other.createdBy, createdBy) ||
+                const DeepCollectionEquality()
+                    .equals(other.createdBy, createdBy)) &&
+            (identical(other.createdDate, createdDate) ||
+                const DeepCollectionEquality()
+                    .equals(other.createdDate, createdDate)) &&
+            (identical(other.lastModifiedBy, lastModifiedBy) ||
+                const DeepCollectionEquality()
+                    .equals(other.lastModifiedBy, lastModifiedBy)) &&
+            (identical(other.lastModifiedDate, lastModifiedDate) ||
+                const DeepCollectionEquality()
+                    .equals(other.lastModifiedDate, lastModifiedDate)) &&
+            (identical(other.active, active) ||
+                const DeepCollectionEquality().equals(other.active, active)) &&
+            (identical(other.additionalInstructions, additionalInstructions) ||
+                const DeepCollectionEquality().equals(
+                    other.additionalInstructions, additionalInstructions)) &&
+            (identical(other.groupExerciseSessions, groupExerciseSessions) ||
+                const DeepCollectionEquality().equals(
+                    other.groupExerciseSessions, groupExerciseSessions)) &&
+            (identical(other.independentExerciseSessions,
+                    independentExerciseSessions) ||
+                const DeepCollectionEquality().equals(
+                    other.independentExerciseSessions,
+                    independentExerciseSessions)) &&
+            (identical(other.steps, steps) ||
+                const DeepCollectionEquality().equals(other.steps, steps)));
+  }
+
+  @override
+  int get hashCode =>
+      runtimeType.hashCode ^
+      const DeepCollectionEquality().hash(activeLivingTemplateId) ^
+      const DeepCollectionEquality().hash(createdBy) ^
+      const DeepCollectionEquality().hash(createdDate) ^
+      const DeepCollectionEquality().hash(lastModifiedBy) ^
+      const DeepCollectionEquality().hash(lastModifiedDate) ^
+      const DeepCollectionEquality().hash(active) ^
+      const DeepCollectionEquality().hash(additionalInstructions) ^
+      const DeepCollectionEquality().hash(groupExerciseSessions) ^
+      const DeepCollectionEquality().hash(independentExerciseSessions) ^
+      const DeepCollectionEquality().hash(steps);
+
+  @JsonKey(ignore: true)
+  @override
+  _$ActiveLivingTemplatesCopyWith<_ActiveLivingTemplates> get copyWith =>
+      __$ActiveLivingTemplatesCopyWithImpl<_ActiveLivingTemplates>(
+          this, _$identity);
+}
+
+abstract class _ActiveLivingTemplates extends ActiveLivingTemplates {
+  factory _ActiveLivingTemplates(
+      {required int activeLivingTemplateId,
+      required StringSingleLine createdBy,
+      required DateTime createdDate,
+      required StringSingleLine lastModifiedBy,
+      required DateTime lastModifiedDate,
+      required int active,
+      required StringMultiLine additionalInstructions,
+      required int groupExerciseSessions,
+      required int independentExerciseSessions,
+      required int steps}) = _$_ActiveLivingTemplates;
+  _ActiveLivingTemplates._() : super._();
+
+  @override
+  int get activeLivingTemplateId => throw _privateConstructorUsedError;
+  @override
+  StringSingleLine get createdBy => throw _privateConstructorUsedError;
+  @override
+  DateTime get createdDate => throw _privateConstructorUsedError;
+  @override
+  StringSingleLine get lastModifiedBy => throw _privateConstructorUsedError;
+  @override
+  DateTime get lastModifiedDate => throw _privateConstructorUsedError;
+  @override
+  int get active => throw _privateConstructorUsedError;
+  @override
+  StringMultiLine get additionalInstructions =>
+      throw _privateConstructorUsedError;
+  @override
+  int get groupExerciseSessions => throw _privateConstructorUsedError;
+  @override
+  int get independentExerciseSessions => throw _privateConstructorUsedError;
+  @override
+  int get steps => throw _privateConstructorUsedError;
+  @override
+  @JsonKey(ignore: true)
+  _$ActiveLivingTemplatesCopyWith<_ActiveLivingTemplates> get copyWith =>
+      throw _privateConstructorUsedError;
+}

a3371a594f9c11b88767ec19651ad269ea839823
aditya <aditya_jinegar@yahoo.com>
2021-08-13 12:09:01 -0400

    Create programTemplatesIntakeSurvey.freezed.dart

---
+++ lib/domain/Templates/ProgramTemplates/ProgramTemplatesIntakeSurvey/programTemplatesIntakeSurvey.freezed.dart
@@ -0,0 +1,189 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides
+
+part of 'programTemplatesIntakeSurvey.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more informations: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+class _$ProgramTemplatesIntakeSurveyTearOff {
+  const _$ProgramTemplatesIntakeSurveyTearOff();
+
+  _ProgramTemplatesIntakeSurvey call(
+      {required int programTemplateProgramTemplateId,
+      required int intakeSurveySurveyTemplateId}) {
+    return _ProgramTemplatesIntakeSurvey(
+      programTemplateProgramTemplateId: programTemplateProgramTemplateId,
+      intakeSurveySurveyTemplateId: intakeSurveySurveyTemplateId,
+    );
+  }
+}
+
+/// @nodoc
+const $ProgramTemplatesIntakeSurvey = _$ProgramTemplatesIntakeSurveyTearOff();
+
+/// @nodoc
+mixin _$ProgramTemplatesIntakeSurvey {
+  int get programTemplateProgramTemplateId =>
+      throw _privateConstructorUsedError;
+  int get intakeSurveySurveyTemplateId => throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $ProgramTemplatesIntakeSurveyCopyWith<ProgramTemplatesIntakeSurvey>
+      get copyWith => throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $ProgramTemplatesIntakeSurveyCopyWith<$Res> {
+  factory $ProgramTemplatesIntakeSurveyCopyWith(
+          ProgramTemplatesIntakeSurvey value,
+          $Res Function(ProgramTemplatesIntakeSurvey) then) =
+      _$ProgramTemplatesIntakeSurveyCopyWithImpl<$Res>;
+  $Res call(
+      {int programTemplateProgramTemplateId, int intakeSurveySurveyTemplateId});
+}
+
+/// @nodoc
+class _$ProgramTemplatesIntakeSurveyCopyWithImpl<$Res>
+    implements $ProgramTemplatesIntakeSurveyCopyWith<$Res> {
+  _$ProgramTemplatesIntakeSurveyCopyWithImpl(this._value, this._then);
+
+  final ProgramTemplatesIntakeSurvey _value;
+  // ignore: unused_field
+  final $Res Function(ProgramTemplatesIntakeSurvey) _then;
+
+  @override
+  $Res call({
+    Object? programTemplateProgramTemplateId = freezed,
+    Object? intakeSurveySurveyTemplateId = freezed,
+  }) {
+    return _then(_value.copyWith(
+      programTemplateProgramTemplateId: programTemplateProgramTemplateId ==
+              freezed
+          ? _value.programTemplateProgramTemplateId
+          : programTemplateProgramTemplateId // ignore: cast_nullable_to_non_nullable
+              as int,
+      intakeSurveySurveyTemplateId: intakeSurveySurveyTemplateId == freezed
+          ? _value.intakeSurveySurveyTemplateId
+          : intakeSurveySurveyTemplateId // ignore: cast_nullable_to_non_nullable
+              as int,
+    ));
+  }
+}
+
+/// @nodoc
+abstract class _$ProgramTemplatesIntakeSurveyCopyWith<$Res>
+    implements $ProgramTemplatesIntakeSurveyCopyWith<$Res> {
+  factory _$ProgramTemplatesIntakeSurveyCopyWith(
+          _ProgramTemplatesIntakeSurvey value,
+          $Res Function(_ProgramTemplatesIntakeSurvey) then) =
+      __$ProgramTemplatesIntakeSurveyCopyWithImpl<$Res>;
+  @override
+  $Res call(
+      {int programTemplateProgramTemplateId, int intakeSurveySurveyTemplateId});
+}
+
+/// @nodoc
+class __$ProgramTemplatesIntakeSurveyCopyWithImpl<$Res>
+    extends _$ProgramTemplatesIntakeSurveyCopyWithImpl<$Res>
+    implements _$ProgramTemplatesIntakeSurveyCopyWith<$Res> {
+  __$ProgramTemplatesIntakeSurveyCopyWithImpl(
+      _ProgramTemplatesIntakeSurvey _value,
+      $Res Function(_ProgramTemplatesIntakeSurvey) _then)
+      : super(_value, (v) => _then(v as _ProgramTemplatesIntakeSurvey));
+
+  @override
+  _ProgramTemplatesIntakeSurvey get _value =>
+      super._value as _ProgramTemplatesIntakeSurvey;
+
+  @override
+  $Res call({
+    Object? programTemplateProgramTemplateId = freezed,
+    Object? intakeSurveySurveyTemplateId = freezed,
+  }) {
+    return _then(_ProgramTemplatesIntakeSurvey(
+      programTemplateProgramTemplateId: programTemplateProgramTemplateId ==
+              freezed
+          ? _value.programTemplateProgramTemplateId
+          : programTemplateProgramTemplateId // ignore: cast_nullable_to_non_nullable
+              as int,
+      intakeSurveySurveyTemplateId: intakeSurveySurveyTemplateId == freezed
+          ? _value.intakeSurveySurveyTemplateId
+          : intakeSurveySurveyTemplateId // ignore: cast_nullable_to_non_nullable
+              as int,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$_ProgramTemplatesIntakeSurvey extends _ProgramTemplatesIntakeSurvey {
+  _$_ProgramTemplatesIntakeSurvey(
+      {required this.programTemplateProgramTemplateId,
+      required this.intakeSurveySurveyTemplateId})
+      : super._();
+
+  @override
+  final int programTemplateProgramTemplateId;
+  @override
+  final int intakeSurveySurveyTemplateId;
+
+  @override
+  String toString() {
+    return 'ProgramTemplatesIntakeSurvey(programTemplateProgramTemplateId: $programTemplateProgramTemplateId, intakeSurveySurveyTemplateId: $intakeSurveySurveyTemplateId)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other is _ProgramTemplatesIntakeSurvey &&
+            (identical(other.programTemplateProgramTemplateId,
+                    programTemplateProgramTemplateId) ||
+                const DeepCollectionEquality().equals(
+                    other.programTemplateProgramTemplateId,
+                    programTemplateProgramTemplateId)) &&
+            (identical(other.intakeSurveySurveyTemplateId,
+                    intakeSurveySurveyTemplateId) ||
+                const DeepCollectionEquality().equals(
+                    other.intakeSurveySurveyTemplateId,
+                    intakeSurveySurveyTemplateId)));
+  }
+
+  @override
+  int get hashCode =>
+      runtimeType.hashCode ^
+      const DeepCollectionEquality().hash(programTemplateProgramTemplateId) ^
+      const DeepCollectionEquality().hash(intakeSurveySurveyTemplateId);
+
+  @JsonKey(ignore: true)
+  @override
+  _$ProgramTemplatesIntakeSurveyCopyWith<_ProgramTemplatesIntakeSurvey>
+      get copyWith => __$ProgramTemplatesIntakeSurveyCopyWithImpl<
+          _ProgramTemplatesIntakeSurvey>(this, _$identity);
+}
+
+abstract class _ProgramTemplatesIntakeSurvey
+    extends ProgramTemplatesIntakeSurvey {
+  factory _ProgramTemplatesIntakeSurvey(
+          {required int programTemplateProgramTemplateId,
+          required int intakeSurveySurveyTemplateId}) =
+      _$_ProgramTemplatesIntakeSurvey;
+  _ProgramTemplatesIntakeSurvey._() : super._();
+
+  @override
+  int get programTemplateProgramTemplateId =>
+      throw _privateConstructorUsedError;
+  @override
+  int get intakeSurveySurveyTemplateId => throw _privateConstructorUsedError;
+  @override
+  @JsonKey(ignore: true)
+  _$ProgramTemplatesIntakeSurveyCopyWith<_ProgramTemplatesIntakeSurvey>
+      get copyWith => throw _privateConstructorUsedError;
+}

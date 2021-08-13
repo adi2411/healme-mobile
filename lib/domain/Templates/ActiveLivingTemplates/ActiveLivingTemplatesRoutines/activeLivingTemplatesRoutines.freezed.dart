54f58cef24445a7e03cf88b7be0d84f506252eb3
aditya <aditya_jinegar@yahoo.com>
2021-08-13 11:35:38 -0400

    Create activeLivingTemplatesRoutines.freezed.dart

---
+++ lib/domain/Templates/ActiveLivingTemplates/ActiveLivingTemplatesRoutines/activeLivingTemplatesRoutines.freezed.dart
@@ -0,0 +1,193 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides
+
+part of 'activeLivingTemplatesRoutines.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more informations: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+class _$ActiveLivingTemplatesRoutinesTearOff {
+  const _$ActiveLivingTemplatesRoutinesTearOff();
+
+  _ActiveLivingTemplatesRoutines call(
+      {required int activeLivingTemplateActiveLivingTemplateId,
+      required int routinesRoutineTemplateId}) {
+    return _ActiveLivingTemplatesRoutines(
+      activeLivingTemplateActiveLivingTemplateId:
+          activeLivingTemplateActiveLivingTemplateId,
+      routinesRoutineTemplateId: routinesRoutineTemplateId,
+    );
+  }
+}
+
+/// @nodoc
+const $ActiveLivingTemplatesRoutines = _$ActiveLivingTemplatesRoutinesTearOff();
+
+/// @nodoc
+mixin _$ActiveLivingTemplatesRoutines {
+  int get activeLivingTemplateActiveLivingTemplateId =>
+      throw _privateConstructorUsedError;
+  int get routinesRoutineTemplateId => throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $ActiveLivingTemplatesRoutinesCopyWith<ActiveLivingTemplatesRoutines>
+      get copyWith => throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $ActiveLivingTemplatesRoutinesCopyWith<$Res> {
+  factory $ActiveLivingTemplatesRoutinesCopyWith(
+          ActiveLivingTemplatesRoutines value,
+          $Res Function(ActiveLivingTemplatesRoutines) then) =
+      _$ActiveLivingTemplatesRoutinesCopyWithImpl<$Res>;
+  $Res call(
+      {int activeLivingTemplateActiveLivingTemplateId,
+      int routinesRoutineTemplateId});
+}
+
+/// @nodoc
+class _$ActiveLivingTemplatesRoutinesCopyWithImpl<$Res>
+    implements $ActiveLivingTemplatesRoutinesCopyWith<$Res> {
+  _$ActiveLivingTemplatesRoutinesCopyWithImpl(this._value, this._then);
+
+  final ActiveLivingTemplatesRoutines _value;
+  // ignore: unused_field
+  final $Res Function(ActiveLivingTemplatesRoutines) _then;
+
+  @override
+  $Res call({
+    Object? activeLivingTemplateActiveLivingTemplateId = freezed,
+    Object? routinesRoutineTemplateId = freezed,
+  }) {
+    return _then(_value.copyWith(
+      activeLivingTemplateActiveLivingTemplateId:
+          activeLivingTemplateActiveLivingTemplateId == freezed
+              ? _value.activeLivingTemplateActiveLivingTemplateId
+              : activeLivingTemplateActiveLivingTemplateId // ignore: cast_nullable_to_non_nullable
+                  as int,
+      routinesRoutineTemplateId: routinesRoutineTemplateId == freezed
+          ? _value.routinesRoutineTemplateId
+          : routinesRoutineTemplateId // ignore: cast_nullable_to_non_nullable
+              as int,
+    ));
+  }
+}
+
+/// @nodoc
+abstract class _$ActiveLivingTemplatesRoutinesCopyWith<$Res>
+    implements $ActiveLivingTemplatesRoutinesCopyWith<$Res> {
+  factory _$ActiveLivingTemplatesRoutinesCopyWith(
+          _ActiveLivingTemplatesRoutines value,
+          $Res Function(_ActiveLivingTemplatesRoutines) then) =
+      __$ActiveLivingTemplatesRoutinesCopyWithImpl<$Res>;
+  @override
+  $Res call(
+      {int activeLivingTemplateActiveLivingTemplateId,
+      int routinesRoutineTemplateId});
+}
+
+/// @nodoc
+class __$ActiveLivingTemplatesRoutinesCopyWithImpl<$Res>
+    extends _$ActiveLivingTemplatesRoutinesCopyWithImpl<$Res>
+    implements _$ActiveLivingTemplatesRoutinesCopyWith<$Res> {
+  __$ActiveLivingTemplatesRoutinesCopyWithImpl(
+      _ActiveLivingTemplatesRoutines _value,
+      $Res Function(_ActiveLivingTemplatesRoutines) _then)
+      : super(_value, (v) => _then(v as _ActiveLivingTemplatesRoutines));
+
+  @override
+  _ActiveLivingTemplatesRoutines get _value =>
+      super._value as _ActiveLivingTemplatesRoutines;
+
+  @override
+  $Res call({
+    Object? activeLivingTemplateActiveLivingTemplateId = freezed,
+    Object? routinesRoutineTemplateId = freezed,
+  }) {
+    return _then(_ActiveLivingTemplatesRoutines(
+      activeLivingTemplateActiveLivingTemplateId:
+          activeLivingTemplateActiveLivingTemplateId == freezed
+              ? _value.activeLivingTemplateActiveLivingTemplateId
+              : activeLivingTemplateActiveLivingTemplateId // ignore: cast_nullable_to_non_nullable
+                  as int,
+      routinesRoutineTemplateId: routinesRoutineTemplateId == freezed
+          ? _value.routinesRoutineTemplateId
+          : routinesRoutineTemplateId // ignore: cast_nullable_to_non_nullable
+              as int,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$_ActiveLivingTemplatesRoutines extends _ActiveLivingTemplatesRoutines {
+  _$_ActiveLivingTemplatesRoutines(
+      {required this.activeLivingTemplateActiveLivingTemplateId,
+      required this.routinesRoutineTemplateId})
+      : super._();
+
+  @override
+  final int activeLivingTemplateActiveLivingTemplateId;
+  @override
+  final int routinesRoutineTemplateId;
+
+  @override
+  String toString() {
+    return 'ActiveLivingTemplatesRoutines(activeLivingTemplateActiveLivingTemplateId: $activeLivingTemplateActiveLivingTemplateId, routinesRoutineTemplateId: $routinesRoutineTemplateId)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other is _ActiveLivingTemplatesRoutines &&
+            (identical(other.activeLivingTemplateActiveLivingTemplateId,
+                    activeLivingTemplateActiveLivingTemplateId) ||
+                const DeepCollectionEquality().equals(
+                    other.activeLivingTemplateActiveLivingTemplateId,
+                    activeLivingTemplateActiveLivingTemplateId)) &&
+            (identical(other.routinesRoutineTemplateId,
+                    routinesRoutineTemplateId) ||
+                const DeepCollectionEquality().equals(
+                    other.routinesRoutineTemplateId,
+                    routinesRoutineTemplateId)));
+  }
+
+  @override
+  int get hashCode =>
+      runtimeType.hashCode ^
+      const DeepCollectionEquality()
+          .hash(activeLivingTemplateActiveLivingTemplateId) ^
+      const DeepCollectionEquality().hash(routinesRoutineTemplateId);
+
+  @JsonKey(ignore: true)
+  @override
+  _$ActiveLivingTemplatesRoutinesCopyWith<_ActiveLivingTemplatesRoutines>
+      get copyWith => __$ActiveLivingTemplatesRoutinesCopyWithImpl<
+          _ActiveLivingTemplatesRoutines>(this, _$identity);
+}
+
+abstract class _ActiveLivingTemplatesRoutines
+    extends ActiveLivingTemplatesRoutines {
+  factory _ActiveLivingTemplatesRoutines(
+          {required int activeLivingTemplateActiveLivingTemplateId,
+          required int routinesRoutineTemplateId}) =
+      _$_ActiveLivingTemplatesRoutines;
+  _ActiveLivingTemplatesRoutines._() : super._();
+
+  @override
+  int get activeLivingTemplateActiveLivingTemplateId =>
+      throw _privateConstructorUsedError;
+  @override
+  int get routinesRoutineTemplateId => throw _privateConstructorUsedError;
+  @override
+  @JsonKey(ignore: true)
+  _$ActiveLivingTemplatesRoutinesCopyWith<_ActiveLivingTemplatesRoutines>
+      get copyWith => throw _privateConstructorUsedError;
+}

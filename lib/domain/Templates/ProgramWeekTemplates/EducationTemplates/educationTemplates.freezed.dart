4e5aaa0a13b12ee4df365e781dcc1bc80c8b2aa9
aditya <aditya_jinegar@yahoo.com>
2021-08-13 12:09:11 -0400

    Create educationTemplates.freezed.dart

---
+++ lib/domain/Templates/ProgramWeekTemplates/EducationTemplates/educationTemplates.freezed.dart
@@ -0,0 +1,276 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides
+
+part of 'educationTemplates.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more informations: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+class _$EducationTemplatesTearOff {
+  const _$EducationTemplatesTearOff();
+
+  _EducationTemplates call(
+      {required int educationTemplateId,
+      required StringSingleLine createdBy,
+      required DateTime createdDate,
+      required StringSingleLine lastModifiedBy,
+      required DateTime lastModifiedDate,
+      required StringMultiLine educationDesc}) {
+    return _EducationTemplates(
+      educationTemplateId: educationTemplateId,
+      createdBy: createdBy,
+      createdDate: createdDate,
+      lastModifiedBy: lastModifiedBy,
+      lastModifiedDate: lastModifiedDate,
+      educationDesc: educationDesc,
+    );
+  }
+}
+
+/// @nodoc
+const $EducationTemplates = _$EducationTemplatesTearOff();
+
+/// @nodoc
+mixin _$EducationTemplates {
+  int get educationTemplateId => throw _privateConstructorUsedError;
+  StringSingleLine get createdBy => throw _privateConstructorUsedError;
+  DateTime get createdDate => throw _privateConstructorUsedError;
+  StringSingleLine get lastModifiedBy => throw _privateConstructorUsedError;
+  DateTime get lastModifiedDate => throw _privateConstructorUsedError;
+  StringMultiLine get educationDesc => throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $EducationTemplatesCopyWith<EducationTemplates> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $EducationTemplatesCopyWith<$Res> {
+  factory $EducationTemplatesCopyWith(
+          EducationTemplates value, $Res Function(EducationTemplates) then) =
+      _$EducationTemplatesCopyWithImpl<$Res>;
+  $Res call(
+      {int educationTemplateId,
+      StringSingleLine createdBy,
+      DateTime createdDate,
+      StringSingleLine lastModifiedBy,
+      DateTime lastModifiedDate,
+      StringMultiLine educationDesc});
+}
+
+/// @nodoc
+class _$EducationTemplatesCopyWithImpl<$Res>
+    implements $EducationTemplatesCopyWith<$Res> {
+  _$EducationTemplatesCopyWithImpl(this._value, this._then);
+
+  final EducationTemplates _value;
+  // ignore: unused_field
+  final $Res Function(EducationTemplates) _then;
+
+  @override
+  $Res call({
+    Object? educationTemplateId = freezed,
+    Object? createdBy = freezed,
+    Object? createdDate = freezed,
+    Object? lastModifiedBy = freezed,
+    Object? lastModifiedDate = freezed,
+    Object? educationDesc = freezed,
+  }) {
+    return _then(_value.copyWith(
+      educationTemplateId: educationTemplateId == freezed
+          ? _value.educationTemplateId
+          : educationTemplateId // ignore: cast_nullable_to_non_nullable
+              as int,
+      createdBy: createdBy == freezed
+          ? _value.createdBy
+          : createdBy // ignore: cast_nullable_to_non_nullable
+              as StringSingleLine,
+      createdDate: createdDate == freezed
+          ? _value.createdDate
+          : createdDate // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      lastModifiedBy: lastModifiedBy == freezed
+          ? _value.lastModifiedBy
+          : lastModifiedBy // ignore: cast_nullable_to_non_nullable
+              as StringSingleLine,
+      lastModifiedDate: lastModifiedDate == freezed
+          ? _value.lastModifiedDate
+          : lastModifiedDate // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      educationDesc: educationDesc == freezed
+          ? _value.educationDesc
+          : educationDesc // ignore: cast_nullable_to_non_nullable
+              as StringMultiLine,
+    ));
+  }
+}
+
+/// @nodoc
+abstract class _$EducationTemplatesCopyWith<$Res>
+    implements $EducationTemplatesCopyWith<$Res> {
+  factory _$EducationTemplatesCopyWith(
+          _EducationTemplates value, $Res Function(_EducationTemplates) then) =
+      __$EducationTemplatesCopyWithImpl<$Res>;
+  @override
+  $Res call(
+      {int educationTemplateId,
+      StringSingleLine createdBy,
+      DateTime createdDate,
+      StringSingleLine lastModifiedBy,
+      DateTime lastModifiedDate,
+      StringMultiLine educationDesc});
+}
+
+/// @nodoc
+class __$EducationTemplatesCopyWithImpl<$Res>
+    extends _$EducationTemplatesCopyWithImpl<$Res>
+    implements _$EducationTemplatesCopyWith<$Res> {
+  __$EducationTemplatesCopyWithImpl(
+      _EducationTemplates _value, $Res Function(_EducationTemplates) _then)
+      : super(_value, (v) => _then(v as _EducationTemplates));
+
+  @override
+  _EducationTemplates get _value => super._value as _EducationTemplates;
+
+  @override
+  $Res call({
+    Object? educationTemplateId = freezed,
+    Object? createdBy = freezed,
+    Object? createdDate = freezed,
+    Object? lastModifiedBy = freezed,
+    Object? lastModifiedDate = freezed,
+    Object? educationDesc = freezed,
+  }) {
+    return _then(_EducationTemplates(
+      educationTemplateId: educationTemplateId == freezed
+          ? _value.educationTemplateId
+          : educationTemplateId // ignore: cast_nullable_to_non_nullable
+              as int,
+      createdBy: createdBy == freezed
+          ? _value.createdBy
+          : createdBy // ignore: cast_nullable_to_non_nullable
+              as StringSingleLine,
+      createdDate: createdDate == freezed
+          ? _value.createdDate
+          : createdDate // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      lastModifiedBy: lastModifiedBy == freezed
+          ? _value.lastModifiedBy
+          : lastModifiedBy // ignore: cast_nullable_to_non_nullable
+              as StringSingleLine,
+      lastModifiedDate: lastModifiedDate == freezed
+          ? _value.lastModifiedDate
+          : lastModifiedDate // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      educationDesc: educationDesc == freezed
+          ? _value.educationDesc
+          : educationDesc // ignore: cast_nullable_to_non_nullable
+              as StringMultiLine,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$_EducationTemplates extends _EducationTemplates {
+  _$_EducationTemplates(
+      {required this.educationTemplateId,
+      required this.createdBy,
+      required this.createdDate,
+      required this.lastModifiedBy,
+      required this.lastModifiedDate,
+      required this.educationDesc})
+      : super._();
+
+  @override
+  final int educationTemplateId;
+  @override
+  final StringSingleLine createdBy;
+  @override
+  final DateTime createdDate;
+  @override
+  final StringSingleLine lastModifiedBy;
+  @override
+  final DateTime lastModifiedDate;
+  @override
+  final StringMultiLine educationDesc;
+
+  @override
+  String toString() {
+    return 'EducationTemplates(educationTemplateId: $educationTemplateId, createdBy: $createdBy, createdDate: $createdDate, lastModifiedBy: $lastModifiedBy, lastModifiedDate: $lastModifiedDate, educationDesc: $educationDesc)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other is _EducationTemplates &&
+            (identical(other.educationTemplateId, educationTemplateId) ||
+                const DeepCollectionEquality()
+                    .equals(other.educationTemplateId, educationTemplateId)) &&
+            (identical(other.createdBy, createdBy) ||
+                const DeepCollectionEquality()
+                    .equals(other.createdBy, createdBy)) &&
+            (identical(other.createdDate, createdDate) ||
+                const DeepCollectionEquality()
+                    .equals(other.createdDate, createdDate)) &&
+            (identical(other.lastModifiedBy, lastModifiedBy) ||
+                const DeepCollectionEquality()
+                    .equals(other.lastModifiedBy, lastModifiedBy)) &&
+            (identical(other.lastModifiedDate, lastModifiedDate) ||
+                const DeepCollectionEquality()
+                    .equals(other.lastModifiedDate, lastModifiedDate)) &&
+            (identical(other.educationDesc, educationDesc) ||
+                const DeepCollectionEquality()
+                    .equals(other.educationDesc, educationDesc)));
+  }
+
+  @override
+  int get hashCode =>
+      runtimeType.hashCode ^
+      const DeepCollectionEquality().hash(educationTemplateId) ^
+      const DeepCollectionEquality().hash(createdBy) ^
+      const DeepCollectionEquality().hash(createdDate) ^
+      const DeepCollectionEquality().hash(lastModifiedBy) ^
+      const DeepCollectionEquality().hash(lastModifiedDate) ^
+      const DeepCollectionEquality().hash(educationDesc);
+
+  @JsonKey(ignore: true)
+  @override
+  _$EducationTemplatesCopyWith<_EducationTemplates> get copyWith =>
+      __$EducationTemplatesCopyWithImpl<_EducationTemplates>(this, _$identity);
+}
+
+abstract class _EducationTemplates extends EducationTemplates {
+  factory _EducationTemplates(
+      {required int educationTemplateId,
+      required StringSingleLine createdBy,
+      required DateTime createdDate,
+      required StringSingleLine lastModifiedBy,
+      required DateTime lastModifiedDate,
+      required StringMultiLine educationDesc}) = _$_EducationTemplates;
+  _EducationTemplates._() : super._();
+
+  @override
+  int get educationTemplateId => throw _privateConstructorUsedError;
+  @override
+  StringSingleLine get createdBy => throw _privateConstructorUsedError;
+  @override
+  DateTime get createdDate => throw _privateConstructorUsedError;
+  @override
+  StringSingleLine get lastModifiedBy => throw _privateConstructorUsedError;
+  @override
+  DateTime get lastModifiedDate => throw _privateConstructorUsedError;
+  @override
+  StringMultiLine get educationDesc => throw _privateConstructorUsedError;
+  @override
+  @JsonKey(ignore: true)
+  _$EducationTemplatesCopyWith<_EducationTemplates> get copyWith =>
+      throw _privateConstructorUsedError;
+}

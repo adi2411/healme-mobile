fed02c26d670f9250feb3d92525e798e81a107f5
aditya <aditya_jinegar@yahoo.com>
2021-08-13 13:52:56 -0400

    Create mindfulEnergyTemplatesCustomGoals.freezed.dart

---
+++ lib/domain/Templates/Resources/MindfulEnergyTemplatesCustomGoals/mindfulEnergyTemplatesCustomGoals.freezed.dart
@@ -0,0 +1,197 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides
+
+part of 'mindfulEnergyTemplatesCustomGoals.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more informations: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+class _$MindfulEnergyTemplatesCustomGoalsTearOff {
+  const _$MindfulEnergyTemplatesCustomGoalsTearOff();
+
+  _MindfulEnergyTemplatesCustomGoals call(
+      {required int mindfulEnergyTemplateMindfulEnergyTemplateId,
+      required int customGoalsGoalTemplateId}) {
+    return _MindfulEnergyTemplatesCustomGoals(
+      mindfulEnergyTemplateMindfulEnergyTemplateId:
+          mindfulEnergyTemplateMindfulEnergyTemplateId,
+      customGoalsGoalTemplateId: customGoalsGoalTemplateId,
+    );
+  }
+}
+
+/// @nodoc
+const $MindfulEnergyTemplatesCustomGoals =
+    _$MindfulEnergyTemplatesCustomGoalsTearOff();
+
+/// @nodoc
+mixin _$MindfulEnergyTemplatesCustomGoals {
+  int get mindfulEnergyTemplateMindfulEnergyTemplateId =>
+      throw _privateConstructorUsedError;
+  int get customGoalsGoalTemplateId => throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $MindfulEnergyTemplatesCustomGoalsCopyWith<MindfulEnergyTemplatesCustomGoals>
+      get copyWith => throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $MindfulEnergyTemplatesCustomGoalsCopyWith<$Res> {
+  factory $MindfulEnergyTemplatesCustomGoalsCopyWith(
+          MindfulEnergyTemplatesCustomGoals value,
+          $Res Function(MindfulEnergyTemplatesCustomGoals) then) =
+      _$MindfulEnergyTemplatesCustomGoalsCopyWithImpl<$Res>;
+  $Res call(
+      {int mindfulEnergyTemplateMindfulEnergyTemplateId,
+      int customGoalsGoalTemplateId});
+}
+
+/// @nodoc
+class _$MindfulEnergyTemplatesCustomGoalsCopyWithImpl<$Res>
+    implements $MindfulEnergyTemplatesCustomGoalsCopyWith<$Res> {
+  _$MindfulEnergyTemplatesCustomGoalsCopyWithImpl(this._value, this._then);
+
+  final MindfulEnergyTemplatesCustomGoals _value;
+  // ignore: unused_field
+  final $Res Function(MindfulEnergyTemplatesCustomGoals) _then;
+
+  @override
+  $Res call({
+    Object? mindfulEnergyTemplateMindfulEnergyTemplateId = freezed,
+    Object? customGoalsGoalTemplateId = freezed,
+  }) {
+    return _then(_value.copyWith(
+      mindfulEnergyTemplateMindfulEnergyTemplateId:
+          mindfulEnergyTemplateMindfulEnergyTemplateId == freezed
+              ? _value.mindfulEnergyTemplateMindfulEnergyTemplateId
+              : mindfulEnergyTemplateMindfulEnergyTemplateId // ignore: cast_nullable_to_non_nullable
+                  as int,
+      customGoalsGoalTemplateId: customGoalsGoalTemplateId == freezed
+          ? _value.customGoalsGoalTemplateId
+          : customGoalsGoalTemplateId // ignore: cast_nullable_to_non_nullable
+              as int,
+    ));
+  }
+}
+
+/// @nodoc
+abstract class _$MindfulEnergyTemplatesCustomGoalsCopyWith<$Res>
+    implements $MindfulEnergyTemplatesCustomGoalsCopyWith<$Res> {
+  factory _$MindfulEnergyTemplatesCustomGoalsCopyWith(
+          _MindfulEnergyTemplatesCustomGoals value,
+          $Res Function(_MindfulEnergyTemplatesCustomGoals) then) =
+      __$MindfulEnergyTemplatesCustomGoalsCopyWithImpl<$Res>;
+  @override
+  $Res call(
+      {int mindfulEnergyTemplateMindfulEnergyTemplateId,
+      int customGoalsGoalTemplateId});
+}
+
+/// @nodoc
+class __$MindfulEnergyTemplatesCustomGoalsCopyWithImpl<$Res>
+    extends _$MindfulEnergyTemplatesCustomGoalsCopyWithImpl<$Res>
+    implements _$MindfulEnergyTemplatesCustomGoalsCopyWith<$Res> {
+  __$MindfulEnergyTemplatesCustomGoalsCopyWithImpl(
+      _MindfulEnergyTemplatesCustomGoals _value,
+      $Res Function(_MindfulEnergyTemplatesCustomGoals) _then)
+      : super(_value, (v) => _then(v as _MindfulEnergyTemplatesCustomGoals));
+
+  @override
+  _MindfulEnergyTemplatesCustomGoals get _value =>
+      super._value as _MindfulEnergyTemplatesCustomGoals;
+
+  @override
+  $Res call({
+    Object? mindfulEnergyTemplateMindfulEnergyTemplateId = freezed,
+    Object? customGoalsGoalTemplateId = freezed,
+  }) {
+    return _then(_MindfulEnergyTemplatesCustomGoals(
+      mindfulEnergyTemplateMindfulEnergyTemplateId:
+          mindfulEnergyTemplateMindfulEnergyTemplateId == freezed
+              ? _value.mindfulEnergyTemplateMindfulEnergyTemplateId
+              : mindfulEnergyTemplateMindfulEnergyTemplateId // ignore: cast_nullable_to_non_nullable
+                  as int,
+      customGoalsGoalTemplateId: customGoalsGoalTemplateId == freezed
+          ? _value.customGoalsGoalTemplateId
+          : customGoalsGoalTemplateId // ignore: cast_nullable_to_non_nullable
+              as int,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$_MindfulEnergyTemplatesCustomGoals
+    extends _MindfulEnergyTemplatesCustomGoals {
+  _$_MindfulEnergyTemplatesCustomGoals(
+      {required this.mindfulEnergyTemplateMindfulEnergyTemplateId,
+      required this.customGoalsGoalTemplateId})
+      : super._();
+
+  @override
+  final int mindfulEnergyTemplateMindfulEnergyTemplateId;
+  @override
+  final int customGoalsGoalTemplateId;
+
+  @override
+  String toString() {
+    return 'MindfulEnergyTemplatesCustomGoals(mindfulEnergyTemplateMindfulEnergyTemplateId: $mindfulEnergyTemplateMindfulEnergyTemplateId, customGoalsGoalTemplateId: $customGoalsGoalTemplateId)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other is _MindfulEnergyTemplatesCustomGoals &&
+            (identical(other.mindfulEnergyTemplateMindfulEnergyTemplateId,
+                    mindfulEnergyTemplateMindfulEnergyTemplateId) ||
+                const DeepCollectionEquality().equals(
+                    other.mindfulEnergyTemplateMindfulEnergyTemplateId,
+                    mindfulEnergyTemplateMindfulEnergyTemplateId)) &&
+            (identical(other.customGoalsGoalTemplateId,
+                    customGoalsGoalTemplateId) ||
+                const DeepCollectionEquality().equals(
+                    other.customGoalsGoalTemplateId,
+                    customGoalsGoalTemplateId)));
+  }
+
+  @override
+  int get hashCode =>
+      runtimeType.hashCode ^
+      const DeepCollectionEquality()
+          .hash(mindfulEnergyTemplateMindfulEnergyTemplateId) ^
+      const DeepCollectionEquality().hash(customGoalsGoalTemplateId);
+
+  @JsonKey(ignore: true)
+  @override
+  _$MindfulEnergyTemplatesCustomGoalsCopyWith<
+          _MindfulEnergyTemplatesCustomGoals>
+      get copyWith => __$MindfulEnergyTemplatesCustomGoalsCopyWithImpl<
+          _MindfulEnergyTemplatesCustomGoals>(this, _$identity);
+}
+
+abstract class _MindfulEnergyTemplatesCustomGoals
+    extends MindfulEnergyTemplatesCustomGoals {
+  factory _MindfulEnergyTemplatesCustomGoals(
+          {required int mindfulEnergyTemplateMindfulEnergyTemplateId,
+          required int customGoalsGoalTemplateId}) =
+      _$_MindfulEnergyTemplatesCustomGoals;
+  _MindfulEnergyTemplatesCustomGoals._() : super._();
+
+  @override
+  int get mindfulEnergyTemplateMindfulEnergyTemplateId =>
+      throw _privateConstructorUsedError;
+  @override
+  int get customGoalsGoalTemplateId => throw _privateConstructorUsedError;
+  @override
+  @JsonKey(ignore: true)
+  _$MindfulEnergyTemplatesCustomGoalsCopyWith<
+          _MindfulEnergyTemplatesCustomGoals>
+      get copyWith => throw _privateConstructorUsedError;
+}

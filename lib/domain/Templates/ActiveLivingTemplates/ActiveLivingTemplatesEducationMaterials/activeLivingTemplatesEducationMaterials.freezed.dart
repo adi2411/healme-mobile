12fa1ad0f047ff98dd3c2af1d6fce2b4452c66e7
aditya <aditya_jinegar@yahoo.com>
2021-08-13 13:50:58 -0400

    Create activeLivingTemplatesEducationMaterials.freezed.dart

---
+++ lib/domain/Templates/ActiveLivingTemplates/ActiveLivingTemplatesEducationMaterials/activeLivingTemplatesEducationMaterials.freezed.dart
@@ -0,0 +1,205 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides
+
+part of 'activeLivingTemplatesEducationMaterials.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more informations: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+class _$ActiveLivingTemplatesEducationMaterialsTearOff {
+  const _$ActiveLivingTemplatesEducationMaterialsTearOff();
+
+  _ActiveLivingTemplatesEducationMaterials call(
+      {required int activeLivingTemplateActiveLivingTemplateId,
+      required int educationMaterialsResourceTemplateId}) {
+    return _ActiveLivingTemplatesEducationMaterials(
+      activeLivingTemplateActiveLivingTemplateId:
+          activeLivingTemplateActiveLivingTemplateId,
+      educationMaterialsResourceTemplateId:
+          educationMaterialsResourceTemplateId,
+    );
+  }
+}
+
+/// @nodoc
+const $ActiveLivingTemplatesEducationMaterials =
+    _$ActiveLivingTemplatesEducationMaterialsTearOff();
+
+/// @nodoc
+mixin _$ActiveLivingTemplatesEducationMaterials {
+  int get activeLivingTemplateActiveLivingTemplateId =>
+      throw _privateConstructorUsedError;
+  int get educationMaterialsResourceTemplateId =>
+      throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $ActiveLivingTemplatesEducationMaterialsCopyWith<
+          ActiveLivingTemplatesEducationMaterials>
+      get copyWith => throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $ActiveLivingTemplatesEducationMaterialsCopyWith<$Res> {
+  factory $ActiveLivingTemplatesEducationMaterialsCopyWith(
+          ActiveLivingTemplatesEducationMaterials value,
+          $Res Function(ActiveLivingTemplatesEducationMaterials) then) =
+      _$ActiveLivingTemplatesEducationMaterialsCopyWithImpl<$Res>;
+  $Res call(
+      {int activeLivingTemplateActiveLivingTemplateId,
+      int educationMaterialsResourceTemplateId});
+}
+
+/// @nodoc
+class _$ActiveLivingTemplatesEducationMaterialsCopyWithImpl<$Res>
+    implements $ActiveLivingTemplatesEducationMaterialsCopyWith<$Res> {
+  _$ActiveLivingTemplatesEducationMaterialsCopyWithImpl(
+      this._value, this._then);
+
+  final ActiveLivingTemplatesEducationMaterials _value;
+  // ignore: unused_field
+  final $Res Function(ActiveLivingTemplatesEducationMaterials) _then;
+
+  @override
+  $Res call({
+    Object? activeLivingTemplateActiveLivingTemplateId = freezed,
+    Object? educationMaterialsResourceTemplateId = freezed,
+  }) {
+    return _then(_value.copyWith(
+      activeLivingTemplateActiveLivingTemplateId:
+          activeLivingTemplateActiveLivingTemplateId == freezed
+              ? _value.activeLivingTemplateActiveLivingTemplateId
+              : activeLivingTemplateActiveLivingTemplateId // ignore: cast_nullable_to_non_nullable
+                  as int,
+      educationMaterialsResourceTemplateId: educationMaterialsResourceTemplateId ==
+              freezed
+          ? _value.educationMaterialsResourceTemplateId
+          : educationMaterialsResourceTemplateId // ignore: cast_nullable_to_non_nullable
+              as int,
+    ));
+  }
+}
+
+/// @nodoc
+abstract class _$ActiveLivingTemplatesEducationMaterialsCopyWith<$Res>
+    implements $ActiveLivingTemplatesEducationMaterialsCopyWith<$Res> {
+  factory _$ActiveLivingTemplatesEducationMaterialsCopyWith(
+          _ActiveLivingTemplatesEducationMaterials value,
+          $Res Function(_ActiveLivingTemplatesEducationMaterials) then) =
+      __$ActiveLivingTemplatesEducationMaterialsCopyWithImpl<$Res>;
+  @override
+  $Res call(
+      {int activeLivingTemplateActiveLivingTemplateId,
+      int educationMaterialsResourceTemplateId});
+}
+
+/// @nodoc
+class __$ActiveLivingTemplatesEducationMaterialsCopyWithImpl<$Res>
+    extends _$ActiveLivingTemplatesEducationMaterialsCopyWithImpl<$Res>
+    implements _$ActiveLivingTemplatesEducationMaterialsCopyWith<$Res> {
+  __$ActiveLivingTemplatesEducationMaterialsCopyWithImpl(
+      _ActiveLivingTemplatesEducationMaterials _value,
+      $Res Function(_ActiveLivingTemplatesEducationMaterials) _then)
+      : super(_value,
+            (v) => _then(v as _ActiveLivingTemplatesEducationMaterials));
+
+  @override
+  _ActiveLivingTemplatesEducationMaterials get _value =>
+      super._value as _ActiveLivingTemplatesEducationMaterials;
+
+  @override
+  $Res call({
+    Object? activeLivingTemplateActiveLivingTemplateId = freezed,
+    Object? educationMaterialsResourceTemplateId = freezed,
+  }) {
+    return _then(_ActiveLivingTemplatesEducationMaterials(
+      activeLivingTemplateActiveLivingTemplateId:
+          activeLivingTemplateActiveLivingTemplateId == freezed
+              ? _value.activeLivingTemplateActiveLivingTemplateId
+              : activeLivingTemplateActiveLivingTemplateId // ignore: cast_nullable_to_non_nullable
+                  as int,
+      educationMaterialsResourceTemplateId: educationMaterialsResourceTemplateId ==
+              freezed
+          ? _value.educationMaterialsResourceTemplateId
+          : educationMaterialsResourceTemplateId // ignore: cast_nullable_to_non_nullable
+              as int,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$_ActiveLivingTemplatesEducationMaterials
+    extends _ActiveLivingTemplatesEducationMaterials {
+  _$_ActiveLivingTemplatesEducationMaterials(
+      {required this.activeLivingTemplateActiveLivingTemplateId,
+      required this.educationMaterialsResourceTemplateId})
+      : super._();
+
+  @override
+  final int activeLivingTemplateActiveLivingTemplateId;
+  @override
+  final int educationMaterialsResourceTemplateId;
+
+  @override
+  String toString() {
+    return 'ActiveLivingTemplatesEducationMaterials(activeLivingTemplateActiveLivingTemplateId: $activeLivingTemplateActiveLivingTemplateId, educationMaterialsResourceTemplateId: $educationMaterialsResourceTemplateId)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other is _ActiveLivingTemplatesEducationMaterials &&
+            (identical(other.activeLivingTemplateActiveLivingTemplateId,
+                    activeLivingTemplateActiveLivingTemplateId) ||
+                const DeepCollectionEquality().equals(
+                    other.activeLivingTemplateActiveLivingTemplateId,
+                    activeLivingTemplateActiveLivingTemplateId)) &&
+            (identical(other.educationMaterialsResourceTemplateId,
+                    educationMaterialsResourceTemplateId) ||
+                const DeepCollectionEquality().equals(
+                    other.educationMaterialsResourceTemplateId,
+                    educationMaterialsResourceTemplateId)));
+  }
+
+  @override
+  int get hashCode =>
+      runtimeType.hashCode ^
+      const DeepCollectionEquality()
+          .hash(activeLivingTemplateActiveLivingTemplateId) ^
+      const DeepCollectionEquality().hash(educationMaterialsResourceTemplateId);
+
+  @JsonKey(ignore: true)
+  @override
+  _$ActiveLivingTemplatesEducationMaterialsCopyWith<
+          _ActiveLivingTemplatesEducationMaterials>
+      get copyWith => __$ActiveLivingTemplatesEducationMaterialsCopyWithImpl<
+          _ActiveLivingTemplatesEducationMaterials>(this, _$identity);
+}
+
+abstract class _ActiveLivingTemplatesEducationMaterials
+    extends ActiveLivingTemplatesEducationMaterials {
+  factory _ActiveLivingTemplatesEducationMaterials(
+          {required int activeLivingTemplateActiveLivingTemplateId,
+          required int educationMaterialsResourceTemplateId}) =
+      _$_ActiveLivingTemplatesEducationMaterials;
+  _ActiveLivingTemplatesEducationMaterials._() : super._();
+
+  @override
+  int get activeLivingTemplateActiveLivingTemplateId =>
+      throw _privateConstructorUsedError;
+  @override
+  int get educationMaterialsResourceTemplateId =>
+      throw _privateConstructorUsedError;
+  @override
+  @JsonKey(ignore: true)
+  _$ActiveLivingTemplatesEducationMaterialsCopyWith<
+          _ActiveLivingTemplatesEducationMaterials>
+      get copyWith => throw _privateConstructorUsedError;
+}

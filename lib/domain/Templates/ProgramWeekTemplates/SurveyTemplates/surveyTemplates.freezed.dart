c169d160f9e6b2e898b1941ebc9633eae2c23e7e
aditya <aditya_jinegar@yahoo.com>
2021-08-13 13:52:52 -0400

    Create surveyTemplates.freezed.dart

---
+++ lib/domain/Templates/ProgramWeekTemplates/SurveyTemplates/surveyTemplates.freezed.dart
@@ -0,0 +1,276 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides
+
+part of 'surveyTemplates.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more informations: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+class _$SurveyTemplatesTearOff {
+  const _$SurveyTemplatesTearOff();
+
+  _SurveyTemplates call(
+      {required int surveyTemplateId,
+      required StringSingleLine createdBy,
+      required DateTime createdDate,
+      required StringSingleLine lastModifiedBy,
+      required DateTime lastModifiedDate,
+      required StringMultiLine surveyDesc}) {
+    return _SurveyTemplates(
+      surveyTemplateId: surveyTemplateId,
+      createdBy: createdBy,
+      createdDate: createdDate,
+      lastModifiedBy: lastModifiedBy,
+      lastModifiedDate: lastModifiedDate,
+      surveyDesc: surveyDesc,
+    );
+  }
+}
+
+/// @nodoc
+const $SurveyTemplates = _$SurveyTemplatesTearOff();
+
+/// @nodoc
+mixin _$SurveyTemplates {
+  int get surveyTemplateId => throw _privateConstructorUsedError;
+  StringSingleLine get createdBy => throw _privateConstructorUsedError;
+  DateTime get createdDate => throw _privateConstructorUsedError;
+  StringSingleLine get lastModifiedBy => throw _privateConstructorUsedError;
+  DateTime get lastModifiedDate => throw _privateConstructorUsedError;
+  StringMultiLine get surveyDesc => throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $SurveyTemplatesCopyWith<SurveyTemplates> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $SurveyTemplatesCopyWith<$Res> {
+  factory $SurveyTemplatesCopyWith(
+          SurveyTemplates value, $Res Function(SurveyTemplates) then) =
+      _$SurveyTemplatesCopyWithImpl<$Res>;
+  $Res call(
+      {int surveyTemplateId,
+      StringSingleLine createdBy,
+      DateTime createdDate,
+      StringSingleLine lastModifiedBy,
+      DateTime lastModifiedDate,
+      StringMultiLine surveyDesc});
+}
+
+/// @nodoc
+class _$SurveyTemplatesCopyWithImpl<$Res>
+    implements $SurveyTemplatesCopyWith<$Res> {
+  _$SurveyTemplatesCopyWithImpl(this._value, this._then);
+
+  final SurveyTemplates _value;
+  // ignore: unused_field
+  final $Res Function(SurveyTemplates) _then;
+
+  @override
+  $Res call({
+    Object? surveyTemplateId = freezed,
+    Object? createdBy = freezed,
+    Object? createdDate = freezed,
+    Object? lastModifiedBy = freezed,
+    Object? lastModifiedDate = freezed,
+    Object? surveyDesc = freezed,
+  }) {
+    return _then(_value.copyWith(
+      surveyTemplateId: surveyTemplateId == freezed
+          ? _value.surveyTemplateId
+          : surveyTemplateId // ignore: cast_nullable_to_non_nullable
+              as int,
+      createdBy: createdBy == freezed
+          ? _value.createdBy
+          : createdBy // ignore: cast_nullable_to_non_nullable
+              as StringSingleLine,
+      createdDate: createdDate == freezed
+          ? _value.createdDate
+          : createdDate // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      lastModifiedBy: lastModifiedBy == freezed
+          ? _value.lastModifiedBy
+          : lastModifiedBy // ignore: cast_nullable_to_non_nullable
+              as StringSingleLine,
+      lastModifiedDate: lastModifiedDate == freezed
+          ? _value.lastModifiedDate
+          : lastModifiedDate // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      surveyDesc: surveyDesc == freezed
+          ? _value.surveyDesc
+          : surveyDesc // ignore: cast_nullable_to_non_nullable
+              as StringMultiLine,
+    ));
+  }
+}
+
+/// @nodoc
+abstract class _$SurveyTemplatesCopyWith<$Res>
+    implements $SurveyTemplatesCopyWith<$Res> {
+  factory _$SurveyTemplatesCopyWith(
+          _SurveyTemplates value, $Res Function(_SurveyTemplates) then) =
+      __$SurveyTemplatesCopyWithImpl<$Res>;
+  @override
+  $Res call(
+      {int surveyTemplateId,
+      StringSingleLine createdBy,
+      DateTime createdDate,
+      StringSingleLine lastModifiedBy,
+      DateTime lastModifiedDate,
+      StringMultiLine surveyDesc});
+}
+
+/// @nodoc
+class __$SurveyTemplatesCopyWithImpl<$Res>
+    extends _$SurveyTemplatesCopyWithImpl<$Res>
+    implements _$SurveyTemplatesCopyWith<$Res> {
+  __$SurveyTemplatesCopyWithImpl(
+      _SurveyTemplates _value, $Res Function(_SurveyTemplates) _then)
+      : super(_value, (v) => _then(v as _SurveyTemplates));
+
+  @override
+  _SurveyTemplates get _value => super._value as _SurveyTemplates;
+
+  @override
+  $Res call({
+    Object? surveyTemplateId = freezed,
+    Object? createdBy = freezed,
+    Object? createdDate = freezed,
+    Object? lastModifiedBy = freezed,
+    Object? lastModifiedDate = freezed,
+    Object? surveyDesc = freezed,
+  }) {
+    return _then(_SurveyTemplates(
+      surveyTemplateId: surveyTemplateId == freezed
+          ? _value.surveyTemplateId
+          : surveyTemplateId // ignore: cast_nullable_to_non_nullable
+              as int,
+      createdBy: createdBy == freezed
+          ? _value.createdBy
+          : createdBy // ignore: cast_nullable_to_non_nullable
+              as StringSingleLine,
+      createdDate: createdDate == freezed
+          ? _value.createdDate
+          : createdDate // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      lastModifiedBy: lastModifiedBy == freezed
+          ? _value.lastModifiedBy
+          : lastModifiedBy // ignore: cast_nullable_to_non_nullable
+              as StringSingleLine,
+      lastModifiedDate: lastModifiedDate == freezed
+          ? _value.lastModifiedDate
+          : lastModifiedDate // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      surveyDesc: surveyDesc == freezed
+          ? _value.surveyDesc
+          : surveyDesc // ignore: cast_nullable_to_non_nullable
+              as StringMultiLine,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$_SurveyTemplates extends _SurveyTemplates {
+  _$_SurveyTemplates(
+      {required this.surveyTemplateId,
+      required this.createdBy,
+      required this.createdDate,
+      required this.lastModifiedBy,
+      required this.lastModifiedDate,
+      required this.surveyDesc})
+      : super._();
+
+  @override
+  final int surveyTemplateId;
+  @override
+  final StringSingleLine createdBy;
+  @override
+  final DateTime createdDate;
+  @override
+  final StringSingleLine lastModifiedBy;
+  @override
+  final DateTime lastModifiedDate;
+  @override
+  final StringMultiLine surveyDesc;
+
+  @override
+  String toString() {
+    return 'SurveyTemplates(surveyTemplateId: $surveyTemplateId, createdBy: $createdBy, createdDate: $createdDate, lastModifiedBy: $lastModifiedBy, lastModifiedDate: $lastModifiedDate, surveyDesc: $surveyDesc)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other is _SurveyTemplates &&
+            (identical(other.surveyTemplateId, surveyTemplateId) ||
+                const DeepCollectionEquality()
+                    .equals(other.surveyTemplateId, surveyTemplateId)) &&
+            (identical(other.createdBy, createdBy) ||
+                const DeepCollectionEquality()
+                    .equals(other.createdBy, createdBy)) &&
+            (identical(other.createdDate, createdDate) ||
+                const DeepCollectionEquality()
+                    .equals(other.createdDate, createdDate)) &&
+            (identical(other.lastModifiedBy, lastModifiedBy) ||
+                const DeepCollectionEquality()
+                    .equals(other.lastModifiedBy, lastModifiedBy)) &&
+            (identical(other.lastModifiedDate, lastModifiedDate) ||
+                const DeepCollectionEquality()
+                    .equals(other.lastModifiedDate, lastModifiedDate)) &&
+            (identical(other.surveyDesc, surveyDesc) ||
+                const DeepCollectionEquality()
+                    .equals(other.surveyDesc, surveyDesc)));
+  }
+
+  @override
+  int get hashCode =>
+      runtimeType.hashCode ^
+      const DeepCollectionEquality().hash(surveyTemplateId) ^
+      const DeepCollectionEquality().hash(createdBy) ^
+      const DeepCollectionEquality().hash(createdDate) ^
+      const DeepCollectionEquality().hash(lastModifiedBy) ^
+      const DeepCollectionEquality().hash(lastModifiedDate) ^
+      const DeepCollectionEquality().hash(surveyDesc);
+
+  @JsonKey(ignore: true)
+  @override
+  _$SurveyTemplatesCopyWith<_SurveyTemplates> get copyWith =>
+      __$SurveyTemplatesCopyWithImpl<_SurveyTemplates>(this, _$identity);
+}
+
+abstract class _SurveyTemplates extends SurveyTemplates {
+  factory _SurveyTemplates(
+      {required int surveyTemplateId,
+      required StringSingleLine createdBy,
+      required DateTime createdDate,
+      required StringSingleLine lastModifiedBy,
+      required DateTime lastModifiedDate,
+      required StringMultiLine surveyDesc}) = _$_SurveyTemplates;
+  _SurveyTemplates._() : super._();
+
+  @override
+  int get surveyTemplateId => throw _privateConstructorUsedError;
+  @override
+  StringSingleLine get createdBy => throw _privateConstructorUsedError;
+  @override
+  DateTime get createdDate => throw _privateConstructorUsedError;
+  @override
+  StringSingleLine get lastModifiedBy => throw _privateConstructorUsedError;
+  @override
+  DateTime get lastModifiedDate => throw _privateConstructorUsedError;
+  @override
+  StringMultiLine get surveyDesc => throw _privateConstructorUsedError;
+  @override
+  @JsonKey(ignore: true)
+  _$SurveyTemplatesCopyWith<_SurveyTemplates> get copyWith =>
+      throw _privateConstructorUsedError;
+}

ebe7cbccd0bd47f9af8f36deb24235b5ffa4158b
aditya <aditya_jinegar@yahoo.com>
2021-08-13 12:08:09 -0400

    Create HealthyEatingTemplates.freezed.dart

---
+++ lib/domain/Templates/HealthyEatingTemplates/HealthyEatingTemplates/HealthyEatingTemplates.freezed.dart
@@ -0,0 +1,379 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides
+
+part of 'HealthyEatingTemplates.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more informations: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+class _$HealthyEatingTemplatesTearOff {
+  const _$HealthyEatingTemplatesTearOff();
+
+  _HealthyEatingTemplates call(
+      {required int healthyEatingTemplateId,
+      required StringSingleLine createdBy,
+      required DateTime createdDate,
+      required StringSingleLine lastModifiedBy,
+      required DateTime lastModifiedDate,
+      required int active,
+      required StringMultiLine additionalInstructions,
+      required int fishServings,
+      required int nutritionTrackingDays,
+      required int proteinServing}) {
+    return _HealthyEatingTemplates(
+      healthyEatingTemplateId: healthyEatingTemplateId,
+      createdBy: createdBy,
+      createdDate: createdDate,
+      lastModifiedBy: lastModifiedBy,
+      lastModifiedDate: lastModifiedDate,
+      active: active,
+      additionalInstructions: additionalInstructions,
+      fishServings: fishServings,
+      nutritionTrackingDays: nutritionTrackingDays,
+      proteinServing: proteinServing,
+    );
+  }
+}
+
+/// @nodoc
+const $HealthyEatingTemplates = _$HealthyEatingTemplatesTearOff();
+
+/// @nodoc
+mixin _$HealthyEatingTemplates {
+  int get healthyEatingTemplateId => throw _privateConstructorUsedError;
+  StringSingleLine get createdBy => throw _privateConstructorUsedError;
+  DateTime get createdDate => throw _privateConstructorUsedError;
+  StringSingleLine get lastModifiedBy => throw _privateConstructorUsedError;
+  DateTime get lastModifiedDate => throw _privateConstructorUsedError;
+  int get active => throw _privateConstructorUsedError;
+  StringMultiLine get additionalInstructions =>
+      throw _privateConstructorUsedError;
+  int get fishServings => throw _privateConstructorUsedError;
+  int get nutritionTrackingDays => throw _privateConstructorUsedError;
+  int get proteinServing => throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $HealthyEatingTemplatesCopyWith<HealthyEatingTemplates> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $HealthyEatingTemplatesCopyWith<$Res> {
+  factory $HealthyEatingTemplatesCopyWith(HealthyEatingTemplates value,
+          $Res Function(HealthyEatingTemplates) then) =
+      _$HealthyEatingTemplatesCopyWithImpl<$Res>;
+  $Res call(
+      {int healthyEatingTemplateId,
+      StringSingleLine createdBy,
+      DateTime createdDate,
+      StringSingleLine lastModifiedBy,
+      DateTime lastModifiedDate,
+      int active,
+      StringMultiLine additionalInstructions,
+      int fishServings,
+      int nutritionTrackingDays,
+      int proteinServing});
+}
+
+/// @nodoc
+class _$HealthyEatingTemplatesCopyWithImpl<$Res>
+    implements $HealthyEatingTemplatesCopyWith<$Res> {
+  _$HealthyEatingTemplatesCopyWithImpl(this._value, this._then);
+
+  final HealthyEatingTemplates _value;
+  // ignore: unused_field
+  final $Res Function(HealthyEatingTemplates) _then;
+
+  @override
+  $Res call({
+    Object? healthyEatingTemplateId = freezed,
+    Object? createdBy = freezed,
+    Object? createdDate = freezed,
+    Object? lastModifiedBy = freezed,
+    Object? lastModifiedDate = freezed,
+    Object? active = freezed,
+    Object? additionalInstructions = freezed,
+    Object? fishServings = freezed,
+    Object? nutritionTrackingDays = freezed,
+    Object? proteinServing = freezed,
+  }) {
+    return _then(_value.copyWith(
+      healthyEatingTemplateId: healthyEatingTemplateId == freezed
+          ? _value.healthyEatingTemplateId
+          : healthyEatingTemplateId // ignore: cast_nullable_to_non_nullable
+              as int,
+      createdBy: createdBy == freezed
+          ? _value.createdBy
+          : createdBy // ignore: cast_nullable_to_non_nullable
+              as StringSingleLine,
+      createdDate: createdDate == freezed
+          ? _value.createdDate
+          : createdDate // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      lastModifiedBy: lastModifiedBy == freezed
+          ? _value.lastModifiedBy
+          : lastModifiedBy // ignore: cast_nullable_to_non_nullable
+              as StringSingleLine,
+      lastModifiedDate: lastModifiedDate == freezed
+          ? _value.lastModifiedDate
+          : lastModifiedDate // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      active: active == freezed
+          ? _value.active
+          : active // ignore: cast_nullable_to_non_nullable
+              as int,
+      additionalInstructions: additionalInstructions == freezed
+          ? _value.additionalInstructions
+          : additionalInstructions // ignore: cast_nullable_to_non_nullable
+              as StringMultiLine,
+      fishServings: fishServings == freezed
+          ? _value.fishServings
+          : fishServings // ignore: cast_nullable_to_non_nullable
+              as int,
+      nutritionTrackingDays: nutritionTrackingDays == freezed
+          ? _value.nutritionTrackingDays
+          : nutritionTrackingDays // ignore: cast_nullable_to_non_nullable
+              as int,
+      proteinServing: proteinServing == freezed
+          ? _value.proteinServing
+          : proteinServing // ignore: cast_nullable_to_non_nullable
+              as int,
+    ));
+  }
+}
+
+/// @nodoc
+abstract class _$HealthyEatingTemplatesCopyWith<$Res>
+    implements $HealthyEatingTemplatesCopyWith<$Res> {
+  factory _$HealthyEatingTemplatesCopyWith(_HealthyEatingTemplates value,
+          $Res Function(_HealthyEatingTemplates) then) =
+      __$HealthyEatingTemplatesCopyWithImpl<$Res>;
+  @override
+  $Res call(
+      {int healthyEatingTemplateId,
+      StringSingleLine createdBy,
+      DateTime createdDate,
+      StringSingleLine lastModifiedBy,
+      DateTime lastModifiedDate,
+      int active,
+      StringMultiLine additionalInstructions,
+      int fishServings,
+      int nutritionTrackingDays,
+      int proteinServing});
+}
+
+/// @nodoc
+class __$HealthyEatingTemplatesCopyWithImpl<$Res>
+    extends _$HealthyEatingTemplatesCopyWithImpl<$Res>
+    implements _$HealthyEatingTemplatesCopyWith<$Res> {
+  __$HealthyEatingTemplatesCopyWithImpl(_HealthyEatingTemplates _value,
+      $Res Function(_HealthyEatingTemplates) _then)
+      : super(_value, (v) => _then(v as _HealthyEatingTemplates));
+
+  @override
+  _HealthyEatingTemplates get _value => super._value as _HealthyEatingTemplates;
+
+  @override
+  $Res call({
+    Object? healthyEatingTemplateId = freezed,
+    Object? createdBy = freezed,
+    Object? createdDate = freezed,
+    Object? lastModifiedBy = freezed,
+    Object? lastModifiedDate = freezed,
+    Object? active = freezed,
+    Object? additionalInstructions = freezed,
+    Object? fishServings = freezed,
+    Object? nutritionTrackingDays = freezed,
+    Object? proteinServing = freezed,
+  }) {
+    return _then(_HealthyEatingTemplates(
+      healthyEatingTemplateId: healthyEatingTemplateId == freezed
+          ? _value.healthyEatingTemplateId
+          : healthyEatingTemplateId // ignore: cast_nullable_to_non_nullable
+              as int,
+      createdBy: createdBy == freezed
+          ? _value.createdBy
+          : createdBy // ignore: cast_nullable_to_non_nullable
+              as StringSingleLine,
+      createdDate: createdDate == freezed
+          ? _value.createdDate
+          : createdDate // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      lastModifiedBy: lastModifiedBy == freezed
+          ? _value.lastModifiedBy
+          : lastModifiedBy // ignore: cast_nullable_to_non_nullable
+              as StringSingleLine,
+      lastModifiedDate: lastModifiedDate == freezed
+          ? _value.lastModifiedDate
+          : lastModifiedDate // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      active: active == freezed
+          ? _value.active
+          : active // ignore: cast_nullable_to_non_nullable
+              as int,
+      additionalInstructions: additionalInstructions == freezed
+          ? _value.additionalInstructions
+          : additionalInstructions // ignore: cast_nullable_to_non_nullable
+              as StringMultiLine,
+      fishServings: fishServings == freezed
+          ? _value.fishServings
+          : fishServings // ignore: cast_nullable_to_non_nullable
+              as int,
+      nutritionTrackingDays: nutritionTrackingDays == freezed
+          ? _value.nutritionTrackingDays
+          : nutritionTrackingDays // ignore: cast_nullable_to_non_nullable
+              as int,
+      proteinServing: proteinServing == freezed
+          ? _value.proteinServing
+          : proteinServing // ignore: cast_nullable_to_non_nullable
+              as int,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$_HealthyEatingTemplates extends _HealthyEatingTemplates {
+  _$_HealthyEatingTemplates(
+      {required this.healthyEatingTemplateId,
+      required this.createdBy,
+      required this.createdDate,
+      required this.lastModifiedBy,
+      required this.lastModifiedDate,
+      required this.active,
+      required this.additionalInstructions,
+      required this.fishServings,
+      required this.nutritionTrackingDays,
+      required this.proteinServing})
+      : super._();
+
+  @override
+  final int healthyEatingTemplateId;
+  @override
+  final StringSingleLine createdBy;
+  @override
+  final DateTime createdDate;
+  @override
+  final StringSingleLine lastModifiedBy;
+  @override
+  final DateTime lastModifiedDate;
+  @override
+  final int active;
+  @override
+  final StringMultiLine additionalInstructions;
+  @override
+  final int fishServings;
+  @override
+  final int nutritionTrackingDays;
+  @override
+  final int proteinServing;
+
+  @override
+  String toString() {
+    return 'HealthyEatingTemplates(healthyEatingTemplateId: $healthyEatingTemplateId, createdBy: $createdBy, createdDate: $createdDate, lastModifiedBy: $lastModifiedBy, lastModifiedDate: $lastModifiedDate, active: $active, additionalInstructions: $additionalInstructions, fishServings: $fishServings, nutritionTrackingDays: $nutritionTrackingDays, proteinServing: $proteinServing)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other is _HealthyEatingTemplates &&
+            (identical(
+                    other.healthyEatingTemplateId, healthyEatingTemplateId) ||
+                const DeepCollectionEquality().equals(
+                    other.healthyEatingTemplateId, healthyEatingTemplateId)) &&
+            (identical(other.createdBy, createdBy) ||
+                const DeepCollectionEquality()
+                    .equals(other.createdBy, createdBy)) &&
+            (identical(other.createdDate, createdDate) ||
+                const DeepCollectionEquality()
+                    .equals(other.createdDate, createdDate)) &&
+            (identical(other.lastModifiedBy, lastModifiedBy) ||
+                const DeepCollectionEquality()
+                    .equals(other.lastModifiedBy, lastModifiedBy)) &&
+            (identical(other.lastModifiedDate, lastModifiedDate) ||
+                const DeepCollectionEquality()
+                    .equals(other.lastModifiedDate, lastModifiedDate)) &&
+            (identical(other.active, active) ||
+                const DeepCollectionEquality().equals(other.active, active)) &&
+            (identical(other.additionalInstructions, additionalInstructions) ||
+                const DeepCollectionEquality().equals(
+                    other.additionalInstructions, additionalInstructions)) &&
+            (identical(other.fishServings, fishServings) ||
+                const DeepCollectionEquality()
+                    .equals(other.fishServings, fishServings)) &&
+            (identical(other.nutritionTrackingDays, nutritionTrackingDays) ||
+                const DeepCollectionEquality().equals(
+                    other.nutritionTrackingDays, nutritionTrackingDays)) &&
+            (identical(other.proteinServing, proteinServing) ||
+                const DeepCollectionEquality()
+                    .equals(other.proteinServing, proteinServing)));
+  }
+
+  @override
+  int get hashCode =>
+      runtimeType.hashCode ^
+      const DeepCollectionEquality().hash(healthyEatingTemplateId) ^
+      const DeepCollectionEquality().hash(createdBy) ^
+      const DeepCollectionEquality().hash(createdDate) ^
+      const DeepCollectionEquality().hash(lastModifiedBy) ^
+      const DeepCollectionEquality().hash(lastModifiedDate) ^
+      const DeepCollectionEquality().hash(active) ^
+      const DeepCollectionEquality().hash(additionalInstructions) ^
+      const DeepCollectionEquality().hash(fishServings) ^
+      const DeepCollectionEquality().hash(nutritionTrackingDays) ^
+      const DeepCollectionEquality().hash(proteinServing);
+
+  @JsonKey(ignore: true)
+  @override
+  _$HealthyEatingTemplatesCopyWith<_HealthyEatingTemplates> get copyWith =>
+      __$HealthyEatingTemplatesCopyWithImpl<_HealthyEatingTemplates>(
+          this, _$identity);
+}
+
+abstract class _HealthyEatingTemplates extends HealthyEatingTemplates {
+  factory _HealthyEatingTemplates(
+      {required int healthyEatingTemplateId,
+      required StringSingleLine createdBy,
+      required DateTime createdDate,
+      required StringSingleLine lastModifiedBy,
+      required DateTime lastModifiedDate,
+      required int active,
+      required StringMultiLine additionalInstructions,
+      required int fishServings,
+      required int nutritionTrackingDays,
+      required int proteinServing}) = _$_HealthyEatingTemplates;
+  _HealthyEatingTemplates._() : super._();
+
+  @override
+  int get healthyEatingTemplateId => throw _privateConstructorUsedError;
+  @override
+  StringSingleLine get createdBy => throw _privateConstructorUsedError;
+  @override
+  DateTime get createdDate => throw _privateConstructorUsedError;
+  @override
+  StringSingleLine get lastModifiedBy => throw _privateConstructorUsedError;
+  @override
+  DateTime get lastModifiedDate => throw _privateConstructorUsedError;
+  @override
+  int get active => throw _privateConstructorUsedError;
+  @override
+  StringMultiLine get additionalInstructions =>
+      throw _privateConstructorUsedError;
+  @override
+  int get fishServings => throw _privateConstructorUsedError;
+  @override
+  int get nutritionTrackingDays => throw _privateConstructorUsedError;
+  @override
+  int get proteinServing => throw _privateConstructorUsedError;
+  @override
+  @JsonKey(ignore: true)
+  _$HealthyEatingTemplatesCopyWith<_HealthyEatingTemplates> get copyWith =>
+      throw _privateConstructorUsedError;
+}

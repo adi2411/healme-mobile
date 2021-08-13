156efd36be82552cf880f4ce44f9797a33600e05
aditya <aditya_jinegar@yahoo.com>
2021-08-13 12:09:21 -0400

    Create programTemplatesProgramWeeks.freezed.dart

---
+++ lib/domain/Templates/ProgramWeekTemplates/ProgramTemplatesProgramWeeks/programTemplatesProgramWeeks.freezed.dart
@@ -0,0 +1,195 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides
+
+part of 'programTemplatesProgramWeeks.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more informations: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+class _$ProgramTemplatesProgramWeeksTearOff {
+  const _$ProgramTemplatesProgramWeeksTearOff();
+
+  _ProgramTemplatesProgramWeeks call(
+      {required int programTemplateProgramTemplateId,
+      required int programWeeksProgramWeekTemplateId}) {
+    return _ProgramTemplatesProgramWeeks(
+      programTemplateProgramTemplateId: programTemplateProgramTemplateId,
+      programWeeksProgramWeekTemplateId: programWeeksProgramWeekTemplateId,
+    );
+  }
+}
+
+/// @nodoc
+const $ProgramTemplatesProgramWeeks = _$ProgramTemplatesProgramWeeksTearOff();
+
+/// @nodoc
+mixin _$ProgramTemplatesProgramWeeks {
+  int get programTemplateProgramTemplateId =>
+      throw _privateConstructorUsedError;
+  int get programWeeksProgramWeekTemplateId =>
+      throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $ProgramTemplatesProgramWeeksCopyWith<ProgramTemplatesProgramWeeks>
+      get copyWith => throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $ProgramTemplatesProgramWeeksCopyWith<$Res> {
+  factory $ProgramTemplatesProgramWeeksCopyWith(
+          ProgramTemplatesProgramWeeks value,
+          $Res Function(ProgramTemplatesProgramWeeks) then) =
+      _$ProgramTemplatesProgramWeeksCopyWithImpl<$Res>;
+  $Res call(
+      {int programTemplateProgramTemplateId,
+      int programWeeksProgramWeekTemplateId});
+}
+
+/// @nodoc
+class _$ProgramTemplatesProgramWeeksCopyWithImpl<$Res>
+    implements $ProgramTemplatesProgramWeeksCopyWith<$Res> {
+  _$ProgramTemplatesProgramWeeksCopyWithImpl(this._value, this._then);
+
+  final ProgramTemplatesProgramWeeks _value;
+  // ignore: unused_field
+  final $Res Function(ProgramTemplatesProgramWeeks) _then;
+
+  @override
+  $Res call({
+    Object? programTemplateProgramTemplateId = freezed,
+    Object? programWeeksProgramWeekTemplateId = freezed,
+  }) {
+    return _then(_value.copyWith(
+      programTemplateProgramTemplateId: programTemplateProgramTemplateId ==
+              freezed
+          ? _value.programTemplateProgramTemplateId
+          : programTemplateProgramTemplateId // ignore: cast_nullable_to_non_nullable
+              as int,
+      programWeeksProgramWeekTemplateId: programWeeksProgramWeekTemplateId ==
+              freezed
+          ? _value.programWeeksProgramWeekTemplateId
+          : programWeeksProgramWeekTemplateId // ignore: cast_nullable_to_non_nullable
+              as int,
+    ));
+  }
+}
+
+/// @nodoc
+abstract class _$ProgramTemplatesProgramWeeksCopyWith<$Res>
+    implements $ProgramTemplatesProgramWeeksCopyWith<$Res> {
+  factory _$ProgramTemplatesProgramWeeksCopyWith(
+          _ProgramTemplatesProgramWeeks value,
+          $Res Function(_ProgramTemplatesProgramWeeks) then) =
+      __$ProgramTemplatesProgramWeeksCopyWithImpl<$Res>;
+  @override
+  $Res call(
+      {int programTemplateProgramTemplateId,
+      int programWeeksProgramWeekTemplateId});
+}
+
+/// @nodoc
+class __$ProgramTemplatesProgramWeeksCopyWithImpl<$Res>
+    extends _$ProgramTemplatesProgramWeeksCopyWithImpl<$Res>
+    implements _$ProgramTemplatesProgramWeeksCopyWith<$Res> {
+  __$ProgramTemplatesProgramWeeksCopyWithImpl(
+      _ProgramTemplatesProgramWeeks _value,
+      $Res Function(_ProgramTemplatesProgramWeeks) _then)
+      : super(_value, (v) => _then(v as _ProgramTemplatesProgramWeeks));
+
+  @override
+  _ProgramTemplatesProgramWeeks get _value =>
+      super._value as _ProgramTemplatesProgramWeeks;
+
+  @override
+  $Res call({
+    Object? programTemplateProgramTemplateId = freezed,
+    Object? programWeeksProgramWeekTemplateId = freezed,
+  }) {
+    return _then(_ProgramTemplatesProgramWeeks(
+      programTemplateProgramTemplateId: programTemplateProgramTemplateId ==
+              freezed
+          ? _value.programTemplateProgramTemplateId
+          : programTemplateProgramTemplateId // ignore: cast_nullable_to_non_nullable
+              as int,
+      programWeeksProgramWeekTemplateId: programWeeksProgramWeekTemplateId ==
+              freezed
+          ? _value.programWeeksProgramWeekTemplateId
+          : programWeeksProgramWeekTemplateId // ignore: cast_nullable_to_non_nullable
+              as int,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$_ProgramTemplatesProgramWeeks extends _ProgramTemplatesProgramWeeks {
+  _$_ProgramTemplatesProgramWeeks(
+      {required this.programTemplateProgramTemplateId,
+      required this.programWeeksProgramWeekTemplateId})
+      : super._();
+
+  @override
+  final int programTemplateProgramTemplateId;
+  @override
+  final int programWeeksProgramWeekTemplateId;
+
+  @override
+  String toString() {
+    return 'ProgramTemplatesProgramWeeks(programTemplateProgramTemplateId: $programTemplateProgramTemplateId, programWeeksProgramWeekTemplateId: $programWeeksProgramWeekTemplateId)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other is _ProgramTemplatesProgramWeeks &&
+            (identical(other.programTemplateProgramTemplateId,
+                    programTemplateProgramTemplateId) ||
+                const DeepCollectionEquality().equals(
+                    other.programTemplateProgramTemplateId,
+                    programTemplateProgramTemplateId)) &&
+            (identical(other.programWeeksProgramWeekTemplateId,
+                    programWeeksProgramWeekTemplateId) ||
+                const DeepCollectionEquality().equals(
+                    other.programWeeksProgramWeekTemplateId,
+                    programWeeksProgramWeekTemplateId)));
+  }
+
+  @override
+  int get hashCode =>
+      runtimeType.hashCode ^
+      const DeepCollectionEquality().hash(programTemplateProgramTemplateId) ^
+      const DeepCollectionEquality().hash(programWeeksProgramWeekTemplateId);
+
+  @JsonKey(ignore: true)
+  @override
+  _$ProgramTemplatesProgramWeeksCopyWith<_ProgramTemplatesProgramWeeks>
+      get copyWith => __$ProgramTemplatesProgramWeeksCopyWithImpl<
+          _ProgramTemplatesProgramWeeks>(this, _$identity);
+}
+
+abstract class _ProgramTemplatesProgramWeeks
+    extends ProgramTemplatesProgramWeeks {
+  factory _ProgramTemplatesProgramWeeks(
+          {required int programTemplateProgramTemplateId,
+          required int programWeeksProgramWeekTemplateId}) =
+      _$_ProgramTemplatesProgramWeeks;
+  _ProgramTemplatesProgramWeeks._() : super._();
+
+  @override
+  int get programTemplateProgramTemplateId =>
+      throw _privateConstructorUsedError;
+  @override
+  int get programWeeksProgramWeekTemplateId =>
+      throw _privateConstructorUsedError;
+  @override
+  @JsonKey(ignore: true)
+  _$ProgramTemplatesProgramWeeksCopyWith<_ProgramTemplatesProgramWeeks>
+      get copyWith => throw _privateConstructorUsedError;
+}

ac2f441fb81f415626be0fa92d3256d422d34175
aditya <aditya_jinegar@yahoo.com>
2021-08-13 12:08:36 -0400

    Create recipeTemplates.freezed.dart

---
+++ lib/domain/Templates/HealthyEatingTemplates/RecipeTemplates/recipeTemplates.freezed.dart
@@ -0,0 +1,275 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides
+
+part of 'recipeTemplates.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more informations: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+class _$RecipeTemplatesTearOff {
+  const _$RecipeTemplatesTearOff();
+
+  _RecipeTemplates call(
+      {required int recipeTemplateId,
+      required StringSingleLine createdBy,
+      required DateTime createdDate,
+      required StringSingleLine lastModifiedBy,
+      required DateTime lastModifiedDate,
+      required int active}) {
+    return _RecipeTemplates(
+      recipeTemplateId: recipeTemplateId,
+      createdBy: createdBy,
+      createdDate: createdDate,
+      lastModifiedBy: lastModifiedBy,
+      lastModifiedDate: lastModifiedDate,
+      active: active,
+    );
+  }
+}
+
+/// @nodoc
+const $RecipeTemplates = _$RecipeTemplatesTearOff();
+
+/// @nodoc
+mixin _$RecipeTemplates {
+  int get recipeTemplateId => throw _privateConstructorUsedError;
+  StringSingleLine get createdBy => throw _privateConstructorUsedError;
+  DateTime get createdDate => throw _privateConstructorUsedError;
+  StringSingleLine get lastModifiedBy => throw _privateConstructorUsedError;
+  DateTime get lastModifiedDate => throw _privateConstructorUsedError;
+  int get active => throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $RecipeTemplatesCopyWith<RecipeTemplates> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $RecipeTemplatesCopyWith<$Res> {
+  factory $RecipeTemplatesCopyWith(
+          RecipeTemplates value, $Res Function(RecipeTemplates) then) =
+      _$RecipeTemplatesCopyWithImpl<$Res>;
+  $Res call(
+      {int recipeTemplateId,
+      StringSingleLine createdBy,
+      DateTime createdDate,
+      StringSingleLine lastModifiedBy,
+      DateTime lastModifiedDate,
+      int active});
+}
+
+/// @nodoc
+class _$RecipeTemplatesCopyWithImpl<$Res>
+    implements $RecipeTemplatesCopyWith<$Res> {
+  _$RecipeTemplatesCopyWithImpl(this._value, this._then);
+
+  final RecipeTemplates _value;
+  // ignore: unused_field
+  final $Res Function(RecipeTemplates) _then;
+
+  @override
+  $Res call({
+    Object? recipeTemplateId = freezed,
+    Object? createdBy = freezed,
+    Object? createdDate = freezed,
+    Object? lastModifiedBy = freezed,
+    Object? lastModifiedDate = freezed,
+    Object? active = freezed,
+  }) {
+    return _then(_value.copyWith(
+      recipeTemplateId: recipeTemplateId == freezed
+          ? _value.recipeTemplateId
+          : recipeTemplateId // ignore: cast_nullable_to_non_nullable
+              as int,
+      createdBy: createdBy == freezed
+          ? _value.createdBy
+          : createdBy // ignore: cast_nullable_to_non_nullable
+              as StringSingleLine,
+      createdDate: createdDate == freezed
+          ? _value.createdDate
+          : createdDate // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      lastModifiedBy: lastModifiedBy == freezed
+          ? _value.lastModifiedBy
+          : lastModifiedBy // ignore: cast_nullable_to_non_nullable
+              as StringSingleLine,
+      lastModifiedDate: lastModifiedDate == freezed
+          ? _value.lastModifiedDate
+          : lastModifiedDate // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      active: active == freezed
+          ? _value.active
+          : active // ignore: cast_nullable_to_non_nullable
+              as int,
+    ));
+  }
+}
+
+/// @nodoc
+abstract class _$RecipeTemplatesCopyWith<$Res>
+    implements $RecipeTemplatesCopyWith<$Res> {
+  factory _$RecipeTemplatesCopyWith(
+          _RecipeTemplates value, $Res Function(_RecipeTemplates) then) =
+      __$RecipeTemplatesCopyWithImpl<$Res>;
+  @override
+  $Res call(
+      {int recipeTemplateId,
+      StringSingleLine createdBy,
+      DateTime createdDate,
+      StringSingleLine lastModifiedBy,
+      DateTime lastModifiedDate,
+      int active});
+}
+
+/// @nodoc
+class __$RecipeTemplatesCopyWithImpl<$Res>
+    extends _$RecipeTemplatesCopyWithImpl<$Res>
+    implements _$RecipeTemplatesCopyWith<$Res> {
+  __$RecipeTemplatesCopyWithImpl(
+      _RecipeTemplates _value, $Res Function(_RecipeTemplates) _then)
+      : super(_value, (v) => _then(v as _RecipeTemplates));
+
+  @override
+  _RecipeTemplates get _value => super._value as _RecipeTemplates;
+
+  @override
+  $Res call({
+    Object? recipeTemplateId = freezed,
+    Object? createdBy = freezed,
+    Object? createdDate = freezed,
+    Object? lastModifiedBy = freezed,
+    Object? lastModifiedDate = freezed,
+    Object? active = freezed,
+  }) {
+    return _then(_RecipeTemplates(
+      recipeTemplateId: recipeTemplateId == freezed
+          ? _value.recipeTemplateId
+          : recipeTemplateId // ignore: cast_nullable_to_non_nullable
+              as int,
+      createdBy: createdBy == freezed
+          ? _value.createdBy
+          : createdBy // ignore: cast_nullable_to_non_nullable
+              as StringSingleLine,
+      createdDate: createdDate == freezed
+          ? _value.createdDate
+          : createdDate // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      lastModifiedBy: lastModifiedBy == freezed
+          ? _value.lastModifiedBy
+          : lastModifiedBy // ignore: cast_nullable_to_non_nullable
+              as StringSingleLine,
+      lastModifiedDate: lastModifiedDate == freezed
+          ? _value.lastModifiedDate
+          : lastModifiedDate // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      active: active == freezed
+          ? _value.active
+          : active // ignore: cast_nullable_to_non_nullable
+              as int,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$_RecipeTemplates extends _RecipeTemplates {
+  _$_RecipeTemplates(
+      {required this.recipeTemplateId,
+      required this.createdBy,
+      required this.createdDate,
+      required this.lastModifiedBy,
+      required this.lastModifiedDate,
+      required this.active})
+      : super._();
+
+  @override
+  final int recipeTemplateId;
+  @override
+  final StringSingleLine createdBy;
+  @override
+  final DateTime createdDate;
+  @override
+  final StringSingleLine lastModifiedBy;
+  @override
+  final DateTime lastModifiedDate;
+  @override
+  final int active;
+
+  @override
+  String toString() {
+    return 'RecipeTemplates(recipeTemplateId: $recipeTemplateId, createdBy: $createdBy, createdDate: $createdDate, lastModifiedBy: $lastModifiedBy, lastModifiedDate: $lastModifiedDate, active: $active)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other is _RecipeTemplates &&
+            (identical(other.recipeTemplateId, recipeTemplateId) ||
+                const DeepCollectionEquality()
+                    .equals(other.recipeTemplateId, recipeTemplateId)) &&
+            (identical(other.createdBy, createdBy) ||
+                const DeepCollectionEquality()
+                    .equals(other.createdBy, createdBy)) &&
+            (identical(other.createdDate, createdDate) ||
+                const DeepCollectionEquality()
+                    .equals(other.createdDate, createdDate)) &&
+            (identical(other.lastModifiedBy, lastModifiedBy) ||
+                const DeepCollectionEquality()
+                    .equals(other.lastModifiedBy, lastModifiedBy)) &&
+            (identical(other.lastModifiedDate, lastModifiedDate) ||
+                const DeepCollectionEquality()
+                    .equals(other.lastModifiedDate, lastModifiedDate)) &&
+            (identical(other.active, active) ||
+                const DeepCollectionEquality().equals(other.active, active)));
+  }
+
+  @override
+  int get hashCode =>
+      runtimeType.hashCode ^
+      const DeepCollectionEquality().hash(recipeTemplateId) ^
+      const DeepCollectionEquality().hash(createdBy) ^
+      const DeepCollectionEquality().hash(createdDate) ^
+      const DeepCollectionEquality().hash(lastModifiedBy) ^
+      const DeepCollectionEquality().hash(lastModifiedDate) ^
+      const DeepCollectionEquality().hash(active);
+
+  @JsonKey(ignore: true)
+  @override
+  _$RecipeTemplatesCopyWith<_RecipeTemplates> get copyWith =>
+      __$RecipeTemplatesCopyWithImpl<_RecipeTemplates>(this, _$identity);
+}
+
+abstract class _RecipeTemplates extends RecipeTemplates {
+  factory _RecipeTemplates(
+      {required int recipeTemplateId,
+      required StringSingleLine createdBy,
+      required DateTime createdDate,
+      required StringSingleLine lastModifiedBy,
+      required DateTime lastModifiedDate,
+      required int active}) = _$_RecipeTemplates;
+  _RecipeTemplates._() : super._();
+
+  @override
+  int get recipeTemplateId => throw _privateConstructorUsedError;
+  @override
+  StringSingleLine get createdBy => throw _privateConstructorUsedError;
+  @override
+  DateTime get createdDate => throw _privateConstructorUsedError;
+  @override
+  StringSingleLine get lastModifiedBy => throw _privateConstructorUsedError;
+  @override
+  DateTime get lastModifiedDate => throw _privateConstructorUsedError;
+  @override
+  int get active => throw _privateConstructorUsedError;
+  @override
+  @JsonKey(ignore: true)
+  _$RecipeTemplatesCopyWith<_RecipeTemplates> get copyWith =>
+      throw _privateConstructorUsedError;
+}

917e91cebd1fecfa94eba9ffd8ab955b6d8d486c
aditya <aditya_jinegar@yahoo.com>
2021-08-13 13:51:10 -0400

    Create routineTemplates.freezed.dart

---
+++ lib/domain/Templates/AppOrganizations/RoutineTemplates/routineTemplates.freezed.dart
@@ -0,0 +1,275 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides
+
+part of 'routineTemplates.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more informations: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+class _$RoutineTemplatesTearOff {
+  const _$RoutineTemplatesTearOff();
+
+  _RoutineTemplates call(
+      {required int routineTemplateId,
+      required StringSingleLine createdBy,
+      required DateTime createdDate,
+      required StringSingleLine lastModifiedBy,
+      required DateTime lastModifiedDate,
+      required int active}) {
+    return _RoutineTemplates(
+      routineTemplateId: routineTemplateId,
+      createdBy: createdBy,
+      createdDate: createdDate,
+      lastModifiedBy: lastModifiedBy,
+      lastModifiedDate: lastModifiedDate,
+      active: active,
+    );
+  }
+}
+
+/// @nodoc
+const $RoutineTemplates = _$RoutineTemplatesTearOff();
+
+/// @nodoc
+mixin _$RoutineTemplates {
+  int get routineTemplateId => throw _privateConstructorUsedError;
+  StringSingleLine get createdBy => throw _privateConstructorUsedError;
+  DateTime get createdDate => throw _privateConstructorUsedError;
+  StringSingleLine get lastModifiedBy => throw _privateConstructorUsedError;
+  DateTime get lastModifiedDate => throw _privateConstructorUsedError;
+  int get active => throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $RoutineTemplatesCopyWith<RoutineTemplates> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $RoutineTemplatesCopyWith<$Res> {
+  factory $RoutineTemplatesCopyWith(
+          RoutineTemplates value, $Res Function(RoutineTemplates) then) =
+      _$RoutineTemplatesCopyWithImpl<$Res>;
+  $Res call(
+      {int routineTemplateId,
+      StringSingleLine createdBy,
+      DateTime createdDate,
+      StringSingleLine lastModifiedBy,
+      DateTime lastModifiedDate,
+      int active});
+}
+
+/// @nodoc
+class _$RoutineTemplatesCopyWithImpl<$Res>
+    implements $RoutineTemplatesCopyWith<$Res> {
+  _$RoutineTemplatesCopyWithImpl(this._value, this._then);
+
+  final RoutineTemplates _value;
+  // ignore: unused_field
+  final $Res Function(RoutineTemplates) _then;
+
+  @override
+  $Res call({
+    Object? routineTemplateId = freezed,
+    Object? createdBy = freezed,
+    Object? createdDate = freezed,
+    Object? lastModifiedBy = freezed,
+    Object? lastModifiedDate = freezed,
+    Object? active = freezed,
+  }) {
+    return _then(_value.copyWith(
+      routineTemplateId: routineTemplateId == freezed
+          ? _value.routineTemplateId
+          : routineTemplateId // ignore: cast_nullable_to_non_nullable
+              as int,
+      createdBy: createdBy == freezed
+          ? _value.createdBy
+          : createdBy // ignore: cast_nullable_to_non_nullable
+              as StringSingleLine,
+      createdDate: createdDate == freezed
+          ? _value.createdDate
+          : createdDate // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      lastModifiedBy: lastModifiedBy == freezed
+          ? _value.lastModifiedBy
+          : lastModifiedBy // ignore: cast_nullable_to_non_nullable
+              as StringSingleLine,
+      lastModifiedDate: lastModifiedDate == freezed
+          ? _value.lastModifiedDate
+          : lastModifiedDate // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      active: active == freezed
+          ? _value.active
+          : active // ignore: cast_nullable_to_non_nullable
+              as int,
+    ));
+  }
+}
+
+/// @nodoc
+abstract class _$RoutineTemplatesCopyWith<$Res>
+    implements $RoutineTemplatesCopyWith<$Res> {
+  factory _$RoutineTemplatesCopyWith(
+          _RoutineTemplates value, $Res Function(_RoutineTemplates) then) =
+      __$RoutineTemplatesCopyWithImpl<$Res>;
+  @override
+  $Res call(
+      {int routineTemplateId,
+      StringSingleLine createdBy,
+      DateTime createdDate,
+      StringSingleLine lastModifiedBy,
+      DateTime lastModifiedDate,
+      int active});
+}
+
+/// @nodoc
+class __$RoutineTemplatesCopyWithImpl<$Res>
+    extends _$RoutineTemplatesCopyWithImpl<$Res>
+    implements _$RoutineTemplatesCopyWith<$Res> {
+  __$RoutineTemplatesCopyWithImpl(
+      _RoutineTemplates _value, $Res Function(_RoutineTemplates) _then)
+      : super(_value, (v) => _then(v as _RoutineTemplates));
+
+  @override
+  _RoutineTemplates get _value => super._value as _RoutineTemplates;
+
+  @override
+  $Res call({
+    Object? routineTemplateId = freezed,
+    Object? createdBy = freezed,
+    Object? createdDate = freezed,
+    Object? lastModifiedBy = freezed,
+    Object? lastModifiedDate = freezed,
+    Object? active = freezed,
+  }) {
+    return _then(_RoutineTemplates(
+      routineTemplateId: routineTemplateId == freezed
+          ? _value.routineTemplateId
+          : routineTemplateId // ignore: cast_nullable_to_non_nullable
+              as int,
+      createdBy: createdBy == freezed
+          ? _value.createdBy
+          : createdBy // ignore: cast_nullable_to_non_nullable
+              as StringSingleLine,
+      createdDate: createdDate == freezed
+          ? _value.createdDate
+          : createdDate // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      lastModifiedBy: lastModifiedBy == freezed
+          ? _value.lastModifiedBy
+          : lastModifiedBy // ignore: cast_nullable_to_non_nullable
+              as StringSingleLine,
+      lastModifiedDate: lastModifiedDate == freezed
+          ? _value.lastModifiedDate
+          : lastModifiedDate // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      active: active == freezed
+          ? _value.active
+          : active // ignore: cast_nullable_to_non_nullable
+              as int,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$_RoutineTemplates extends _RoutineTemplates {
+  _$_RoutineTemplates(
+      {required this.routineTemplateId,
+      required this.createdBy,
+      required this.createdDate,
+      required this.lastModifiedBy,
+      required this.lastModifiedDate,
+      required this.active})
+      : super._();
+
+  @override
+  final int routineTemplateId;
+  @override
+  final StringSingleLine createdBy;
+  @override
+  final DateTime createdDate;
+  @override
+  final StringSingleLine lastModifiedBy;
+  @override
+  final DateTime lastModifiedDate;
+  @override
+  final int active;
+
+  @override
+  String toString() {
+    return 'RoutineTemplates(routineTemplateId: $routineTemplateId, createdBy: $createdBy, createdDate: $createdDate, lastModifiedBy: $lastModifiedBy, lastModifiedDate: $lastModifiedDate, active: $active)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other is _RoutineTemplates &&
+            (identical(other.routineTemplateId, routineTemplateId) ||
+                const DeepCollectionEquality()
+                    .equals(other.routineTemplateId, routineTemplateId)) &&
+            (identical(other.createdBy, createdBy) ||
+                const DeepCollectionEquality()
+                    .equals(other.createdBy, createdBy)) &&
+            (identical(other.createdDate, createdDate) ||
+                const DeepCollectionEquality()
+                    .equals(other.createdDate, createdDate)) &&
+            (identical(other.lastModifiedBy, lastModifiedBy) ||
+                const DeepCollectionEquality()
+                    .equals(other.lastModifiedBy, lastModifiedBy)) &&
+            (identical(other.lastModifiedDate, lastModifiedDate) ||
+                const DeepCollectionEquality()
+                    .equals(other.lastModifiedDate, lastModifiedDate)) &&
+            (identical(other.active, active) ||
+                const DeepCollectionEquality().equals(other.active, active)));
+  }
+
+  @override
+  int get hashCode =>
+      runtimeType.hashCode ^
+      const DeepCollectionEquality().hash(routineTemplateId) ^
+      const DeepCollectionEquality().hash(createdBy) ^
+      const DeepCollectionEquality().hash(createdDate) ^
+      const DeepCollectionEquality().hash(lastModifiedBy) ^
+      const DeepCollectionEquality().hash(lastModifiedDate) ^
+      const DeepCollectionEquality().hash(active);
+
+  @JsonKey(ignore: true)
+  @override
+  _$RoutineTemplatesCopyWith<_RoutineTemplates> get copyWith =>
+      __$RoutineTemplatesCopyWithImpl<_RoutineTemplates>(this, _$identity);
+}
+
+abstract class _RoutineTemplates extends RoutineTemplates {
+  factory _RoutineTemplates(
+      {required int routineTemplateId,
+      required StringSingleLine createdBy,
+      required DateTime createdDate,
+      required StringSingleLine lastModifiedBy,
+      required DateTime lastModifiedDate,
+      required int active}) = _$_RoutineTemplates;
+  _RoutineTemplates._() : super._();
+
+  @override
+  int get routineTemplateId => throw _privateConstructorUsedError;
+  @override
+  StringSingleLine get createdBy => throw _privateConstructorUsedError;
+  @override
+  DateTime get createdDate => throw _privateConstructorUsedError;
+  @override
+  StringSingleLine get lastModifiedBy => throw _privateConstructorUsedError;
+  @override
+  DateTime get lastModifiedDate => throw _privateConstructorUsedError;
+  @override
+  int get active => throw _privateConstructorUsedError;
+  @override
+  @JsonKey(ignore: true)
+  _$RoutineTemplatesCopyWith<_RoutineTemplates> get copyWith =>
+      throw _privateConstructorUsedError;
+}

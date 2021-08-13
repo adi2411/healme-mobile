bb143094b2a986aba09835059179c7dca41628bb
aditya <aditya_jinegar@yahoo.com>
2021-08-13 13:51:40 -0400

    Create recipeProteinTemplates.freezed.dart

---
+++ lib/domain/Templates/HealthyEatingTemplates/RecipeProteinTemplates/recipeProteinTemplates.freezed.dart
@@ -0,0 +1,278 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides
+
+part of 'recipeProteinTemplates.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more informations: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+class _$RecipeProteinTemplatesTearOff {
+  const _$RecipeProteinTemplatesTearOff();
+
+  _RecipeProteinTemplates call(
+      {required int recipeProteinTemplateId,
+      required StringSingleLine createdBy,
+      required DateTime createdDate,
+      required StringSingleLine lastModifiedBy,
+      required DateTime lastModifiedDate,
+      required StringSingleLine proteinName}) {
+    return _RecipeProteinTemplates(
+      recipeProteinTemplateId: recipeProteinTemplateId,
+      createdBy: createdBy,
+      createdDate: createdDate,
+      lastModifiedBy: lastModifiedBy,
+      lastModifiedDate: lastModifiedDate,
+      proteinName: proteinName,
+    );
+  }
+}
+
+/// @nodoc
+const $RecipeProteinTemplates = _$RecipeProteinTemplatesTearOff();
+
+/// @nodoc
+mixin _$RecipeProteinTemplates {
+  int get recipeProteinTemplateId => throw _privateConstructorUsedError;
+  StringSingleLine get createdBy => throw _privateConstructorUsedError;
+  DateTime get createdDate => throw _privateConstructorUsedError;
+  StringSingleLine get lastModifiedBy => throw _privateConstructorUsedError;
+  DateTime get lastModifiedDate => throw _privateConstructorUsedError;
+  StringSingleLine get proteinName => throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $RecipeProteinTemplatesCopyWith<RecipeProteinTemplates> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $RecipeProteinTemplatesCopyWith<$Res> {
+  factory $RecipeProteinTemplatesCopyWith(RecipeProteinTemplates value,
+          $Res Function(RecipeProteinTemplates) then) =
+      _$RecipeProteinTemplatesCopyWithImpl<$Res>;
+  $Res call(
+      {int recipeProteinTemplateId,
+      StringSingleLine createdBy,
+      DateTime createdDate,
+      StringSingleLine lastModifiedBy,
+      DateTime lastModifiedDate,
+      StringSingleLine proteinName});
+}
+
+/// @nodoc
+class _$RecipeProteinTemplatesCopyWithImpl<$Res>
+    implements $RecipeProteinTemplatesCopyWith<$Res> {
+  _$RecipeProteinTemplatesCopyWithImpl(this._value, this._then);
+
+  final RecipeProteinTemplates _value;
+  // ignore: unused_field
+  final $Res Function(RecipeProteinTemplates) _then;
+
+  @override
+  $Res call({
+    Object? recipeProteinTemplateId = freezed,
+    Object? createdBy = freezed,
+    Object? createdDate = freezed,
+    Object? lastModifiedBy = freezed,
+    Object? lastModifiedDate = freezed,
+    Object? proteinName = freezed,
+  }) {
+    return _then(_value.copyWith(
+      recipeProteinTemplateId: recipeProteinTemplateId == freezed
+          ? _value.recipeProteinTemplateId
+          : recipeProteinTemplateId // ignore: cast_nullable_to_non_nullable
+              as int,
+      createdBy: createdBy == freezed
+          ? _value.createdBy
+          : createdBy // ignore: cast_nullable_to_non_nullable
+              as StringSingleLine,
+      createdDate: createdDate == freezed
+          ? _value.createdDate
+          : createdDate // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      lastModifiedBy: lastModifiedBy == freezed
+          ? _value.lastModifiedBy
+          : lastModifiedBy // ignore: cast_nullable_to_non_nullable
+              as StringSingleLine,
+      lastModifiedDate: lastModifiedDate == freezed
+          ? _value.lastModifiedDate
+          : lastModifiedDate // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      proteinName: proteinName == freezed
+          ? _value.proteinName
+          : proteinName // ignore: cast_nullable_to_non_nullable
+              as StringSingleLine,
+    ));
+  }
+}
+
+/// @nodoc
+abstract class _$RecipeProteinTemplatesCopyWith<$Res>
+    implements $RecipeProteinTemplatesCopyWith<$Res> {
+  factory _$RecipeProteinTemplatesCopyWith(_RecipeProteinTemplates value,
+          $Res Function(_RecipeProteinTemplates) then) =
+      __$RecipeProteinTemplatesCopyWithImpl<$Res>;
+  @override
+  $Res call(
+      {int recipeProteinTemplateId,
+      StringSingleLine createdBy,
+      DateTime createdDate,
+      StringSingleLine lastModifiedBy,
+      DateTime lastModifiedDate,
+      StringSingleLine proteinName});
+}
+
+/// @nodoc
+class __$RecipeProteinTemplatesCopyWithImpl<$Res>
+    extends _$RecipeProteinTemplatesCopyWithImpl<$Res>
+    implements _$RecipeProteinTemplatesCopyWith<$Res> {
+  __$RecipeProteinTemplatesCopyWithImpl(_RecipeProteinTemplates _value,
+      $Res Function(_RecipeProteinTemplates) _then)
+      : super(_value, (v) => _then(v as _RecipeProteinTemplates));
+
+  @override
+  _RecipeProteinTemplates get _value => super._value as _RecipeProteinTemplates;
+
+  @override
+  $Res call({
+    Object? recipeProteinTemplateId = freezed,
+    Object? createdBy = freezed,
+    Object? createdDate = freezed,
+    Object? lastModifiedBy = freezed,
+    Object? lastModifiedDate = freezed,
+    Object? proteinName = freezed,
+  }) {
+    return _then(_RecipeProteinTemplates(
+      recipeProteinTemplateId: recipeProteinTemplateId == freezed
+          ? _value.recipeProteinTemplateId
+          : recipeProteinTemplateId // ignore: cast_nullable_to_non_nullable
+              as int,
+      createdBy: createdBy == freezed
+          ? _value.createdBy
+          : createdBy // ignore: cast_nullable_to_non_nullable
+              as StringSingleLine,
+      createdDate: createdDate == freezed
+          ? _value.createdDate
+          : createdDate // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      lastModifiedBy: lastModifiedBy == freezed
+          ? _value.lastModifiedBy
+          : lastModifiedBy // ignore: cast_nullable_to_non_nullable
+              as StringSingleLine,
+      lastModifiedDate: lastModifiedDate == freezed
+          ? _value.lastModifiedDate
+          : lastModifiedDate // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      proteinName: proteinName == freezed
+          ? _value.proteinName
+          : proteinName // ignore: cast_nullable_to_non_nullable
+              as StringSingleLine,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$_RecipeProteinTemplates extends _RecipeProteinTemplates {
+  _$_RecipeProteinTemplates(
+      {required this.recipeProteinTemplateId,
+      required this.createdBy,
+      required this.createdDate,
+      required this.lastModifiedBy,
+      required this.lastModifiedDate,
+      required this.proteinName})
+      : super._();
+
+  @override
+  final int recipeProteinTemplateId;
+  @override
+  final StringSingleLine createdBy;
+  @override
+  final DateTime createdDate;
+  @override
+  final StringSingleLine lastModifiedBy;
+  @override
+  final DateTime lastModifiedDate;
+  @override
+  final StringSingleLine proteinName;
+
+  @override
+  String toString() {
+    return 'RecipeProteinTemplates(recipeProteinTemplateId: $recipeProteinTemplateId, createdBy: $createdBy, createdDate: $createdDate, lastModifiedBy: $lastModifiedBy, lastModifiedDate: $lastModifiedDate, proteinName: $proteinName)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other is _RecipeProteinTemplates &&
+            (identical(
+                    other.recipeProteinTemplateId, recipeProteinTemplateId) ||
+                const DeepCollectionEquality().equals(
+                    other.recipeProteinTemplateId, recipeProteinTemplateId)) &&
+            (identical(other.createdBy, createdBy) ||
+                const DeepCollectionEquality()
+                    .equals(other.createdBy, createdBy)) &&
+            (identical(other.createdDate, createdDate) ||
+                const DeepCollectionEquality()
+                    .equals(other.createdDate, createdDate)) &&
+            (identical(other.lastModifiedBy, lastModifiedBy) ||
+                const DeepCollectionEquality()
+                    .equals(other.lastModifiedBy, lastModifiedBy)) &&
+            (identical(other.lastModifiedDate, lastModifiedDate) ||
+                const DeepCollectionEquality()
+                    .equals(other.lastModifiedDate, lastModifiedDate)) &&
+            (identical(other.proteinName, proteinName) ||
+                const DeepCollectionEquality()
+                    .equals(other.proteinName, proteinName)));
+  }
+
+  @override
+  int get hashCode =>
+      runtimeType.hashCode ^
+      const DeepCollectionEquality().hash(recipeProteinTemplateId) ^
+      const DeepCollectionEquality().hash(createdBy) ^
+      const DeepCollectionEquality().hash(createdDate) ^
+      const DeepCollectionEquality().hash(lastModifiedBy) ^
+      const DeepCollectionEquality().hash(lastModifiedDate) ^
+      const DeepCollectionEquality().hash(proteinName);
+
+  @JsonKey(ignore: true)
+  @override
+  _$RecipeProteinTemplatesCopyWith<_RecipeProteinTemplates> get copyWith =>
+      __$RecipeProteinTemplatesCopyWithImpl<_RecipeProteinTemplates>(
+          this, _$identity);
+}
+
+abstract class _RecipeProteinTemplates extends RecipeProteinTemplates {
+  factory _RecipeProteinTemplates(
+      {required int recipeProteinTemplateId,
+      required StringSingleLine createdBy,
+      required DateTime createdDate,
+      required StringSingleLine lastModifiedBy,
+      required DateTime lastModifiedDate,
+      required StringSingleLine proteinName}) = _$_RecipeProteinTemplates;
+  _RecipeProteinTemplates._() : super._();
+
+  @override
+  int get recipeProteinTemplateId => throw _privateConstructorUsedError;
+  @override
+  StringSingleLine get createdBy => throw _privateConstructorUsedError;
+  @override
+  DateTime get createdDate => throw _privateConstructorUsedError;
+  @override
+  StringSingleLine get lastModifiedBy => throw _privateConstructorUsedError;
+  @override
+  DateTime get lastModifiedDate => throw _privateConstructorUsedError;
+  @override
+  StringSingleLine get proteinName => throw _privateConstructorUsedError;
+  @override
+  @JsonKey(ignore: true)
+  _$RecipeProteinTemplatesCopyWith<_RecipeProteinTemplates> get copyWith =>
+      throw _privateConstructorUsedError;
+}

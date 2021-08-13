0577866e2b445e2cba231622e087be6befd89908
aditya <aditya_jinegar@yahoo.com>
2021-08-13 13:53:00 -0400

    Create mindfulEnergyTemplatesEducationMaterials.freezed.dart

---
+++ lib/domain/Templates/Resources/MindfulEnergyTemplatesEducationMaterials/mindfulEnergyTemplatesEducationMaterials.freezed.dart
@@ -0,0 +1,205 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides
+
+part of 'mindfulEnergyTemplatesEducationMaterials.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more informations: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+class _$MindfulEnergyTemplatesEducationMaterialsTearOff {
+  const _$MindfulEnergyTemplatesEducationMaterialsTearOff();
+
+  _MindfulEnergyTemplatesEducationMaterials call(
+      {required int mindfulEnergyTemplateMindfulEnergyTemplateId,
+      required int educationMaterialsResourceTemplateId}) {
+    return _MindfulEnergyTemplatesEducationMaterials(
+      mindfulEnergyTemplateMindfulEnergyTemplateId:
+          mindfulEnergyTemplateMindfulEnergyTemplateId,
+      educationMaterialsResourceTemplateId:
+          educationMaterialsResourceTemplateId,
+    );
+  }
+}
+
+/// @nodoc
+const $MindfulEnergyTemplatesEducationMaterials =
+    _$MindfulEnergyTemplatesEducationMaterialsTearOff();
+
+/// @nodoc
+mixin _$MindfulEnergyTemplatesEducationMaterials {
+  int get mindfulEnergyTemplateMindfulEnergyTemplateId =>
+      throw _privateConstructorUsedError;
+  int get educationMaterialsResourceTemplateId =>
+      throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $MindfulEnergyTemplatesEducationMaterialsCopyWith<
+          MindfulEnergyTemplatesEducationMaterials>
+      get copyWith => throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $MindfulEnergyTemplatesEducationMaterialsCopyWith<$Res> {
+  factory $MindfulEnergyTemplatesEducationMaterialsCopyWith(
+          MindfulEnergyTemplatesEducationMaterials value,
+          $Res Function(MindfulEnergyTemplatesEducationMaterials) then) =
+      _$MindfulEnergyTemplatesEducationMaterialsCopyWithImpl<$Res>;
+  $Res call(
+      {int mindfulEnergyTemplateMindfulEnergyTemplateId,
+      int educationMaterialsResourceTemplateId});
+}
+
+/// @nodoc
+class _$MindfulEnergyTemplatesEducationMaterialsCopyWithImpl<$Res>
+    implements $MindfulEnergyTemplatesEducationMaterialsCopyWith<$Res> {
+  _$MindfulEnergyTemplatesEducationMaterialsCopyWithImpl(
+      this._value, this._then);
+
+  final MindfulEnergyTemplatesEducationMaterials _value;
+  // ignore: unused_field
+  final $Res Function(MindfulEnergyTemplatesEducationMaterials) _then;
+
+  @override
+  $Res call({
+    Object? mindfulEnergyTemplateMindfulEnergyTemplateId = freezed,
+    Object? educationMaterialsResourceTemplateId = freezed,
+  }) {
+    return _then(_value.copyWith(
+      mindfulEnergyTemplateMindfulEnergyTemplateId:
+          mindfulEnergyTemplateMindfulEnergyTemplateId == freezed
+              ? _value.mindfulEnergyTemplateMindfulEnergyTemplateId
+              : mindfulEnergyTemplateMindfulEnergyTemplateId // ignore: cast_nullable_to_non_nullable
+                  as int,
+      educationMaterialsResourceTemplateId: educationMaterialsResourceTemplateId ==
+              freezed
+          ? _value.educationMaterialsResourceTemplateId
+          : educationMaterialsResourceTemplateId // ignore: cast_nullable_to_non_nullable
+              as int,
+    ));
+  }
+}
+
+/// @nodoc
+abstract class _$MindfulEnergyTemplatesEducationMaterialsCopyWith<$Res>
+    implements $MindfulEnergyTemplatesEducationMaterialsCopyWith<$Res> {
+  factory _$MindfulEnergyTemplatesEducationMaterialsCopyWith(
+          _MindfulEnergyTemplatesEducationMaterials value,
+          $Res Function(_MindfulEnergyTemplatesEducationMaterials) then) =
+      __$MindfulEnergyTemplatesEducationMaterialsCopyWithImpl<$Res>;
+  @override
+  $Res call(
+      {int mindfulEnergyTemplateMindfulEnergyTemplateId,
+      int educationMaterialsResourceTemplateId});
+}
+
+/// @nodoc
+class __$MindfulEnergyTemplatesEducationMaterialsCopyWithImpl<$Res>
+    extends _$MindfulEnergyTemplatesEducationMaterialsCopyWithImpl<$Res>
+    implements _$MindfulEnergyTemplatesEducationMaterialsCopyWith<$Res> {
+  __$MindfulEnergyTemplatesEducationMaterialsCopyWithImpl(
+      _MindfulEnergyTemplatesEducationMaterials _value,
+      $Res Function(_MindfulEnergyTemplatesEducationMaterials) _then)
+      : super(_value,
+            (v) => _then(v as _MindfulEnergyTemplatesEducationMaterials));
+
+  @override
+  _MindfulEnergyTemplatesEducationMaterials get _value =>
+      super._value as _MindfulEnergyTemplatesEducationMaterials;
+
+  @override
+  $Res call({
+    Object? mindfulEnergyTemplateMindfulEnergyTemplateId = freezed,
+    Object? educationMaterialsResourceTemplateId = freezed,
+  }) {
+    return _then(_MindfulEnergyTemplatesEducationMaterials(
+      mindfulEnergyTemplateMindfulEnergyTemplateId:
+          mindfulEnergyTemplateMindfulEnergyTemplateId == freezed
+              ? _value.mindfulEnergyTemplateMindfulEnergyTemplateId
+              : mindfulEnergyTemplateMindfulEnergyTemplateId // ignore: cast_nullable_to_non_nullable
+                  as int,
+      educationMaterialsResourceTemplateId: educationMaterialsResourceTemplateId ==
+              freezed
+          ? _value.educationMaterialsResourceTemplateId
+          : educationMaterialsResourceTemplateId // ignore: cast_nullable_to_non_nullable
+              as int,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$_MindfulEnergyTemplatesEducationMaterials
+    extends _MindfulEnergyTemplatesEducationMaterials {
+  _$_MindfulEnergyTemplatesEducationMaterials(
+      {required this.mindfulEnergyTemplateMindfulEnergyTemplateId,
+      required this.educationMaterialsResourceTemplateId})
+      : super._();
+
+  @override
+  final int mindfulEnergyTemplateMindfulEnergyTemplateId;
+  @override
+  final int educationMaterialsResourceTemplateId;
+
+  @override
+  String toString() {
+    return 'MindfulEnergyTemplatesEducationMaterials(mindfulEnergyTemplateMindfulEnergyTemplateId: $mindfulEnergyTemplateMindfulEnergyTemplateId, educationMaterialsResourceTemplateId: $educationMaterialsResourceTemplateId)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other is _MindfulEnergyTemplatesEducationMaterials &&
+            (identical(other.mindfulEnergyTemplateMindfulEnergyTemplateId,
+                    mindfulEnergyTemplateMindfulEnergyTemplateId) ||
+                const DeepCollectionEquality().equals(
+                    other.mindfulEnergyTemplateMindfulEnergyTemplateId,
+                    mindfulEnergyTemplateMindfulEnergyTemplateId)) &&
+            (identical(other.educationMaterialsResourceTemplateId,
+                    educationMaterialsResourceTemplateId) ||
+                const DeepCollectionEquality().equals(
+                    other.educationMaterialsResourceTemplateId,
+                    educationMaterialsResourceTemplateId)));
+  }
+
+  @override
+  int get hashCode =>
+      runtimeType.hashCode ^
+      const DeepCollectionEquality()
+          .hash(mindfulEnergyTemplateMindfulEnergyTemplateId) ^
+      const DeepCollectionEquality().hash(educationMaterialsResourceTemplateId);
+
+  @JsonKey(ignore: true)
+  @override
+  _$MindfulEnergyTemplatesEducationMaterialsCopyWith<
+          _MindfulEnergyTemplatesEducationMaterials>
+      get copyWith => __$MindfulEnergyTemplatesEducationMaterialsCopyWithImpl<
+          _MindfulEnergyTemplatesEducationMaterials>(this, _$identity);
+}
+
+abstract class _MindfulEnergyTemplatesEducationMaterials
+    extends MindfulEnergyTemplatesEducationMaterials {
+  factory _MindfulEnergyTemplatesEducationMaterials(
+          {required int mindfulEnergyTemplateMindfulEnergyTemplateId,
+          required int educationMaterialsResourceTemplateId}) =
+      _$_MindfulEnergyTemplatesEducationMaterials;
+  _MindfulEnergyTemplatesEducationMaterials._() : super._();
+
+  @override
+  int get mindfulEnergyTemplateMindfulEnergyTemplateId =>
+      throw _privateConstructorUsedError;
+  @override
+  int get educationMaterialsResourceTemplateId =>
+      throw _privateConstructorUsedError;
+  @override
+  @JsonKey(ignore: true)
+  _$MindfulEnergyTemplatesEducationMaterialsCopyWith<
+          _MindfulEnergyTemplatesEducationMaterials>
+      get copyWith => throw _privateConstructorUsedError;
+}
